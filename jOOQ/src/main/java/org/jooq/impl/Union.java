0eeb7cbf41f888f5cfd300d195a9c9be70782a24
Lukas Eder <lukas.eder@gmail.com>
2011-09-06 20:49:26 +0000

    [jOOQ/jOOQ#800] Add support for Sybase Adaptive Server Enterprise - fixed some integration tests

--- jOOQ/src/main/java/org/jooq/impl/Union.java
+++ jOOQ/src/main/java/org/jooq/impl/Union.java
@@ -101,10 +101,11 @@ class Union<R extends Record> extends AbstractSelect<R> {
 
     private final void wrappingParenthesis(RenderContext context, String parenthesis) {
         switch (context.getDialect()) {
-            // SQLite and DERBY have some syntax issues with unions.
+            // Sybase ASE, SQLite and DERBY have some syntax issues with unions.
             // Check out https://issues.apache.org/jira/browse/DERBY-2374
-            case SQLITE: // no break
-            case DERBY:  // no break
+            case SQLITE:
+            case DERBY:
+            case ASE:
 
             // [#288] MySQL has a very special way of dealing with UNION's
             // So include it as well

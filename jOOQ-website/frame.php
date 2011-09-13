030817ca58453e2894a989bbcd321aba408422f8
Lukas Eder <lukas.eder@gmail.com>
2011-09-13 22:28:22 +0000

    First draft of jOOQ website

---
+++ jOOQ-website/frame.php
@@ -0,0 +1,64 @@
+<html>
+	<head>
+		<title>jOOQ</title>
+		<link href="css/jooq.css" type="text/css" rel="stylesheet">
+		<link href="js/prettify/prettify.css" type="text/css" rel="stylesheet" />
+		<script type="text/javascript" src="js/prettify/prettify.js"></script>
+	</head>
+	<body onload="prettyPrint()">
+		<div id="wrapper">
+			<div id="all">
+				<div id="header">
+				    <div id="navigation">
+				    	<div class="navigation-item-left">
+				    		<a href="index.php" title="jOOQ Home Page">Home</a>
+			    		</div>
+				    	<div class="navigation-item-left">
+				    		<a href="https://sourceforge.net/projects/jooq/files/" title="jOOQ Download">Download</a>
+				    	</div>
+				    	<div class="navigation-item-left">
+				    		<a href="manual.php" title="jOOQ User Manual">Manual</a>
+				    	</div>
+				    	<div class="navigation-item-left">
+				    		<a href="http://jooq.sourceforge.net/javadoc/latest/" title="jOOQ Main Javadoc">Javadoc</a>
+				    	</div>
+				    	<div class="navigation-item-left">
+				    		<a href="http://jooq.sourceforge.net/releasenotes/RELEASENOTES.txt" title="jOOQ Release Notes">Release Notes</a>
+				    	</div>
+				    	<div class="navigation-item-left">
+				    		<a href="https://sourceforge.net/apps/trac/jooq/report/6" title="jOOQ Roadmap">Roadmap</a>
+				    	</div>
+				    	<div class="navigation-item-left">
+				    		<a href="contribute.php" title="Contribute to jOOQ">Contribute</a>
+				    	</div>
+				    	<div class="navigation-item-left">
+				    		<a href="https://sourceforge.net/project/project_donations.php?group_id=283484" title="Donate to jOOQ, if you like it!">Donate</a>
+				    	</div>
+				    	<div class="navigation-item-left">
+				    		<a href="links.php" title="Interesting links for jOOQ users">Links</a>
+				    	</div>
+				    </div>
+
+					<table width="100%" cellpadding="0" cellspacing="0">
+						<tr>
+							<td width="700">
+								<h1>jOOQ : A peace treaty between SQL and Java</h1></td>
+							<td align="right"><img src="img/logo.png" alt="jOOQ Logo"/></td>
+						</tr>
+					</table>
+				</div>
+				<div id="content">
+					<?php printSlogan(); ?>
+					<?php printContent(); ?>
+				</div>
+				
+				<br/>
+				<br/>
+			</div>
+		</div>
+		
+		<a href="https://github.com/lukaseder/jOOQ"> <img
+			alt="Fork me on GitHub" src="img/forkme.png"
+			style="position: absolute; top: 0; right: 0; border: 0;"> </a>
+	</body>
+</html>

5d8e96e58f1f2ec0a05dae2aaa2f0e74802ac20f
Lukas Eder <lukas.eder@gmail.com>
2011-09-17 14:17:28 +0000

    Added some sections to the manual
    git-svn-id: https://jooq.svn.sourceforge.net/svnroot/jooq/trunk@1368 fe1456f5-c80f-4a60-b34a-41941cf1a92a

--- jOOQ-website/links.php
+++ jOOQ-website/links.php
@@ -1,18 +1,23 @@
 <?php 
 require 'frame.php';
 function printH1() {
-	print 'Useful links for jOOQ';
+	print 'Useful links for jOOQ users and developers';
 }
-function getSlogan() {}
+function getSlogan() {
+	return "You've come far in your search of more intuitive integration of SQL and Java. 
+			Here you will find further resources related to jOOQ, to similar products and
+			other inspiration related to jOOQ";
+}
 function printContent() {
+	global $root;
 ?>
-<h2>NEWS</h2>
+<h2>jOOQ news groups</h2>
 <ul>
 	<li>Subscribe to the <a href="http://groups.google.com/group/jooq-user">jOOQ User Group</a></li>
 	<li>Subscribe to the <a href="http://groups.google.com/group/jooq-developer">jOOQ Developer Group</a></li>
 </ul>
 
-<h2>Follow jOOQ</h2>
+<h2>jOOQ in the WWW</h2>
 <ul>
 	<li>On Wordpress: <a href="http://lukaseder.wordpress.com/" title="Lukas Eder's blog about Java, SQL and jOOQ">http://lukaseder.wordpress.com/</a></li>
     <li>On SourceForge: <a href="https://sourceforge.net/projects/jooq/" title="jOOQ download and hosting on source forge">https://sourceforge.net/projects/jooq/</a></li>
@@ -22,7 +27,7 @@ function printContent() {
     <li>On Stack Overflow: <a href="http://stackoverflow.com/questions/tagged/jooq" title="ask questions about jOOQ on Stack Overflow">http://stackoverflow.com/questions/tagged/jooq</a></li> 
 </ul>
 
-<h2>Similar products</h2>
+<h2>jOOQ's source of inspiration, similar products</h2>
 <p>When you consider jOOQ for your project, you might also have considered any of these similar products:</p>
 
 <ul>
@@ -42,11 +47,11 @@ function printContent() {
 	<li><a href="http://www.oracle.com/technetwork/java/javaee/tech/persistence-jsp-140049.html" title="QueryDSL, a product similar to jOOQ">JPA</a>: The J2EE standard</li>
 </ul>
 
-<h2>Other tools</h2>
+<h2>jOOQ's little sister</h2>
 <div style="height: 100px;">
 	<a href="http://code.google.com/p/joox/"
 		title="jOOX, a product inspired by jOOQ">
-		<img src="<?=root?>/img/joox-small.png" alt="jOOX" style="float:left; margin-right: 4em; border: 0"/></a>
+		<img src="<?=$root?>/img/joox-small.png" alt="jOOX" style="float:left; margin-right: 4em; border: 0"/></a>
 	<p><a href="http://code.google.com/p/joox/"
 		title="jOOX, a product inspired by jOOQ">jOOX</a> stands for Java
 	Object Oriented XML. It is a simple wrapper for the org.w3c.dom package,

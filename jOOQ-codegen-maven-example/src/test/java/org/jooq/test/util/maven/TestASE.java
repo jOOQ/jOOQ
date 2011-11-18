453ddf915ac6c994fe240ef3e50670aeee306f5a
Lukas Eder <lukas.eder@gmail.com>
2011-11-18 14:52:21 +0000

    [#954] Add examples for source code generation of multiple schemata with Maven
    git-svn-id: https://jooq.svn.sourceforge.net/svnroot/jooq/trunk@1566 fe1456f5-c80f-4a60-b34a-41941cf1a92a

--- jOOQ-codegen-maven-example/src/test/java/org/jooq/test/util/maven/TestASE.java
+++ jOOQ-codegen-maven-example/src/test/java/org/jooq/test/util/maven/TestASE.java
@@ -36,57 +36,59 @@
 package org.jooq.test.util.maven;
 
 import static junit.framework.Assert.assertEquals;
+import static org.jooq.impl.Factory.countDistinct;
+import static org.jooq.util.maven.example.ase.Tables.T_AUTHOR;
+import static org.jooq.util.maven.example.ase.Tables.T_BOOK;
+import static org.jooq.util.maven.example.ase.Tables.T_BOOK_STORE;
+import static org.jooq.util.maven.example.ase.Tables.T_BOOK_TO_BOOK_STORE;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.Arrays;
-import java.util.List;
 
 import org.jooq.Record;
 import org.jooq.Result;
-import org.jooq.util.maven.example.postgres.PublicFactory;
-import org.jooq.util.maven.example.postgres.tables.TBook;
-import org.jooq.util.maven.example.postgres.tables.records.TBookRecord;
+import org.jooq.util.maven.example.ase.DboFactory;
+import org.jooq.util.maven.example.ase.tables.TAuthor;
+import org.jooq.util.maven.example.ase.tables.TBook;
+import org.jooq.util.maven.example.ase.tables.TBookStore;
+import org.jooq.util.maven.example.ase.tables.TBookToBookStore;
 
 import org.junit.Test;
 
 /**
  * @author Lukas Eder
  */
-public class TestGeneratedSources {
-
-    private static final List<Integer> BOOK_IDS    = Arrays.asList(1, 2, 3, 4);
-    private static final List<String>  BOOK_TITLES   = Arrays.asList("1984", "Animal Farm", "O Alquimista", "Brida");
+public class TestASE {
 
     @Test
-    public void testFetch() throws Exception {
-        Connection connection = DriverManager.getConnection("jdbc:postgresql:postgres", "postgres", "test");
-        PublicFactory create = new PublicFactory(connection);
+    public void testInstanceModel() throws Exception {
+        Class.forName("net.sourceforge.jtds.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:jtds:sybase://lukas-hp:5000/TEST", "sa", "");
+        DboFactory create = new DboFactory(connection);
 
-        Result<Record> books =
-        create.select(TBook.ID, TBook.TITLE)
-              .from(TBook.T_BOOK)
-              .orderBy(TBook.ID)
+        TBook b = T_BOOK.as("b");
+        TAuthor a = T_AUTHOR.as("a");
+        TBookStore s = T_BOOK_STORE.as("s");
+        TBookToBookStore t = T_BOOK_TO_BOOK_STORE.as("t");
+
+        Result<Record> result =
+        create.select(a.FIRST_NAME, a.LAST_NAME, countDistinct(s.NAME))
+              .from(a)
+              .join(b).on(b.AUTHOR_ID.equal(a.ID))
+              .join(t).on(t.BOOK_ID.equal(b.ID))
+              .join(s).on(t.BOOK_STORE_NAME.equal(s.NAME))
+              .groupBy(a.FIRST_NAME, a.LAST_NAME)
+              .orderBy(countDistinct(s.NAME).desc())
               .fetch();
 
-        assertEquals(BOOK_IDS, books.getValues(0));
-        assertEquals(BOOK_TITLES, books.getValues(1));
-    }
+        assertEquals(2, result.size());
+        assertEquals("Paulo", result.getValue(0, a.FIRST_NAME));
+        assertEquals("George", result.getValue(1, a.FIRST_NAME));
 
-    @Test
-    public void testFetchInto() throws Exception {
-        Connection connection = DriverManager.getConnection("jdbc:postgresql:postgres", "postgres", "test");
-        PublicFactory create = new PublicFactory(connection);
+        assertEquals("Coelho", result.getValue(0, a.LAST_NAME));
+        assertEquals("Orwell", result.getValue(1, a.LAST_NAME));
 
-        List<TBookRecord> books =
-        create.select(TBook.ID, TBook.TITLE)
-              .from(TBook.T_BOOK)
-              .orderBy(TBook.ID)
-              .fetchInto(TBookRecord.class);
-
-        for (int i = 0; i < BOOK_IDS.size(); i++) {
-            assertEquals(BOOK_IDS.get(i), books.get(i).getId());
-            assertEquals(BOOK_TITLES.get(i), books.get(i).getTitle());
-        }
+        assertEquals(Integer.valueOf(3), result.getValue(0, countDistinct(s.NAME)));
+        assertEquals(Integer.valueOf(2), result.getValue(1, countDistinct(s.NAME)));
     }
 }

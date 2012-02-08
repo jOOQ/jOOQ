220d56f46ff49d8e1a1c1da1c3694bcecd53fa7a
Lukas Eder <lukas.eder@gmail.com>
2012-02-08 21:50:13 +0000

    Fixed home page example

--- jOOQ-website/index.php
+++ jOOQ-website/index.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 require 'frame.php';
 function getH1() {
 	return 'jOOQ : A peace treaty between SQL and Java';
 }
 function getSlogan() {
-	return "SQL was never meant to be abstracted. To be confined in the narrow boundaries 
+	return "SQL was never meant to be abstracted. To be confined in the narrow boundaries
 			of heavy mappers, hiding the beauty and simplicity of relational data.
 			SQL was never meant to be object-oriented. SQL was never meant to be
 			anything other than... SQL!";
@@ -48,18 +48,18 @@ create.selectFrom(BOOK)
 GROUP BY FIRST_NAME, LAST_NAME
   HAVING COUNT(*) &gt; 5
 ORDER BY LAST_NAME ASC NULLS FIRST
-   LIMIT 2 
+   LIMIT 2
   OFFSET 1
      FOR UPDATE
       OF FIRST_NAME, LAST_NAME</pre></td>
 		<td width="50%" class="right"><pre class="prettyprint lang-java">
-create.select(FIRST_NAME, LAST_NAME, create.count())
+create.select(FIRST_NAME, LAST_NAME, count())
       .from(AUTHOR)
       .join(BOOK).on(Author.ID.equal(Book.AUTHOR_ID))
       .where(LANGUAGE.equal("DE"))
-      .and(PUBLISHED.greaterThan(parseDate('2008-01-01')))
+      .and(PUBLISHED.greaterThan(parseDate("2008-01-01")))
       .groupBy(FIRST_NAME, LAST_NAME)
-      .having(create.count().greaterThan(5))
+      .having(count().greaterThan(5))
       .orderBy(LAST_NAME.asc().nullsFirst())
       .limit(2)
       .offset(1)
@@ -67,10 +67,10 @@ create.select(FIRST_NAME, LAST_NAME, create.count())
       .of(FIRST_NAME, LAST_NAME)</pre></td>
 	</tr>
 </table>
-      
+
     <h2>What is jOOQ?</h2>
     <p>jOOQ stands for Java Object Oriented Querying. It combines these essential features:</p>
-      
+
     <ul>
     <li>Code Generation: jOOQ generates a simple Java representation of your database schema. Every table, view, stored procedure, enum, UDT is a class.</li>
 	<li>Active records: jOOQ implements an easy-to-use active record pattern. It is NOT an OR-mapper, but provides a 1:1 mapping between tables/views and classes. Between columns and members.</li>
@@ -78,7 +78,7 @@ create.select(FIRST_NAME, LAST_NAME, create.count())
     <li>SQL standard: jOOQ supports all standard SQL language features including the more complex UNION's, nested SELECTs, joins, aliasing</li>
     <li>Vendor-specific feature support: jOOQ encourages the use of vendor-specific extensions such as stored procedures, UDT's and ARRAY's, recursive queries, and many more.</li>
     </ul>
-      
+
     <h2>How does jOOQ help you?</h2>
     <ul>
     <li>Your database always comes FIRST! That's where the schema is, not in your Java code or some XML mapping file.</li>
@@ -95,7 +95,7 @@ create.select(FIRST_NAME, LAST_NAME, create.count())
     <ul>
     <li>You can be productive again!</li>
     </ul>
-    
+
     <h2>When to use jOOQ</h2>
     <ul>
     <li>When you love your RDBMS of choice, including all its vendor-specific features.</li>
@@ -105,7 +105,7 @@ create.select(FIRST_NAME, LAST_NAME, create.count())
     <li>When you love stored procedures.</li>
     <li>When you love both <a href="http://en.wikipedia.org/wiki/OLAP" title="Online Analytical Processing, advanced SELECT statements, i.e. what none of the ORM's do, but jOOQ does best">OLAP</a> and <a href="http://en.wikipedia.org/wiki/OLTP" title="Online Transaction Processing, i.e. basic CRUD operations, what all ORM's do">OLTP</a></li>
     </ul>
-    
+
     <h2>When not to use jOOQ</h2>
     <p>On the other hand, many people like the ease of use of Hibernate or other products, when it comes to simply persisting any domain model in any database. You should not use jOOQ...</p>
     <ul>
@@ -113,15 +113,15 @@ create.select(FIRST_NAME, LAST_NAME, create.count())
     <li>When you don't really need SQL.</li>
     <li>When you want to map your object-oriented domain model to a database and not vice versa.</li>
     <li>When your schema changes more frequently than you can re-deploy jOOQ-generated source code.</li>
-    <li>When you need to write DDL statements. jOOQ only supports DML statements.</li> 
+    <li>When you need to write DDL statements. jOOQ only supports DML statements.</li>
     </ul>
-    
+
     <h2>What databases are supported</h2>
-    <p>Every RDMBS out there has its own little specialties. 
-    jOOQ considers those specialties as much as possible, while trying to 
-    standardise the behaviour in jOOQ. In order to increase the quality of jOOQ, 
-    some 70 unit tests are run for syntax and variable binding verification, 
-    as well as some 130 integration tests with an overall of around 900 queries for any 
+    <p>Every RDMBS out there has its own little specialties.
+    jOOQ considers those specialties as much as possible, while trying to
+    standardise the behaviour in jOOQ. In order to increase the quality of jOOQ,
+    some 70 unit tests are run for syntax and variable binding verification,
+    as well as some 130 integration tests with an overall of around 900 queries for any
     of these databases:</p>
 <ul>
     <li>DB2 9.7</li>
@@ -141,7 +141,7 @@ create.select(FIRST_NAME, LAST_NAME, create.count())
 <ul>
 	<li>Google Cloud SQL (MySQL)</li>
 </ul>
-	
+
 <h3>Planned (Contributions and suggestions are very welcome!)</h3>
     <ul>
     <li>Access</li>
@@ -152,14 +152,14 @@ create.select(FIRST_NAME, LAST_NAME, create.count())
     <li>Sybase SQL Anywhere OnDemand</li>
     <li>Teradata</li>
     </ul>
-    
+
     <h2>Other requirements</h2>
-<p>jOOQ runs with Java 1.6+</p> 
+<p>jOOQ runs with Java 1.6+</p>
 
 <h3>License</h3>
-<p>jOOQ is licensed under the <a href="http://www.apache.org/licenses/LICENSE-2.0" title="Apache Software License 2.0">Apache Software License 2.0</a></p>	
-	
-	
+<p>jOOQ is licensed under the <a href="http://www.apache.org/licenses/LICENSE-2.0" title="Apache Software License 2.0">Apache Software License 2.0</a></p>
+
+
 <h3>Thanks</h3>
 <p>YourKit is kindly supporting open source projects with its
 full-featured Java Profiler. YourKit, LLC is the creator of innovative
@@ -169,6 +169,6 @@ look at YourKit's leading software products: <a
 Profiler</a> and <a
 	href="http://www.yourkit.com/.net/profiler/index.jsp">YourKit .NET
 Profiler.</a></p>
-<?php 
+<?php
 }
 ?>

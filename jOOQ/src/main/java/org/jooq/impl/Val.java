edcd307b04b6fd00d282ab24c7ec3e2a556ebd12
Lukas Eder <lukas.eder@gmail.com>
2012-02-10 19:22:54 +0000

    [#1125] Postgres needs casting for date time data types in queries like SELECT ? FROM DUAL

--- jOOQ/src/main/java/org/jooq/impl/Val.java
+++ jOOQ/src/main/java/org/jooq/impl/Val.java
@@ -170,8 +170,9 @@ class Val<T> extends AbstractField<T> implements Param<T>, BindingProvider {
 
         // [#1029] Postgres generally doesn't need the casting. Only in the
         // above case where the type is OTHER
-        // [#1125] TODO Also with temporal data types, casting is needed some times
-        else if (context.getDialect() == POSTGRES /* && !type.isTemporal() */) {
+        // [#1125] Also with temporal data types, casting is needed some times
+        // [#1130] TODO type can be null for ARRAY types, etc.
+        else if (context.getDialect() == POSTGRES && (type == null || !type.isTemporal())) {
             toSQL(context, getValue(), getType());
         }
 

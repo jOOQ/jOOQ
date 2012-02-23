4c72e6982055e6816db84f7337a21fb874f331a3
Lukas Eder <lukas.eder@gmail.com>
2012-02-23 19:09:11 +0000

    [#1177] Add SQL Console module to jOOQ - Added default Eclipse settings to project [#1177] Add SQL Console module to jOOQ - Added license header to Java classes

--- jOOQ-sql-console/src/main/java/org/jooq/debugger/UsageTrackingPreparedStatement.java
+++ jOOQ-sql-console/src/main/java/org/jooq/debugger/UsageTrackingPreparedStatement.java
@@ -1,3 +1,39 @@
+/**
+ * Copyright (c) 2009-2012, Lukas Eder, lukas.eder@gmail.com
+ *                          Christopher Deckers, chrriis@gmail.com
+ * All rights reserved.
+ *
+ * This software is licensed to you under the Apache License, Version 2.0
+ * (the "License"); You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * . Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * . Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * . Neither the name "jOOQ" nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.jooq.debugger;
 
 import java.io.InputStream;
@@ -28,7 +64,7 @@ import java.util.List;
 public class UsageTrackingPreparedStatement implements PreparedStatement {
 
 	private PreparedStatement stmt;
-	
+
     public UsageTrackingPreparedStatement(PreparedStatement stmt) {
         this.stmt = stmt;
     }
@@ -607,7 +643,7 @@ public class UsageTrackingPreparedStatement implements PreparedStatement {
     	sb.append("]");
     	isNew = true;
     }
-    
+
     private void logValue(int parameterIndex, Object o) {
     	isNew = false;
 		int missingLength = parameterIndex + 1 - paramList.size();
@@ -616,7 +652,7 @@ public class UsageTrackingPreparedStatement implements PreparedStatement {
     	}
     	paramList.set(parameterIndex, String.valueOf(o));
 	}
-    
+
     public String getParameterDescription() {
     	if(isNew) {
     		if(paramList.isEmpty() && sb.length() == 0) {
@@ -626,5 +662,5 @@ public class UsageTrackingPreparedStatement implements PreparedStatement {
     	}
     	return sb.toString();
     }
-    
+
 }

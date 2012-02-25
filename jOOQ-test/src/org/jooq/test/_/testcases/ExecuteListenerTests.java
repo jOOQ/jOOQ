8844362bf7959d965858e04fa3daec9a2d152414
Lukas Eder <lukas.eder@gmail.com>
2012-02-25 09:30:42 +0000

    Small test fix

--- jOOQ-test/src/org/jooq/test/_/testcases/ExecuteListenerTests.java
+++ jOOQ-test/src/org/jooq/test/_/testcases/ExecuteListenerTests.java
@@ -113,7 +113,7 @@ extends BaseTest<A, B, S, B2S, BS, L, X, DATE, D, T, U, I, IPK, T658, T725, T639
         assertEquals(3, ResultQueryListener.renderEnd);
         assertEquals(4, ResultQueryListener.prepareStart);
         assertEquals(5, ResultQueryListener.prepareEnd);
-        assertEquals(plus > 2 ? 6 : 0, ResultQueryListener.bindStart);
+        assertEquals(plus > 0 ? 6 : 0, ResultQueryListener.bindStart);
         assertEquals(plus > 0 ? 7 : 0, ResultQueryListener.bindEnd);
         assertEquals(6 + plus, ResultQueryListener.executeStart);
         assertEquals(7 + plus, ResultQueryListener.executeEnd);

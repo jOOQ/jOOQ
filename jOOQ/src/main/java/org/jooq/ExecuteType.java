8494280b3aacbe4df89c8b6e2ece67c7e5c3b6c6
Lukas Eder <lukas.eder@gmail.com>
2012-02-26 10:24:56 +0000

    [jOOQ/jOOQ#1177] Add SQL Console module to jOOQ - Fixeed ordering of ExecuteType literals for sorting in SQL Console UI

--- jOOQ/src/main/java/org/jooq/ExecuteType.java
+++ jOOQ/src/main/java/org/jooq/ExecuteType.java
@@ -36,31 +36,27 @@
 package org.jooq;
 
 /**
- * The type of database interaction that is being executed with this
- * context.
+ * The type of database interaction that is being executed with this context.
  */
 public enum ExecuteType {
 
     /**
      * A <code>SELECT</code> query is being executed
+     * <p>
+     * This may also apply to plain SQL <code>WITH .. SELECT</code> queries
+     * (selections with common table expressions), <code>FETCH</code> queries
+     * and other types of vendor-specific queries.
      */
     READ,
 
     /**
      * An <code>INSERT</code>, <code>UPDATE</code>, <code>DELETE</code>,
      * <code>MERGE</code> query is being executed
+     * <p>
+     * This may also apply to plain SQL <code>REPLACE</code>,
+     * <code>UPSERT</code> and other vendor-specific queries.
      */
     WRITE,
-
-    /**
-     * A routine (stored procedure or function) is being executed
-     */
-    ROUTINE,
-
-    /**
-     * A batch statement is being executed (not yet supported)
-     */
-    BATCH,
 
     /**
      * A DDL statement is being executed
@@ -68,6 +64,16 @@ public enum ExecuteType {
      * Currently, this only applies to <code>TRUNCATE</code> statements
      */
     DDL,
+
+    /**
+     * A batch statement is being executed (not yet supported)
+     */
+    BATCH,
+
+    /**
+     * A routine (stored procedure or function) is being executed
+     */
+    ROUTINE,
 
     /**
      * An other (unknown) type of database interaction is being executed

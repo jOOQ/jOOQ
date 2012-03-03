86402653edfa074b5f8fa75ccfb82cff67948d86
Lukas Eder <lukas.eder@gmail.com>
2012-03-03 13:44:12 +0000

    [#1202] Add support for the relational division operation: A.divideBy(B).on(A.ID.equal(B.A_ID)).returning(A.X, A.Y, ...)

---
+++ jOOQ/src/main/java/org/jooq/DivideByOnConditionStep.java
@@ -0,0 +1,159 @@
+/**
+ * Copyright (c) 2009-2012, Lukas Eder, lukas.eder@gmail.com
+ * All rights reserved.
+ *
+ * This software is licensed to you under the Apache License, Version 2.0
+ * (the "License"); You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * . Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * . Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * . Neither the name "jOOQ" nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.jooq;
+
+import org.jooq.impl.Factory;
+
+/**
+ * An intermediate type for the construction of a relational division. This type
+ * can be used as a convenience type for connecting more conditions.
+ *
+ * @author Lukas Eder
+ */
+public interface DivideByOnConditionStep extends DivideByReturningStep {
+
+    /**
+     * Combine the currently assembled conditions with another one using the
+     * {@link Operator#AND} operator.
+     */
+    @Support
+    DivideByOnConditionStep and(Condition condition);
+
+    /**
+     * Combine the currently assembled conditions with another one using the
+     * {@link Operator#AND} operator.
+     * <p>
+     * <b>NOTE</b>: When inserting plain SQL into jOOQ objects, you must
+     * guarantee syntax integrity. You may also create the possibility of
+     * malicious SQL injection. Be sure to properly use bind variables and/or
+     * escape literals when concatenated into SQL clauses!
+     *
+     * @see Factory#condition(String)
+     */
+    @Support
+    DivideByOnConditionStep and(String sql);
+
+    /**
+     * Combine the currently assembled conditions with another one using the
+     * {@link Operator#AND} operator.
+     * <p>
+     * <b>NOTE</b>: When inserting plain SQL into jOOQ objects, you must
+     * guarantee syntax integrity. You may also create the possibility of
+     * malicious SQL injection. Be sure to properly use bind variables and/or
+     * escape literals when concatenated into SQL clauses!
+     *
+     * @see Factory#condition(String, Object...)
+     */
+    @Support
+    DivideByOnConditionStep and(String sql, Object... bindings);
+
+    /**
+     * Combine the currently assembled conditions with a negated other one using
+     * the {@link Operator#AND} operator.
+     */
+    @Support
+    DivideByOnConditionStep andNot(Condition condition);
+
+    /**
+     * Combine the currently assembled conditions with an <code>EXISTS</code>
+     * clause using the {@link Operator#AND} operator.
+     */
+    @Support
+    DivideByOnConditionStep andExists(Select<?> select);
+
+    /**
+     * Combine the currently assembled conditions with a <code>NOT EXISTS</code>
+     * clause using the {@link Operator#AND} operator.
+     */
+    @Support
+    DivideByOnConditionStep andNotExists(Select<?> select);
+
+    /**
+     * Combine the currently assembled conditions with another one using the
+     * {@link Operator#OR} operator.
+     */
+    @Support
+    DivideByOnConditionStep or(Condition condition);
+
+    /**
+     * Combine the currently assembled conditions with another one using the
+     * {@link Operator#OR} operator.
+     * <p>
+     * <b>NOTE</b>: When inserting plain SQL into jOOQ objects, you must
+     * guarantee syntax integrity. You may also create the possibility of
+     * malicious SQL injection. Be sure to properly use bind variables and/or
+     * escape literals when concatenated into SQL clauses!
+     *
+     * @see Factory#condition(String)
+     */
+    @Support
+    DivideByOnConditionStep or(String sql);
+
+    /**
+     * Combine the currently assembled conditions with another one using the
+     * {@link Operator#OR} operator.
+     * <p>
+     * <b>NOTE</b>: When inserting plain SQL into jOOQ objects, you must
+     * guarantee syntax integrity. You may also create the possibility of
+     * malicious SQL injection. Be sure to properly use bind variables and/or
+     * escape literals when concatenated into SQL clauses!
+     *
+     * @see Factory#condition(String, Object...)
+     */
+    @Support
+    DivideByOnConditionStep or(String sql, Object... bindings);
+
+    /**
+     * Combine the currently assembled conditions with a negated other one using
+     * the {@link Operator#OR} operator.
+     */
+    @Support
+    DivideByOnConditionStep orNot(Condition condition);
+
+    /**
+     * Combine the currently assembled conditions with an <code>EXISTS</code>
+     * clause using the {@link Operator#OR} operator.
+     */
+    @Support
+    DivideByOnConditionStep orExists(Select<?> select);
+
+    /**
+     * Combine the currently assembled conditions with a <code>NOT EXISTS</code>
+     * clause using the {@link Operator#OR} operator.
+     */
+    @Support
+    DivideByOnConditionStep orNotExists(Select<?> select);
+}

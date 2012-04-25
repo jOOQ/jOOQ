5b02ea7d79e00eb4a27ace8033f6a207865f180a
Lukas Eder <lukas.eder@gmail.com>
2012-04-25 20:07:46 +0200

    Fixed potential issue with default value for generation directory

--- jOOQ-codegen/src/main/java/org/jooq/util/GenerationTool.java
+++ jOOQ-codegen/src/main/java/org/jooq/util/GenerationTool.java
@@ -362,7 +362,7 @@ public class GenerationTool {
             if (StringUtils.isBlank(g.getTarget().getPackageName()))
                 g.getTarget().setPackageName("org.jooq.generated");
             if (StringUtils.isBlank(g.getTarget().getDirectory()))
-                g.getTarget().setPackageName("target/generated-sources/jooq");
+                g.getTarget().setDirectory("target/generated-sources/jooq");
 
             generator.setTargetPackage(g.getTarget().getPackageName());
             generator.setTargetDirectory(g.getTarget().getDirectory());

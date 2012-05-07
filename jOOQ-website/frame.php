3357b14c0969df1af8c23400cf4cf1f5e4ccbffc
Lukas Eder <lukas.eder@gmail.com>
2012-05-07 21:19:23 +0200

    Added Google Analytics code snippet

--- jOOQ-website/frame.php
+++ jOOQ-website/frame.php
@@ -26,6 +26,19 @@
 		<script type="text/javascript" src="<?=$root?>/js/prettify/prettify.js"></script>
 		<script type="text/javascript" src="<?=$root?>/js/prettify/lang-sql.js"></script>
 		<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+
+		<script type="text/javascript">
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-30716479-1']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+
+		</script>
 	</head>
 	<body onload="prettyPrint()">
 	    <div id="navigation">

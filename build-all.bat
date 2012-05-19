e7ccbad90738ced6d01a29db51d21664e3164a6c
Lukas Eder <lukas.eder@gmail.com>
2012-05-19 14:19:21 +0200

    Created release maven profile to remove gpg signing from regular builds

--- build-all.bat
+++ build-all.bat
@@ -1,23 +1,23 @@
 cd jooq
-call mvn clean %1 %2 %3 %4 %5
+call mvn clean %1 %2 %3 %4 %5 -P release
 cd ..
 
 cd jooq-meta
-call mvn clean %1 %2 %3 %4 %5
+call mvn clean %1 %2 %3 %4 %5 -P release
 cd ..
 
 cd jooq-codegen
-call mvn clean %1 %2 %3 %4 %5
+call mvn clean %1 %2 %3 %4 %5 -P release
 cd ..
 
 cd jooq-codegen-maven
-call mvn clean %1 %2 %3 %4 %5
+call mvn clean %1 %2 %3 %4 %5 -P release
 cd ..
 
 cd jooq-codegen-maven-example
-call mvn clean %1 %2 %3 %4 %5
+call mvn clean %1 %2 %3 %4 %5 -P release
 cd ..
 
 cd jooq-console
-call mvn clean %1 %2 %3 %4 %5
+call mvn clean %1 %2 %3 %4 %5 -P release
 cd ..

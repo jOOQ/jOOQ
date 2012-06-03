2e36d4c6883004d8361d240da5e386442261d164
Lukas Eder <lukas.eder@gmail.com>
2012-06-03 16:04:22 +0200

    Release 2.3.2

--- jOOQ/src/main/java/org/jooq/Constants.java
+++ jOOQ/src/main/java/org/jooq/Constants.java
@@ -45,7 +45,7 @@ public final class Constants {
     /**
      * The latest jOOQ version
      */
-    public static final String VERSION = "2.4.0";
+    public static final String VERSION = "2.3.2";
 
     /**
      * No further instances

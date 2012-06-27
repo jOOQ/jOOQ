2acd6dc05c623e9368d21b0e5894eb007305baa8
Mark <github880520@mstier.de>
2012-06-27 13:07:38 +0200

    added import for Sybase ASE stored procedures; no SQL generation

--- jOOQ-meta/src/main/java/org/jooq/util/ase/ASEDatabase.java
+++ jOOQ-meta/src/main/java/org/jooq/util/ase/ASEDatabase.java
@@ -35,6 +35,7 @@
  */
 package org.jooq.util.ase;
 
+import java.math.BigDecimal;
 import static java.util.Arrays.asList;
 import static org.jooq.impl.Factory.concat;
 import static org.jooq.impl.Factory.field;
@@ -262,9 +263,30 @@ public class ASEDatabase extends AbstractDatabase {
         return result;
     }
 
+    private List<Record> fetchRoutines() {
+        List<Record> result = new ArrayList<Record>();
+
+        for (Record record : create().fetch("sp_help")) {
+            if (asList("stored procedure").contains(record.getValueAsString("Object_type"))) {
+                if (getInputSchemata().contains(record.getValueAsString("Owner"))) {
+                    result.add(record);
+                }
+            }
+        }
+
+        return result;
+    }
+
     @Override
     protected List<RoutineDefinition> getRoutines0() throws SQLException {
         List<RoutineDefinition> result = new ArrayList<RoutineDefinition>();
+        
+        for (Record record : fetchRoutines()) {
+            SchemaDefinition schema = getSchema(record.getValueAsString("Owner"));
+            String name = record.getValueAsString("Name");
+            result.add(new ASERoutineDefinition(schema, null, name, "", null, null, null));
+        }
+
         return result;
     }
 

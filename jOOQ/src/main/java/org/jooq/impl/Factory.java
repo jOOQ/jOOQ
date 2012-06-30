206ec3cde452fb3f0e4fc7ef33754b222269cd19
Lukas Eder <lukas.eder@gmail.com>
2012-06-30 10:13:57 +0200

    Fixed bad refactoring change

--- jOOQ/src/main/java/org/jooq/impl/Factory.java
+++ jOOQ/src/main/java/org/jooq/impl/Factory.java
@@ -6074,14 +6074,14 @@ public class Factory implements FactoryOperations {
     /**
      * Get a default <code>Factory</code> without a {@link Connection}
      */
-    final static FactoryOperations getNewFactory(SQLDialect dialect) {
+    final static Factory getNewFactory(SQLDialect dialect) {
         return getNewFactory(DEFAULT_INSTANCES[dialect.ordinal()]);
     }
 
     /**
      * Get a default <code>Factory</code> without a {@link Connection}
      */
-    final static FactoryOperations getStaticFactory(SQLDialect dialect) {
+    final static Factory getStaticFactory(SQLDialect dialect) {
         return DEFAULT_INSTANCES[dialect.ordinal()];
     }
 

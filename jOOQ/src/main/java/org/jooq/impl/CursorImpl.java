e2b6b8dc9ac3a3ae196a9ee3a52042135a83c4b1
Lukas Eder <lukas.eder@gmail.com>
2012-07-27 13:52:50 +0200

    [#1633] Improve the performance of CursorImpl.CursorIterator by setting Record values by index rather than by field

--- jOOQ/src/main/java/org/jooq/impl/CursorImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/CursorImpl.java
@@ -1287,7 +1287,7 @@ class CursorImpl<R extends Record> implements Cursor<R> {
          */
         private final <T> void setValue(AbstractRecord record, Field<T> field, int index) throws SQLException {
             T value = FieldTypeHelper.getFromResultSet(ctx, field, index + 1);
-            record.setValue(field, new Value<T>(value));
+            record.setValue(index, new Value<T>(value));
         }
 
         @Override

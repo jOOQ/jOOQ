6234f86b3d4a6df620f26b54f1154af4de961bc0
Lukas Eder <lukas.eder@gmail.com>
2012-08-09 17:57:54 +0200

    [#1685] Improve Javadoc of Attachable.attach(). Document how "detaching" works

--- jOOQ/src/main/java/org/jooq/Attachable.java
+++ jOOQ/src/main/java/org/jooq/Attachable.java
@@ -38,21 +38,24 @@ package org.jooq;
 import java.io.Serializable;
 import java.sql.Connection;
 
+import javax.sql.DataSource;
+
 /**
  * An object in jOOQ that can have an underlying {@link Configuration} attached
  * or detached.
  * <p>
  * Detaching an <code>Attachable</code> from its configuration means, that the
- * underlying {@link Connection} is removed. Attaching an
+ * underlying {@link Connection} or {@link DataSource} is removed. Attaching an
  * <code>Attachable</code> to a new <code>Configuration</code> means, that its
- * underlying <code>Connection</code> will be restored.
+ * underlying <code>Connection</code> or <code>DataSource</code> will be
+ * restored.
  * <p>
  * Detaching an <code>Attachable</code> will <b>NOT</b> close the underlying
- * connection!
+ * <code>Connection</code> or <code>DataSource</code>!
  * <p>
- * Attachables are also {@link Serializable}. The underlying Configuration is
- * <code>transient</code>. Serialising an Attachable will always detach it
- * first.
+ * Attachables are also {@link Serializable}. The underlying
+ * <code>Connection</code> or <code>DataSource</code> is <code>transient</code>.
+ * Serialising an Attachable will always detach it first.
  *
  * @author Lukas Eder
  */
@@ -61,6 +64,10 @@ public interface Attachable extends Adapter, Serializable {
 
     /**
      * Attach this object to a new {@link Configuration}
+     *
+     * @param configuration A configuration or <code>null</code>, if you wish to
+     *            detach this <code>Attachable</code> from its previous
+     *            configuration.
      */
     void attach(Configuration configuration);
 }

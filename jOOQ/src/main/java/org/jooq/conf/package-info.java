7ba5bb1beb3621cc259b03905792a8798f4b59d4
Lukas Eder <lukas.eder@gmail.com>
2012-09-01 11:43:45 +0200

    [#1782] Move JAXB bindings out of XSD, in order to support more advanced bindings

--- jOOQ/src/main/java/org/jooq/conf/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * The <code>org.jooq.conf</code> package contains jOOQ's runtime configuration
- * classes
- * <p>
- * This package contains JAXB-annotated configuration classes that can be
- * provided to a {@link org.jooq.impl.Factory} in order to influence jOOQ's
- * runtime behaviour.
- */
-package org.jooq.conf;

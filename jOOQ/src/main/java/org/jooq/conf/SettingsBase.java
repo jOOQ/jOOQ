5960f699dd557cc52c20fbc7114464490f6105c8
Lukas Eder <lukas.eder@gmail.com>
2012-09-02 12:13:06 +0200

    [jOOQ/jOOQ#1484] Let XJC-generated artefacts implement Cloneable

---
+++ jOOQ/src/main/java/org/jooq/conf/SettingsBase.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2009-2012, Lukas Eder, lukas.eder@gmail.com
+ * All rights reserved.
+ *
+ * This software is licensed to you under the Apache License, Version 2.0
+ * (the "License"); You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * . Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * . Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * . Neither the name "jOOQ" nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.jooq.conf;
+
+import java.io.ByteArrayOutputStream;
+import java.io.Serializable;
+
+import javax.xml.bind.JAXB;
+
+/**
+ * This base class is extended by all XJC-generated {@link Settings} classes
+ * <p>
+ * Using such a base class seems to be a lot simpler than depending on any one
+ * of those many JAXB / XJC plugins. Besides, cloning objects through the
+ * standard Java {@link Cloneable} mechanism is around factor 1000x faster than
+ * using {@link Serializable}, and even 10000x faster than using
+ * {@link JAXB#marshal(Object, java.io.OutputStream)}, marshalling a JAXB object
+ * into a {@link ByteArrayOutputStream}.
+ *
+ * @author Lukas Eder
+ */
+abstract class SettingsBase implements Serializable, Cloneable {
+
+    /**
+     * Generated UID
+     */
+    private static final long serialVersionUID = 958655542175990197L;
+
+    @Override
+    public Object clone() {
+        try {
+            return super.clone();
+        }
+        catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

27f9f47020991fca590e6b52463e3f8de940d56c
Lukas Eder <lukas.eder@gmail.com>
2012-10-26 09:54:32 +0200

    Release 2.6.0

--- jOOQ-website/frame.php
+++ jOOQ-website/frame.php
@@ -1,6 +1,6 @@
 <?php
   $root = "";
-  $minorVersion = "2.5";
+  $minorVersion = "2.6";
   $version = $minorVersion . ".0";
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">

14e0ba4a1c4556925ece0f6d8db8bc8a49c9b076
Lukas Eder <lukas.eder@gmail.com>
2012-10-26 20:50:01 +0200

    [jOOQ/jOOQ#1874] Add Record1, Record2, ... Record8 similar to Row1, Row2, ... Row8, to support record type-safety

---
+++ jOOQ/src/main/java/org/jooq/Record5.java
@@ -0,0 +1,118 @@
+/**
+ * Copyright (c) 2009-2012, Lukas Eder, lukas.eder@gmail.com
+ * All rights reserved.
+ *
+ * This software is licensed to you under the Apache License, Version 2.0
+ * (the "License"); You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * . Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * . Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * . Neither the name "jOOQ" nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.jooq;
+
+/**
+ * A model type for a records with degree <code>5</code>
+ *
+ * @see Row5
+ * @author Lukas Eder
+ */
+public interface Record5<T1, T2, T3, T4, T5> extends Record {
+
+    // ------------------------------------------------------------------------
+    // Row value expressions
+    // ------------------------------------------------------------------------
+
+    /**
+     * Get this record's fields as a {@link Row5}
+     */
+    Row5<T1, T2, T3, T4, T5> fieldsRow();
+
+    /**
+     * Get this record's values as a {@link Row5}
+     */
+    Row5<T1, T2, T3, T4, T5> valuesRow();
+
+    // ------------------------------------------------------------------------
+    // Field accessors
+    // ------------------------------------------------------------------------
+
+    /**
+     * Get the first field
+     */
+    Field<T1> field1();
+
+    /**
+     * Get the second field
+     */
+    Field<T2> field2();
+
+    /**
+     * Get the third field
+     */
+    Field<T3> field3();
+
+    /**
+     * Get the fourth field
+     */
+    Field<T4> field4();
+
+    /**
+     * Get the fifth field
+     */
+    Field<T5> field5();
+
+    // ------------------------------------------------------------------------
+    // Value accessors
+    // ------------------------------------------------------------------------
+
+    /**
+     * Get the first value
+     */
+    T1 value1();
+
+    /**
+     * Get the second value
+     */
+    T2 value2();
+
+    /**
+     * Get the third value
+     */
+    T3 value3();
+
+    /**
+     * Get the fourth value
+     */
+    T4 value4();
+
+    /**
+     * Get the fifth value
+     */
+    T5 value5();
+
+}

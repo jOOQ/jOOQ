d40b5234439c7092d539ffd30ee1594cbcde6f53
Lukas Eder <lukas.eder@gmail.com>
2012-10-31 21:58:00 +0100

    [jOOQ/jOOQ#915] Add <T1, T2, ..., T[N]> Table<Record[N]<T1, T2, ..., T[N]>> Factory.values(Row[N]<T1, T2, ..., T[N]>...), to create ad-hoc tables from data

---
+++ jOOQ/src/main/java/org/jooq/impl/Values.java
@@ -0,0 +1,100 @@
+/**
+ * Copyright (c) 2009-2012, Lukas Eder, lukas.eder@gmail.com
+ * All rights reserved.
+ *
+ * This software is licensed to you under the Apache License, Version 2.0
+ * (the "License"); You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * . Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * . Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * . Neither the name "jOOQ" nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.jooq.impl;
+
+import org.jooq.BindContext;
+import org.jooq.Record;
+import org.jooq.RenderContext;
+import org.jooq.Row;
+import org.jooq.Support;
+import org.jooq.Table;
+import org.jooq.exception.DataAccessException;
+
+/**
+ * An implementation for the <code>VALUES(...)</code> table constructor
+ *
+ * @author Lukas Eder
+ */
+class Values<R extends Record> extends AbstractTable<R> {
+
+    /**
+     * Generated UID
+     */
+    private static final long serialVersionUID = -637982217747670311L;
+
+    private final Row[]       rows;
+
+    Values(Row[] rows) {
+        super("values");
+
+        this.rows = rows;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public final Class<? extends R> getRecordType() {
+        return (Class<? extends R>) RecordImpl.class;
+    }
+
+    @Override
+    @Support
+    public final Table<R> as(String alias) {
+        return new TableAlias<R>(this, alias, true);
+    }
+
+    @Override
+    public final void toSQL(RenderContext context) {
+        context.keyword("values");
+
+        String separator = "";
+        for (Row row : rows) {
+            context.sql(separator)
+                   .sql(row);
+
+            separator = ", ";
+        }
+    }
+
+    @Override
+    public final void bind(BindContext context) throws DataAccessException {
+        context.bind(rows);
+    }
+
+    @Override
+    protected final FieldList getFieldList() {
+        return new FieldList(rows[0].getFields());
+    }
+}

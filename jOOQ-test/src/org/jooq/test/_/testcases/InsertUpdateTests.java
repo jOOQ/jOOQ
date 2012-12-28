d4826e8354e59f2225959efd83f4680a7fa2c0d5
Lukas Eder <lukas.eder@gmail.com>
2012-12-28 16:06:11 +0100

    [jOOQ/jOOQ#2055] MySQL's UPDATE [t1] JOIN [t2] syntax can cause syntax errors as column references are not fully qualified - Fix for Firebird

--- jOOQ-test/src/org/jooq/test/_/testcases/InsertUpdateTests.java
+++ jOOQ-test/src/org/jooq/test/_/testcases/InsertUpdateTests.java
@@ -1103,6 +1103,7 @@ extends BaseTest<A, AP, B, S, B2S, BS, L, X, DATE, BOOL, D, T, U, I, IPK, T725, 
         switch (getDialect()) {
             case DB2:
             case DERBY:
+            case FIREBIRD:
             case H2:
             case HSQLDB:
             case ORACLE:

1999c730651e50488917eb010f5b72627d28abb1
Lukas Eder <lukas.eder@gmail.com>
2013-01-02 17:48:06 +0100

    Fixed compiler warning

--- jOOQ/src/main/java/org/jooq/Row10.java
+++ jOOQ/src/main/java/org/jooq/Row10.java
@@ -35,7 +35,6 @@
  */
 package org.jooq;
 
-import static org.jooq.SQLDialect.CUBRID;
 import static org.jooq.SQLDialect.DB2;
 import static org.jooq.SQLDialect.HSQLDB;
 import static org.jooq.SQLDialect.MYSQL;

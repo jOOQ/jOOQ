e7235b6e034c5d27b581639ebec8b4b00bd8d8ba
Lukas Eder <lukas.eder@gmail.com>
2013-01-03 23:04:13 +0100

    [#2052] Add [not]Between[Symmetric]() to Row[N] types - Added fix for MySQL

--- jOOQ/src/main/java/org/jooq/impl/RowBetweenCondition.java
+++ jOOQ/src/main/java/org/jooq/impl/RowBetweenCondition.java
@@ -701,7 +701,7 @@ implements
 
         // These dialects either don't support row value expressions, or they
         // Can't handle row value expressions with the BETWEEN predicate
-        else if (row.getDegree() > 1 && asList(CUBRID, DERBY, FIREBIRD, ORACLE, SQLITE, SQLSERVER, SYBASE).contains(configuration.getDialect())) {
+        else if (row.getDegree() > 1 && asList(CUBRID, DERBY, FIREBIRD, MYSQL, ORACLE, SQLITE, SQLSERVER, SYBASE).contains(configuration.getDialect())) {
             Condition result = r.ge(min).and(r.le(max));
 
             if (not) {

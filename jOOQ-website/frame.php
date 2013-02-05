97ec7a3023e629a485b3eef522625de5dfd431c2
Lukas Eder <lukas.eder@gmail.com>
2013-02-05 11:43:02 +0100

    Release 2.6.2

--- jOOQ-website/frame.php
+++ jOOQ-website/frame.php
@@ -1,7 +1,7 @@
 <?php
   $root = "";
   $minorVersion = "2.6";
-  $version = $minorVersion . ".1";
+  $version = $minorVersion . ".2";
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
--- jOOQ/src/main/java/org/jooq/Constants.java
+++ jOOQ/src/main/java/org/jooq/Constants.java
@@ -45,7 +45,7 @@ public final class Constants {
     /**
      * The latest jOOQ version
      */
-    public static final String VERSION = "2.6.1";
+    public static final String VERSION = "2.6.2";
 
     /**
      * No further instances

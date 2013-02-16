033a167974446c3d2413f3b1523ec7d98ed0504b
Lukas Eder <lukas.eder@gmail.com>
2013-02-16 15:11:21 +0100

    Release 3.0.0-RC1 - Fixed tests against maven-generated resources

--- jOOQ-codegen-maven-example/src/test/java/org/jooq/test/util/maven/TestH2.java
+++ jOOQ-codegen-maven-example/src/test/java/org/jooq/test/util/maven/TestH2.java
@@ -36,54 +36,60 @@
 package org.jooq.test.util.maven;
 
 import static junit.framework.Assert.assertEquals;
+import static org.jooq.impl.Factory.countDistinct;
+import static org.jooq.maven.example.h2.Tables.T_AUTHOR;
+import static org.jooq.maven.example.h2.Tables.T_BOOK;
+import static org.jooq.maven.example.h2.Tables.T_BOOK_STORE;
+import static org.jooq.maven.example.h2.Tables.T_BOOK_TO_BOOK_STORE;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.List;
 
-import org.jooq.Record;
+import org.jooq.Record3;
 import org.jooq.Result;
 import org.jooq.SQLDialect;
-import org.jooq.impl.Factory;
-import org.jooq.util.maven.example.postgres.tables.TBook;
-import org.jooq.util.maven.example.postgres.tables.records.TBookRecord;
+import org.jooq.impl.Executor;
+import org.jooq.maven.example.h2.tables.TAuthor;
+import org.jooq.maven.example.h2.tables.TBook;
+import org.jooq.maven.example.h2.tables.TBookStore;
+import org.jooq.maven.example.h2.tables.TBookToBookStore;
 
 import org.junit.Test;
 
 /**
  * @author Lukas Eder
  */
-public class TestPostgres {
+public class TestH2 {
 
     @Test
-    public void testFetch() throws Exception {
-        Connection connection = DriverManager.getConnection("jdbc:postgresql:postgres", "postgres", "test");
-        Factory create = new Factory(connection, SQLDialect.POSTGRES);
+    public void testInstanceModel() throws Exception {
+        Class.forName("org.h2.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:h2:~/maven-test", "sa", "");
+        Executor create = new Executor(connection, SQLDialect.H2);
 
-        Result<Record> books =
-        create.select(TBook.ID, TBook.TITLE)
-              .from(TBook.T_BOOK)
-              .orderBy(TBook.ID)
+        TBook b = T_BOOK.as("b");
+        TAuthor a = T_AUTHOR.as("a");
+        TBookStore s = T_BOOK_STORE.as("s");
+        TBookToBookStore t = T_BOOK_TO_BOOK_STORE.as("t");
+
+        Result<Record3<String, String, Integer>> result =
+        create.select(a.FIRST_NAME, a.LAST_NAME, countDistinct(s.NAME))
+              .from(a)
+              .join(b).on(b.AUTHOR_ID.equal(a.ID))
+              .join(t).on(t.BOOK_ID.equal(b.ID))
+              .join(s).on(t.BOOK_STORE_NAME.equal(s.NAME))
+              .groupBy(a.FIRST_NAME, a.LAST_NAME)
+              .orderBy(countDistinct(s.NAME).desc())
               .fetch();
 
-        assertEquals(Data.BOOK_IDS, books.getValues(0));
-        assertEquals(Data.BOOK_TITLES, books.getValues(1));
-    }
+        assertEquals(2, result.size());
+        assertEquals("Paulo", result.getValue(0, a.FIRST_NAME));
+        assertEquals("George", result.getValue(1, a.FIRST_NAME));
 
-    @Test
-    public void testFetchInto() throws Exception {
-        Connection connection = DriverManager.getConnection("jdbc:postgresql:postgres", "postgres", "test");
-        Factory create = new Factory(connection, SQLDialect.POSTGRES);
+        assertEquals("Coelho", result.getValue(0, a.LAST_NAME));
+        assertEquals("Orwell", result.getValue(1, a.LAST_NAME));
 
-        List<TBookRecord> books =
-        create.select(TBook.ID, TBook.TITLE)
-              .from(TBook.T_BOOK)
-              .orderBy(TBook.ID)
-              .fetchInto(TBookRecord.class);
-
-        for (int i = 0; i < Data.BOOK_IDS.size(); i++) {
-            assertEquals(Data.BOOK_IDS.get(i), books.get(i).getId());
-            assertEquals(Data.BOOK_TITLES.get(i), books.get(i).getTitle());
-        }
+        assertEquals(Integer.valueOf(3), result.getValue(0, countDistinct(s.NAME)));
+        assertEquals(Integer.valueOf(2), result.getValue(1, countDistinct(s.NAME)));
     }
 }

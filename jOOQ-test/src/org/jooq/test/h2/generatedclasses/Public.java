d66fbbab21152012ccb048fd747c0e0ab4c52891
Lukas Eder <lukas.eder@gmail.com>
2013-02-19 18:53:26 +0100

    [#2212] "code size too large" in generated SchemaImpl, when the number of tables exceeds 15k - Regenerated H2 schema

--- jOOQ-test/src/org/jooq/test/h2/generatedclasses/Public.java
+++ jOOQ-test/src/org/jooq/test/h2/generatedclasses/Public.java
@@ -9,7 +9,7 @@ package org.jooq.test.h2.generatedclasses;
 @java.lang.SuppressWarnings("all")
 public class Public extends org.jooq.impl.SchemaImpl {
 
-	private static final long serialVersionUID = -681131167;
+	private static final long serialVersionUID = 2038109924;
 
 	/**
 	 * The singleton instance of <code>PUBLIC</code>
@@ -25,6 +25,12 @@ public class Public extends org.jooq.impl.SchemaImpl {
 
 	@Override
 	public final java.util.List<org.jooq.Sequence<?>> getSequences() {
+		java.util.List result = new java.util.ArrayList();
+		result.addAll(getSequences0());
+		return result;
+	}
+
+	private final java.util.List<org.jooq.Sequence<?>> getSequences0() {
 		return java.util.Arrays.<org.jooq.Sequence<?>>asList(
 			org.jooq.test.h2.generatedclasses.Sequences.S_AUTHOR_ID,
 			org.jooq.test.h2.generatedclasses.Sequences.S_TRIGGERS_SEQUENCE);
@@ -32,6 +38,12 @@ public class Public extends org.jooq.impl.SchemaImpl {
 
 	@Override
 	public final java.util.List<org.jooq.Table<?>> getTables() {
+		java.util.List result = new java.util.ArrayList();
+		result.addAll(getTables0());
+		return result;
+	}
+
+	private final java.util.List<org.jooq.Table<?>> getTables0() {
 		return java.util.Arrays.<org.jooq.Table<?>>asList(
 			org.jooq.test.h2.generatedclasses.tables.TIdentityPk.T_IDENTITY_PK,
 			org.jooq.test.h2.generatedclasses.tables.TIdentity.T_IDENTITY,

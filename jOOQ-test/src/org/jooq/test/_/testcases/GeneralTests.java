f97f81a43ca84ffee1d47318b7824294d4998e2a
Lukas Eder <lukas.eder@gmail.com>
2013-03-22 12:37:54 +0100

    [#2345] Singleton instances are not preserved after deserialisation - Reverted adding of test case

--- jOOQ-test/src/org/jooq/test/_/testcases/GeneralTests.java
+++ jOOQ-test/src/org/jooq/test/_/testcases/GeneralTests.java
@@ -61,8 +61,6 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.util.Arrays;
 
-import junit.framework.Assert;
-
 import org.jooq.ExecuteContext;
 import org.jooq.Field;
 import org.jooq.Record;
@@ -264,11 +262,6 @@ extends BaseTest<A, AP, B, S, B2S, BS, L, X, DATE, BOOL, D, T, U, UU, I, IPK, T7
             s = create().select(SAuthorID().nextval(), SAuthorID().currval());
             s = runSerialisation(s);
         }
-    }
-
-    @Test
-    public void testSerialisationOfSingletons() throws Exception {
-        Assert.assertSame(TBook(), runSerialisation(TBook()));
     }
 
     public static class ConnectionProviderListener extends DefaultExecuteListener {

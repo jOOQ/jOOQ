ec4283f169489d6d998246b2546545f2e9ee9a4c
Lukas Eder <lukas.eder@gmail.com>
2013-04-03 21:25:03 +0200

    [#2366] Add org.jooq.util.example package to jOOQ-codegen with some example GeneratorStrategies

--- jOOQ-test/examples/org/jooq/examples/mysql/sakila/Tables.java
+++ jOOQ-test/examples/org/jooq/examples/mysql/sakila/Tables.java
@@ -14,115 +14,115 @@ public class Tables {
 	/**
 	 * The table sakila.actor
 	 */
-	public static final org.jooq.examples.mysql.sakila.tables.Actor ACTOR = org.jooq.examples.mysql.sakila.tables.Actor.ACTOR;
+	public static final org.jooq.examples.mysql.sakila.tables.SakilaActor ACTOR = org.jooq.examples.mysql.sakila.tables.SakilaActor.ACTOR;
 
 	/**
 	 * VIEW
 	 */
-	public static final org.jooq.examples.mysql.sakila.tables.ActorInfo ACTOR_INFO = org.jooq.examples.mysql.sakila.tables.ActorInfo.ACTOR_INFO;
+	public static final org.jooq.examples.mysql.sakila.tables.SakilaActorInfo ACTOR_INFO = org.jooq.examples.mysql.sakila.tables.SakilaActorInfo.ACTOR_INFO;
 
 	/**
 	 * The table sakila.address
 	 */
-	public static final org.jooq.examples.mysql.sakila.tables.Address ADDRESS = org.jooq.examples.mysql.sakila.tables.Address.ADDRESS;
+	public static final org.jooq.examples.mysql.sakila.tables.SakilaAddress ADDRESS = org.jooq.examples.mysql.sakila.tables.SakilaAddress.ADDRESS;
 
 	/**
 	 * The table sakila.category
 	 */
-	public static final org.jooq.examples.mysql.sakila.tables.Category CATEGORY = org.jooq.examples.mysql.sakila.tables.Category.CATEGORY;
+	public static final org.jooq.examples.mysql.sakila.tables.SakilaCategory CATEGORY = org.jooq.examples.mysql.sakila.tables.SakilaCategory.CATEGORY;
 
 	/**
 	 * The table sakila.city
 	 */
-	public static final org.jooq.examples.mysql.sakila.tables.City CITY = org.jooq.examples.mysql.sakila.tables.City.CITY;
+	public static final org.jooq.examples.mysql.sakila.tables.SakilaCity CITY = org.jooq.examples.mysql.sakila.tables.SakilaCity.CITY;
 
 	/**
 	 * The table sakila.country
 	 */
-	public static final org.jooq.examples.mysql.sakila.tables.Country COUNTRY = org.jooq.examples.mysql.sakila.tables.Country.COUNTRY;
+	public static final org.jooq.examples.mysql.sakila.tables.SakilaCountry COUNTRY = org.jooq.examples.mysql.sakila.tables.SakilaCountry.COUNTRY;
 
 	/**
 	 * The table sakila.customer
 	 */
-	public static final org.jooq.examples.mysql.sakila.tables.Customer CUSTOMER = org.jooq.examples.mysql.sakila.tables.Customer.CUSTOMER;
+	public static final org.jooq.examples.mysql.sakila.tables.SakilaCustomer CUSTOMER = org.jooq.examples.mysql.sakila.tables.SakilaCustomer.CUSTOMER;
 
 	/**
 	 * VIEW
 	 */
-	public static final org.jooq.examples.mysql.sakila.tables.CustomerList CUSTOMER_LIST = org.jooq.examples.mysql.sakila.tables.CustomerList.CUSTOMER_LIST;
+	public static final org.jooq.examples.mysql.sakila.tables.SakilaCustomerList CUSTOMER_LIST = org.jooq.examples.mysql.sakila.tables.SakilaCustomerList.CUSTOMER_LIST;
 
 	/**
 	 * The table sakila.film
 	 */
-	public static final org.jooq.examples.mysql.sakila.tables.Film FILM = org.jooq.examples.mysql.sakila.tables.Film.FILM;
+	public static final org.jooq.examples.mysql.sakila.tables.SakilaFilm FILM = org.jooq.examples.mysql.sakila.tables.SakilaFilm.FILM;
 
 	/**
 	 * The table sakila.film_actor
 	 */
-	public static final org.jooq.examples.mysql.sakila.tables.FilmActor FILM_ACTOR = org.jooq.examples.mysql.sakila.tables.FilmActor.FILM_ACTOR;
+	public static final org.jooq.examples.mysql.sakila.tables.SakilaFilmActor FILM_ACTOR = org.jooq.examples.mysql.sakila.tables.SakilaFilmActor.FILM_ACTOR;
 
 	/**
 	 * The table sakila.film_category
 	 */
-	public static final org.jooq.examples.mysql.sakila.tables.FilmCategory FILM_CATEGORY = org.jooq.examples.mysql.sakila.tables.FilmCategory.FILM_CATEGORY;
+	public static final org.jooq.examples.mysql.sakila.tables.SakilaFilmCategory FILM_CATEGORY = org.jooq.examples.mysql.sakila.tables.SakilaFilmCategory.FILM_CATEGORY;
 
 	/**
 	 * VIEW
 	 */
-	public static final org.jooq.examples.mysql.sakila.tables.FilmList FILM_LIST = org.jooq.examples.mysql.sakila.tables.FilmList.FILM_LIST;
+	public static final org.jooq.examples.mysql.sakila.tables.SakilaFilmList FILM_LIST = org.jooq.examples.mysql.sakila.tables.SakilaFilmList.FILM_LIST;
 
 	/**
 	 * The table sakila.film_text
 	 */
-	public static final org.jooq.examples.mysql.sakila.tables.FilmText FILM_TEXT = org.jooq.examples.mysql.sakila.tables.FilmText.FILM_TEXT;
+	public static final org.jooq.examples.mysql.sakila.tables.SakilaFilmText FILM_TEXT = org.jooq.examples.mysql.sakila.tables.SakilaFilmText.FILM_TEXT;
 
 	/**
 	 * The table sakila.inventory
 	 */
-	public static final org.jooq.examples.mysql.sakila.tables.Inventory INVENTORY = org.jooq.examples.mysql.sakila.tables.Inventory.INVENTORY;
+	public static final org.jooq.examples.mysql.sakila.tables.SakilaInventory INVENTORY = org.jooq.examples.mysql.sakila.tables.SakilaInventory.INVENTORY;
 
 	/**
 	 * The table sakila.language
 	 */
-	public static final org.jooq.examples.mysql.sakila.tables.Language LANGUAGE = org.jooq.examples.mysql.sakila.tables.Language.LANGUAGE;
+	public static final org.jooq.examples.mysql.sakila.tables.SakilaLanguage LANGUAGE = org.jooq.examples.mysql.sakila.tables.SakilaLanguage.LANGUAGE;
 
 	/**
 	 * VIEW
 	 */
-	public static final org.jooq.examples.mysql.sakila.tables.NicerButSlowerFilmList NICER_BUT_SLOWER_FILM_LIST = org.jooq.examples.mysql.sakila.tables.NicerButSlowerFilmList.NICER_BUT_SLOWER_FILM_LIST;
+	public static final org.jooq.examples.mysql.sakila.tables.SakilaNicerButSlowerFilmList NICER_BUT_SLOWER_FILM_LIST = org.jooq.examples.mysql.sakila.tables.SakilaNicerButSlowerFilmList.NICER_BUT_SLOWER_FILM_LIST;
 
 	/**
 	 * The table sakila.payment
 	 */
-	public static final org.jooq.examples.mysql.sakila.tables.Payment PAYMENT = org.jooq.examples.mysql.sakila.tables.Payment.PAYMENT;
+	public static final org.jooq.examples.mysql.sakila.tables.SakilaPayment PAYMENT = org.jooq.examples.mysql.sakila.tables.SakilaPayment.PAYMENT;
 
 	/**
 	 * The table sakila.rental
 	 */
-	public static final org.jooq.examples.mysql.sakila.tables.Rental RENTAL = org.jooq.examples.mysql.sakila.tables.Rental.RENTAL;
+	public static final org.jooq.examples.mysql.sakila.tables.SakilaRental RENTAL = org.jooq.examples.mysql.sakila.tables.SakilaRental.RENTAL;
 
 	/**
 	 * VIEW
 	 */
-	public static final org.jooq.examples.mysql.sakila.tables.SalesByFilmCategory SALES_BY_FILM_CATEGORY = org.jooq.examples.mysql.sakila.tables.SalesByFilmCategory.SALES_BY_FILM_CATEGORY;
+	public static final org.jooq.examples.mysql.sakila.tables.SakilaSalesByFilmCategory SALES_BY_FILM_CATEGORY = org.jooq.examples.mysql.sakila.tables.SakilaSalesByFilmCategory.SALES_BY_FILM_CATEGORY;
 
 	/**
 	 * VIEW
 	 */
-	public static final org.jooq.examples.mysql.sakila.tables.SalesByStore SALES_BY_STORE = org.jooq.examples.mysql.sakila.tables.SalesByStore.SALES_BY_STORE;
+	public static final org.jooq.examples.mysql.sakila.tables.SakilaSalesByStore SALES_BY_STORE = org.jooq.examples.mysql.sakila.tables.SakilaSalesByStore.SALES_BY_STORE;
 
 	/**
 	 * The table sakila.staff
 	 */
-	public static final org.jooq.examples.mysql.sakila.tables.Staff STAFF = org.jooq.examples.mysql.sakila.tables.Staff.STAFF;
+	public static final org.jooq.examples.mysql.sakila.tables.SakilaStaff STAFF = org.jooq.examples.mysql.sakila.tables.SakilaStaff.STAFF;
 
 	/**
 	 * VIEW
 	 */
-	public static final org.jooq.examples.mysql.sakila.tables.StaffList STAFF_LIST = org.jooq.examples.mysql.sakila.tables.StaffList.STAFF_LIST;
+	public static final org.jooq.examples.mysql.sakila.tables.SakilaStaffList STAFF_LIST = org.jooq.examples.mysql.sakila.tables.SakilaStaffList.STAFF_LIST;
 
 	/**
 	 * The table sakila.store
 	 */
-	public static final org.jooq.examples.mysql.sakila.tables.Store STORE = org.jooq.examples.mysql.sakila.tables.Store.STORE;
+	public static final org.jooq.examples.mysql.sakila.tables.SakilaStore STORE = org.jooq.examples.mysql.sakila.tables.SakilaStore.STORE;
 }

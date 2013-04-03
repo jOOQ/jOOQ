fe22d7b8d0c0a1d5e9d8ce29c62124295e502ff7
Lukas Eder <lukas.eder@gmail.com>
2013-04-03 21:15:50 +0200

    [#2366] Add org.jooq.util.example package to jOOQ-codegen with some example GeneratorStrategies

---
+++ jOOQ-codegen/src/main/java/org/jooq/util/example/JPrefixGeneratorStrategy.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2009-2013, Lukas Eder, lukas.eder@gmail.com
+ * All rights reserved.
+ *
+ * This software is licensed to you under the Apache License, Version 2.0
+ * (the "License"); You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * . Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * . Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * . Neither the name "jOOQ" nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.jooq.util.example;
+
+import org.jooq.util.DefaultGeneratorStrategy;
+import org.jooq.util.Definition;
+
+/**
+ * A generator strategy that prefixes class names with a letter <code>J</code>.
+ * <p>
+ * An example variant of the default naming strategy
+ * {@link DefaultGeneratorStrategy} adding the prefix 'J' to generated class
+ * names (e.g. to avoid name clashes with existing JPA entities and such).
+ * <p>
+ * This strategy is to be understood as a working example, not part of the code
+ * generation library. It may be modified / adapted in the future. Use at your
+ * own risk.
+ *
+ * @author Peter Ertl
+ * @author Lukas Eder
+ */
+public class JPrefixGeneratorStrategy extends DefaultGeneratorStrategy {
+
+    @Override
+    public String getJavaClassName(final Definition definition, final Mode mode) {
+        return 'J' + super.getJavaClassName(definition, mode);
+    }
+}

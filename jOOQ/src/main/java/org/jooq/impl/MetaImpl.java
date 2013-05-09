562f2a72d531a06bfdadcaac8daf770aa106b51c
Lukas Eder <lukas.eder@gmail.com>
2013-05-09 14:24:34 +0200

    [jOOQ/jOOQ#2448] DSLContext.meta() returns Table objects for Oracle SYNONYMS, without providing columns

--- jOOQ/src/main/java/org/jooq/impl/MetaImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/MetaImpl.java
@@ -217,9 +217,11 @@ class MetaImpl implements Meta, Serializable {
                         types = new String[] { "TABLE", "VIEW" };
                         break;
 
-                    // [#2448] Avoid returning Oracle table SYNONYMs
+                    // [#2448] Avoid returning Oracle table SYNONYMs.
+                    // Note: "MATERIALIZED VIEW" is not included, as they are also
+                    // returned as "TABLE" by Oracle JDBC
                     case ORACLE:
-                        types = new String[] { "TABLE", "VIEW", "MATERIALIZED VIEW" };
+                        types = new String[] { "TABLE", "VIEW" };
                         break;
                 }
 

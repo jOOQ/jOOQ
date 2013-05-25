100f981ee3d6e59948b6aa278bfdd8f9203940bf
Lukas Eder <lukas.eder@gmail.com>
2013-05-25 15:37:27 +0200

    Release 3.0.1

--- jOOQ/src/main/java/org/jooq/Constants.java
+++ jOOQ/src/main/java/org/jooq/Constants.java
@@ -53,7 +53,7 @@ public final class Constants {
      * This is the same as {@link #MINOR_VERSION}, but it may include patch
      * version suffixes.
      */
-    public static final String VERSION        = "3.0.0";
+    public static final String VERSION        = "3.0.1";
 
     /**
      * The latest jOOQ full version.
@@ -61,7 +61,7 @@ public final class Constants {
      * This is the same as {@link #VERSION}, but it may include release
      * candidate and other suffixes.
      */
-    public static final String FULL_VERSION   = "3.0.0";
+    public static final String FULL_VERSION   = "3.0.1";
 
     /**
      * The maximum degree of {@link Row} and {@link Record} subtypes

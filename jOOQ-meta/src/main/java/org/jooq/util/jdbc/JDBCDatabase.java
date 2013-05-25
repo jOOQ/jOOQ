d41591eecc8409b99f762d181f413362e87b9a7a
Lukas Eder <lukas.eder@gmail.com>
2013-05-25 14:07:24 +0200

    [jOOQ/jOOQ#2445] Merged some 3.1 API

--- jOOQ-meta/src/main/java/org/jooq/util/jdbc/JDBCDatabase.java
+++ jOOQ-meta/src/main/java/org/jooq/util/jdbc/JDBCDatabase.java
@@ -36,15 +36,33 @@
 
 package org.jooq.util.jdbc;
 
+import static org.jooq.SQLDialect.ASE;
+import static org.jooq.SQLDialect.CUBRID;
+import static org.jooq.SQLDialect.DB2;
+import static org.jooq.SQLDialect.DERBY;
+import static org.jooq.SQLDialect.FIREBIRD;
+import static org.jooq.SQLDialect.H2;
+import static org.jooq.SQLDialect.HSQLDB;
+import static org.jooq.SQLDialect.INGRES;
+import static org.jooq.SQLDialect.MYSQL;
+import static org.jooq.SQLDialect.ORACLE;
+import static org.jooq.SQLDialect.POSTGRES;
+import static org.jooq.SQLDialect.SQLITE;
+import static org.jooq.SQLDialect.SQLSERVER;
+import static org.jooq.SQLDialect.SYBASE;
+
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
 import org.jooq.Schema;
 import org.jooq.Sequence;
 import org.jooq.Table;
 import org.jooq.impl.DSL;
+import org.jooq.tools.jdbc.JDBCUtils;
 import org.jooq.util.AbstractDatabase;
 import org.jooq.util.ArrayDefinition;
 import org.jooq.util.DataTypeDefinition;
@@ -70,7 +88,88 @@ public class JDBCDatabase extends AbstractDatabase {
 
     @Override
     protected DSLContext create0() {
-        return DSL.using(getConnection());
+        Connection connection = getConnection();
+        return DSL.using(connection, dialect(connection));
+    }
+
+    /**
+     * Method merged from jOOQ 3.1's {@link JDBCUtils}
+     */
+    @SuppressWarnings("deprecation")
+    static final SQLDialect dialect(Connection connection) {
+        SQLDialect result = SQLDialect.SQL99;
+
+        try {
+            String url = connection.getMetaData().getURL();
+            result = dialect(url);
+        }
+        catch (SQLException ignore) {}
+
+        if (result == SQLDialect.SQL99) {
+            // If the dialect cannot be guessed from the URL, take some other
+            // measures, e.g. by querying DatabaseMetaData.getDatabaseProductName()
+        }
+
+        return result;
+    }
+
+    /**
+     * Method merged from jOOQ 3.1's {@link JDBCUtils}
+     */
+    @SuppressWarnings("deprecation")
+    static final SQLDialect dialect(String url) {
+
+        // The below list might not be accurate or complete. Feel free to
+        // contribute fixes related to new / different JDBC driver configuraitons
+        if (url.startsWith("jdbc:jtds:sybase:")) {
+            return ASE;
+        }
+        else if (url.startsWith("jdbc:cubrid:")) {
+            return CUBRID;
+        }
+        else if (url.startsWith("jdbc:db2:")) {
+            return DB2;
+        }
+        else if (url.startsWith("jdbc:derby:")) {
+            return DERBY;
+        }
+        else if (url.startsWith("jdbc:firebirdsql:")) {
+            return FIREBIRD;
+        }
+        else if (url.startsWith("jdbc:h2:")) {
+            return H2;
+        }
+        else if (url.startsWith("jdbc:hsqldb:")) {
+            return HSQLDB;
+        }
+        else if (url.startsWith("jdbc:ingres:")) {
+            return INGRES;
+        }
+        else if (url.startsWith("jdbc:mysql:")
+              || url.startsWith("jdbc:google:")) {
+            return MYSQL;
+        }
+        else if (url.startsWith("jdbc:oracle:")
+              || url.startsWith("jdbc:oracle:oci")) {
+            return ORACLE;
+        }
+        else if (url.startsWith("jdbc:postgresql:")) {
+            return POSTGRES;
+        }
+        else if (url.startsWith("jdbc:sqlite:")) {
+            return SQLITE;
+        }
+        else if (url.startsWith("jdbc:sqlserver:")
+              || url.startsWith("jdbc:jtds:sqlserver:")
+              || url.startsWith("jdbc:microsoft:sqlserver:")
+              || url.contains(":mssql")) {
+            return SQLSERVER;
+        }
+        else if (url.startsWith("jdbc:sybase:")) {
+            return SYBASE;
+        }
+
+        return SQLDialect.SQL99;
     }
 
     @Override

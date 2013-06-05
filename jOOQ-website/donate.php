87a7634d773065a450d4667aa69990dc8df98c81
Lukas Eder <lukas.eder@gmail.com>
2013-06-05 09:53:38 +0200

    Removed shirt city link

--- jOOQ-website/donate.php
+++ jOOQ-website/donate.php
@@ -28,14 +28,6 @@ You might have other ideas about features that could be funded in jOOQ. For more
 </p>
 
 
-<h2>Get your geeky jOOQ t-shirt or mug from the shop</h2>
-<p>
-Get some of the jOOQ web shop's fancy merchandise and donate between 7&euro; and 12&euro; with your purchase. Happy shopping!
-</p>
-<p>
-<a href="http://www.shirtcity.com/shop/jooq/" title="The jOOQ t-shirt and mug shop for true SQL nerds">http://www.shirtcity.com/shop/jooq/</a>
-</p>
-
 <h2>Donate directly through SourceForge / PayPal</h2>
 <p>
 If you don't need any merchandise, or if you want to donate a bigger amount, feel free to do so directly here:

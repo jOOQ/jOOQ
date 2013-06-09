4b770fae50f3f920c93198af9d76f63f91859135
Lukas Eder <lukas.eder@gmail.com>
2013-06-09 16:01:15 +0200

    [#2506] SQLDialectNotSupportedException on DSL.inline(T, Class), when jOOQ's internals are not (yet) properly initialised

---
+++ jOOQ/src/test/java/org/jooq/test/init/SeparateClassloaderTestRunner.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2009-2013, Lukas Eder, lukas.eder@gmail.com
+ * All rights reserved.
+ *
+ * This software is licensed to you under the Apache License, Version 2.0
+ * (the "License"); You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * . Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * . Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * . Neither the name "jOOQ" nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.jooq.test.init;
+
+import java.net.URLClassLoader;
+
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runners.model.InitializationError;
+
+/**
+ * A test runner used for running tests in a separate class loader.
+ * <p>
+ * Inspiration was taken from here: <a
+ * href="http://stackoverflow.com/a/9192126/521799"
+ * >http://stackoverflow.com/a/9192126/521799</a>
+ *
+ * @author Lukas Eder
+ */
+public class SeparateClassloaderTestRunner extends BlockJUnit4ClassRunner {
+
+    public SeparateClassloaderTestRunner(Class<?> clazz) throws InitializationError {
+        super(getFromTestClassloader(clazz));
+    }
+
+    private static Class<?> getFromTestClassloader(Class<?> clazz) throws InitializationError {
+        try {
+            ClassLoader testClassLoader = new TestClassLoader();
+            return Class.forName(clazz.getName(), true, testClassLoader);
+        }
+        catch (ClassNotFoundException e) {
+            throw new InitializationError(e);
+        }
+    }
+
+    public static class TestClassLoader extends URLClassLoader {
+        public TestClassLoader() {
+            super(((URLClassLoader) getSystemClassLoader()).getURLs());
+        }
+
+        @Override
+        public Class<?> loadClass(String name) throws ClassNotFoundException {
+            if (name.startsWith("org.jooq.")) {
+                return super.findClass(name);
+            }
+            return super.loadClass(name);
+        }
+    }
+}

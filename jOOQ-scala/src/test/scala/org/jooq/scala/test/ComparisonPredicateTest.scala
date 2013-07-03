4010ffc18b65ec93342b6293ffc720c49dd279c3
Lukas Eder <lukas.eder@gmail.com>
2013-07-03 11:40:49 +0200

    [#2596] Scala tests don't run with Maven

--- jOOQ-scala/src/test/scala/org/jooq/scala/test/ComparisonPredicateTest.scala
+++ jOOQ-scala/src/test/scala/org/jooq/scala/test/ComparisonPredicateTest.scala
@@ -9,7 +9,10 @@ import org.jooq.scala.Conversions._
 import org.jooq.conf.Settings
 import javax.xml.bind.JAXB
 import org.jooq.conf.SettingsTools
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
 
+@RunWith(classOf[JUnitRunner])
 class ComparisonPredicateTest extends FunSuite {
 
   test("comparison predicate") {
@@ -28,27 +31,27 @@ class ComparisonPredicateTest extends FunSuite {
     val le1 = T_BOOK.ID <= T_BOOK.AUTHOR_ID
     val le2 = T_BOOK.ID <= 1
 
-    assert("t_book.id = t_book.author_id"  == eq1.toString(), eq1.toString())
-    assert("t_book.id = 1"                 == eq2.toString(), eq2.toString())
-    assert("t_book.id <> t_book.author_id" == ne1.toString(), ne1.toString())
-    assert("t_book.id <> 1"                == ne2.toString(), ne2.toString())
-    assert("t_book.id <> t_book.author_id" == ne3.toString(), ne3.toString())
-    assert("t_book.id <> 1"                == ne4.toString(), ne4.toString())
-    assert("t_book.id > t_book.author_id"  == gt1.toString(), gt1.toString())
-    assert("t_book.id > 1"                 == gt2.toString(), gt2.toString())
-    assert("t_book.id >= t_book.author_id" == ge1.toString(), ge1.toString())
-    assert("t_book.id >= 1"                == ge2.toString(), ge2.toString())
-    assert("t_book.id < t_book.author_id"  == lt1.toString(), lt1.toString())
-    assert("t_book.id < 1"                 == lt2.toString(), lt2.toString())
-    assert("t_book.id <= t_book.author_id" == le1.toString(), le1.toString())
-    assert("t_book.id <= 1"                == le2.toString(), le2.toString())
+    assert(""""PUBLIC"."T_BOOK"."ID" = "PUBLIC"."T_BOOK"."AUTHOR_ID""""  == eq1.toString(), eq1.toString())
+    assert(""""PUBLIC"."T_BOOK"."ID" = 1"""                              == eq2.toString(), eq2.toString())
+    assert(""""PUBLIC"."T_BOOK"."ID" <> "PUBLIC"."T_BOOK"."AUTHOR_ID"""" == ne1.toString(), ne1.toString())
+    assert(""""PUBLIC"."T_BOOK"."ID" <> 1"""                             == ne2.toString(), ne2.toString())
+    assert(""""PUBLIC"."T_BOOK"."ID" <> "PUBLIC"."T_BOOK"."AUTHOR_ID"""" == ne3.toString(), ne3.toString())
+    assert(""""PUBLIC"."T_BOOK"."ID" <> 1"""                             == ne4.toString(), ne4.toString())
+    assert(""""PUBLIC"."T_BOOK"."ID" > "PUBLIC"."T_BOOK"."AUTHOR_ID""""  == gt1.toString(), gt1.toString())
+    assert(""""PUBLIC"."T_BOOK"."ID" > 1"""                              == gt2.toString(), gt2.toString())
+    assert(""""PUBLIC"."T_BOOK"."ID" >= "PUBLIC"."T_BOOK"."AUTHOR_ID"""" == ge1.toString(), ge1.toString())
+    assert(""""PUBLIC"."T_BOOK"."ID" >= 1"""                             == ge2.toString(), ge2.toString())
+    assert(""""PUBLIC"."T_BOOK"."ID" < "PUBLIC"."T_BOOK"."AUTHOR_ID""""  == lt1.toString(), lt1.toString())
+    assert(""""PUBLIC"."T_BOOK"."ID" < 1"""                              == lt2.toString(), lt2.toString())
+    assert(""""PUBLIC"."T_BOOK"."ID" <= "PUBLIC"."T_BOOK"."AUTHOR_ID"""" == le1.toString(), le1.toString())
+    assert(""""PUBLIC"."T_BOOK"."ID" <= 1"""                             == le2.toString(), le2.toString())
   }
 
   test("distinct predicate") {
     val d1 = T_BOOK.ID <=> T_BOOK.AUTHOR_ID
     val d2 = T_BOOK.ID <=> 1
 
-    assert("t_book.id IS NOT DISTINCT FROM t_book.author_id" == d1.toString(), d1.toString())
-    assert("t_book.id IS NOT DISTINCT FROM 1"                == d2.toString(), d2.toString())
+    assert(""""PUBLIC"."T_BOOK"."ID" is not distinct from "PUBLIC"."T_BOOK"."AUTHOR_ID"""" == d1.toString(), d1.toString())
+    assert(""""PUBLIC"."T_BOOK"."ID" is not distinct from 1"""                             == d2.toString(), d2.toString())
   }
 }

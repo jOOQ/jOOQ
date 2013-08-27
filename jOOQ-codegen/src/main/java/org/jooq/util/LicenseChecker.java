9b5291c665c8e215bbb843a3531b0259c01c73dc
Lukas Eder <lukas.eder@gmail.com>
2013-08-27 14:41:32 +0200

    New Licensor: Data Geekery GmbH
    - Transferred license from Lukas Eder to Data Geekery GmbH
    - Changed license from ASL 2.0 to
      - LGPL + Commercial for jOOQ
      - AGPL + Commercial for jOOQ-codegen, jOOQ-meta, jOOQ-scala and others

---
+++ jOOQ-codegen/src/main/java/org/jooq/util/LicenseChecker.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2009-2013, Data Geekery GmbH (http://www.datageekery.com)
+ * All rights reserved.
+ * 
+ * This work is dual-licensed Open Source, under AGPL and jOOQ EULA
+ * =============================================================================
+ * You may freely choose which license applies to you. For more information 
+ * about licensing, please visit http://www.jooq.org/licenses
+ *
+ * AGPL:  
+ * -----------------------------------------------------------------------------
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public
+ * License as published by the Free Software Foundation; either 
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public 
+ * License along with this library.
+ * If not, see http://www.gnu.org/licenses.
+ * 
+ * jOOQ End User License Agreement:
+ * -----------------------------------------------------------------------------
+ * This library is commercial software; you may not redistribute it and/or
+ * modify it.
+ * 
+ * This library is distributed with a LIMITED WARRANTY. See the jOOQ End User
+ * License Agreement for more details.
+ * 
+ * You should have received a copy of the jOOQ End User License Agreement
+ * along with this library.
+ * If not, see http://www.jooq.org/eula
+ */
+package org.jooq.util;
+
+import org.jooq.tools.JooqLogger;
+import org.jooq.util.jaxb.License;
+
+/**
+ * @author Lukas Eder
+ */
+public class LicenseChecker {
+
+    private static final JooqLogger log = JooqLogger.getLogger(LicenseChecker.class);
+
+    // We at Data Geekery GmbH believe in honesty and Open Source. It is
+    // hard to provide you with Open Source AND prevent piracy. At the same
+    // time, it is hard to prevent piracy without annoying honest customers.
+    // THAT IS WHY WE BELIEVE THAT YOU WILL AGREE NOT TO REMOVE THESE LINES.
+    public static boolean isValid(License license) {
+
+        // TODO: Implement this
+        return false;
+    }
+}
--- jOOQ-scala/src/test/scala/org/jooq/scala/test/ArithmeticExpressionTest.scala
+++ jOOQ-scala/src/test/scala/org/jooq/scala/test/ArithmeticExpressionTest.scala
@@ -1,3 +1,40 @@
+/**
+ * Copyright (c) 2009-2013, Data Geekery GmbH (http://www.datageekery.com)
+ * All rights reserved.
+ * 
+ * This work is dual-licensed Open Source, under LGPL and jOOQ EULA
+ * =============================================================================
+ * You may freely choose which license applies to you. For more information
+ * about licensing, please visit http://www.jooq.org/licenses
+ * 
+ * LGPL:
+ * -----------------------------------------------------------------------------
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ * If not, see http://www.gnu.org/licenses.
+ * 
+ * jOOQ End User License Agreement:
+ * -----------------------------------------------------------------------------
+ * This library is commercial software; you may not redistribute it nor
+ * modify it.
+ * 
+ * This library is distributed with a LIMITED WARRANTY. See the jOOQ End User
+ * License Agreement for more details.
+ * 
+ * You should have received a copy of the jOOQ End User License Agreement
+ * along with this library.
+ * If not, see http://www.jooq.org/eula
+ */
 package org.jooq.scala.test
 
 import collection.JavaConversions._

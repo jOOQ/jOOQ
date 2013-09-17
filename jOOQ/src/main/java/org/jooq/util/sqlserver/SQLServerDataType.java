4c979a5e3ce66040842107392a18004510f9018a
Lukas Eder <lukas.eder@gmail.com>
2013-09-17 12:00:50 +0200

    Settle for final multi-licensing model: ASL 2.0 / Commercial

--- jOOQ/src/main/java/org/jooq/util/sqlserver/SQLServerDataType.java
+++ jOOQ/src/main/java/org/jooq/util/sqlserver/SQLServerDataType.java
@@ -2,14 +2,16 @@
  * Copyright (c) 2009-2013, Data Geekery GmbH (http://www.datageekery.com)
  * All rights reserved.
  *
- * This work is triple-licensed under ASL 2.0, AGPL 3.0, and jOOQ EULA
+ * This work is dual-licensed
+ * - under the Apache Software License 2.0 (the "ASL")
+ * - under the jOOQ License and Maintenance Agreement (the "jOOQ License")
  * =============================================================================
  * You may choose which license applies to you:
  *
  * - If you're using this work with Open Source databases, you may choose
- *   ASL 2.0 or jOOQ EULA.
- * - If you're using this work with at least one commercial database, you may
- *   choose AGPL 3.0 or jOOQ EULA.
+ *   either ASL or jOOQ License.
+ * - If you're using this work with at least one commercial database, you must
+ *   choose jOOQ License
  *
  * For more information, please visit http://www.jooq.org/licenses
  *
@@ -27,29 +29,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * AGPL 3.0
+ * jOOQ License and Maintenance Agreement:
  * -----------------------------------------------------------------------------
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation; either
- * version 3.0 of the License, or (at your option) any later version.
+ * Data Geekery grants the Customer the non-exclusive, timely limited and
+ * non-transferable license to install and use the Software under the terms of
+ * the jOOQ License and Maintenance Agreement.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public
- * License along with this library.
- * If not, see http://www.gnu.org/licenses.
- *
- * jOOQ End User License Agreement:
- * -----------------------------------------------------------------------------
- * This library is commercial software; you may not redistribute it nor
- * modify it.
- *
- * This library is distributed with a LIMITED WARRANTY. See the jOOQ End User
- * License Agreement for more details: http://www.jooq.org/eula
+ * This library is distributed with a LIMITED WARRANTY. See the jOOQ License
+ * and Maintenance Agreement for more details: http://www.jooq.org/eula
  */
 package org.jooq.util.sqlserver;
 

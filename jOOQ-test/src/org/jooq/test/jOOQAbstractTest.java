4f0de5ca78a0f3fb9a896c7fe9c7330b6a38385c
lukaseder <lukas.eder@gmail.com>
2013-09-24 22:31:46 +0200

    [jOOQ/jOOQ#2580] Bad SQL rendered when combining DISTINCT with LIMIT .. OFFSET in DB2, SQL Server

--- jOOQ-test/src/org/jooq/test/jOOQAbstractTest.java
+++ jOOQ-test/src/org/jooq/test/jOOQAbstractTest.java
@@ -1805,6 +1805,11 @@ public abstract class jOOQAbstractTest<
     }
 
     @Test
+    public void testLimitDistinct() throws Exception {
+        new OrderByTests(this).testLimitDistinct();
+    }
+
+    @Test
     public void testLimitAliased() throws Exception {
         new OrderByTests(this).testLimitAliased();
     }

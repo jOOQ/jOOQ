180b3a193b3c4b5aa1277f9474ea6c70ffb6336b
Lukas Eder <lukas.eder@gmail.com>
2013-09-24 11:44:16 +0200

    Fixed a regression for Oracle

--- jOOQ/src/main/java/org/jooq/impl/MetaImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/MetaImpl.java
@@ -209,8 +209,7 @@ class MetaImpl implements Meta, Serializable {
                         meta().getSchemas(),
 
                         // [#2681] Work around a flaw in the MySQL JDBC driver
-                        SQLDataType.VARCHAR, // TABLE_SCHEM
-                        SQLDataType.VARCHAR  // TABLE_CATALOG
+                        SQLDataType.VARCHAR // TABLE_SCHEM
                     );
 
                     for (String name : schemas.getValues(0, String.class)) {

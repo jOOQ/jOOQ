2c738d5797640b724c46ea5b479fffba1f8863be
Lukas Eder <lukas.eder@gmail.com>
2013-10-23 09:53:54 +0200

    [#2792] data(Object, Object) methods do not unset the value if null is passed

--- jOOQ/src/main/java/org/jooq/Configuration.java
+++ jOOQ/src/main/java/org/jooq/Configuration.java
@@ -123,8 +123,7 @@ public interface Configuration extends Serializable {
      * See {@link ExecuteListener} for more details.
      *
      * @param key A key to identify the custom data
-     * @param value The custom data or <code>null</code> to unset the custom
-     *            data
+     * @param value The custom data
      * @return The previously set custom data or <code>null</code> if no data
      *         was previously set for the given key
      * @see ExecuteListener
--- jOOQ/src/main/java/org/jooq/RecordContext.java
+++ jOOQ/src/main/java/org/jooq/RecordContext.java
@@ -93,8 +93,7 @@ public interface RecordContext {
      * that of a single query execution.
      *
      * @param key A key to identify the custom data
-     * @param value The custom data or <code>null</code> to unset the custom
-     *            data
+     * @param value The custom data
      * @return The previously set custom data or <code>null</code> if no data
      *         was previously set for the given key
      * @see RecordListener

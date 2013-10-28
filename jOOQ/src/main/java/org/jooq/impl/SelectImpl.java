135a9f950c3620336876d8579e0348a0bb801f50
Lukas Eder <lukas.eder@gmail.com>
2013-10-28 17:52:12 +0100

    [jOOQ/jOOQ#2811] Deprecate ResultQuery.fetchLazy(int) - fetchSize is now passed to ResultQuery.fetchSize() not only for lazy fetching

--- jOOQ/src/main/java/org/jooq/impl/SelectImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/SelectImpl.java
@@ -1689,6 +1689,7 @@ class SelectImpl<R extends Record, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,
     }
 
     @Override
+    @Deprecated
     public final Cursor<R> fetchLazy(int fetchSize) {
         return getDelegate().fetchLazy(fetchSize);
     }

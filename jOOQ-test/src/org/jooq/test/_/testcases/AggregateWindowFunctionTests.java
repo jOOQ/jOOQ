763939e149b0d917a8efb86dcf1edce7a86d9994
Lukas Eder <lukas.eder@gmail.com>
2013-10-30 15:20:42 +0100

    [jOOQ/jOOQ#2492] Skip WINDOW clause

--- jOOQ-test/src/org/jooq/test/_/testcases/AggregateWindowFunctionTests.java
+++ jOOQ-test/src/org/jooq/test/_/testcases/AggregateWindowFunctionTests.java
@@ -1007,6 +1007,7 @@ extends BaseTest<A, AP, B, S, B2S, BS, L, X, DATE, BOOL, D, T, U, UU, I, IPK, T7
     public void testWindowClause() throws Exception {
         switch (dialect()) {
             /* [pro] */
+            case ACCESS:
             case ASE:
             case INGRES:
             /* [/pro] */

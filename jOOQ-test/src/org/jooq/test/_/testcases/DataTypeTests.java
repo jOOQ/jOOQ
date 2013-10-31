b1a77c837834d0bc20c404671ea35c02763bc664
Lukas Eder <lukas.eder@gmail.com>
2013-10-31 18:15:32 +0100

    [jOOQ/jOOQ#2492] Skip Char cast tests

--- jOOQ-test/src/org/jooq/test/_/testcases/DataTypeTests.java
+++ jOOQ-test/src/org/jooq/test/_/testcases/DataTypeTests.java
@@ -45,6 +45,7 @@ import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertNull;
 import static junit.framework.Assert.assertTrue;
+import static org.jooq.SQLDialect.ACCESS;
 import static org.jooq.SQLDialect.ASE;
 import static org.jooq.SQLDialect.CUBRID;
 import static org.jooq.SQLDialect.DB2;
@@ -144,6 +145,10 @@ extends BaseTest<A, AP, B, S, B2S, BS, L, X, DATE, BOOL, D, T, U, UU, I, IPK, T7
 
     @Test
     public void testCharCasts() throws Exception {
+        if (asList(ACCESS).contains(dialect().family())) {
+            log.info("SKIPPING", "Char cast tests");
+            return;
+        }
 
         // [#1241] Casting to CHAR. Some dialects don't like that. They should
         // be casting to VARCHAR instead

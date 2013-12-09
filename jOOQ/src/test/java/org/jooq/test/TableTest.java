26fcc296f4d463913379868b11652445ea6abe2d
Lukas Eder <lukas.eder@gmail.com>
2013-12-09 15:08:29 +0100

    Added some insignificant tests

---
+++ jOOQ/src/test/java/org/jooq/test/TableTest.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2009-2013, Data Geekery GmbH (http://www.datageekery.com)
+ * All rights reserved.
+ *
+ * This work is dual-licensed
+ * - under the Apache Software License 2.0 (the "ASL")
+ * - under the jOOQ License and Maintenance Agreement (the "jOOQ License")
+ * =============================================================================
+ * You may choose which license applies to you:
+ *
+ * - If you're using this work with Open Source databases, you may choose
+ *   either ASL or jOOQ License.
+ * - If you're using this work with at least one commercial database, you must
+ *   choose jOOQ License
+ *
+ * For more information, please visit http://www.jooq.org/licenses
+ *
+ * Apache Software License 2.0:
+ * -----------------------------------------------------------------------------
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * jOOQ License and Maintenance Agreement:
+ * -----------------------------------------------------------------------------
+ * Data Geekery grants the Customer the non-exclusive, timely limited and
+ * non-transferable license to install and use the Software under the terms of
+ * the jOOQ License and Maintenance Agreement.
+ *
+ * This library is distributed with a LIMITED WARRANTY. See the jOOQ License
+ * and Maintenance Agreement for more details: http://www.jooq.org/licensing
+ */
+
+package org.jooq.test;
+
+import static org.jooq.test.data.Table1.FIELD_DATE1;
+import static org.jooq.test.data.Table1.FIELD_ID1;
+import static org.jooq.test.data.Table1.FIELD_NAME1;
+import static org.jooq.test.data.Table1.TABLE1;
+import static org.jooq.test.data.Table2.TABLE2;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+
+/**
+ * A test suite for jOOQ functionality related to tables
+ *
+ * @author Lukas Eder
+ */
+public class TableTest extends AbstractTest {
+
+    @Test
+    public void testTableFieldAccess() throws Exception {
+        assertNotNull(TABLE1.field(FIELD_ID1));
+        assertNull(TABLE2.field(FIELD_ID1));
+
+        assertEquals(FIELD_ID1, TABLE1.field(0));
+        assertEquals(FIELD_NAME1, TABLE1.field(1));
+        assertEquals(FIELD_DATE1, TABLE1.field(2));
+        assertNull(TABLE1.field(3));
+    }
+}

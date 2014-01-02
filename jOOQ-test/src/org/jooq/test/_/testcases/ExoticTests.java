22da91e3ca261643ed9c2cb1807204ad75add2c2
Lukas Eder <lukas.eder@gmail.com>
2014-01-02 16:29:36 +0100

    [jOOQ/jOOQ#2492] Skip PIVOT test in MS Access

--- jOOQ-test/src/org/jooq/test/_/testcases/ExoticTests.java
+++ jOOQ-test/src/org/jooq/test/_/testcases/ExoticTests.java
@@ -153,6 +153,7 @@ extends BaseTest<A, AP, B, S, B2S, BS, L, X, DATE, BOOL, D, T, U, UU, I, IPK, T7
     public void testPivotClause() throws Exception {
         /* [pro] */
         switch (dialect().family()) {
+            case ACCESS:
             case ASE:
             case DB2:
             case INGRES:

7bb402f678f34a2f051fca9daa905d25d2970b8f
Lukas Eder <lukas.eder@gmail.com>
2014-01-24 20:18:31 +0100

    [jOOQ/jOOQ#2981] Add an official Spring / Guice example

---
+++ jOOQ-examples/jOOQ-spring-guice-example/src/main/java/org/jooq/example/guice/TransactionalMethodInterceptor.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2009-2013, Data Geekery GmbH (http://www.datageekery.com)
+ * All rights reserved.
+ *
+ * This work is dual-licensed
+ * - under the Apache Software License 2.0 (the "ASL")
+ * - under the jOOQ License and Maintenance Agreement (the "jOOQ License")
+ * =============================================================================
+ * You may choose which license applies to you:
+ *
+ * - If you're using this work with Open Source databases, you may choose
+ *   either ASL or jOOQ License.
+ * - If you're using this work with at least one commercial database, you must
+ *   choose jOOQ License
+ *
+ * For more information, please visit http://www.jooq.org/licenses
+ *
+ * Apache Software License 2.0:
+ * -----------------------------------------------------------------------------
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * jOOQ License and Maintenance Agreement:
+ * -----------------------------------------------------------------------------
+ * Data Geekery grants the Customer the non-exclusive, timely limited and
+ * non-transferable license to install and use the Software under the terms of
+ * the jOOQ License and Maintenance Agreement.
+ *
+ * This library is distributed with a LIMITED WARRANTY. See the jOOQ License
+ * and Maintenance Agreement for more details: http://www.jooq.org/licensing
+ */
+package org.jooq.example.guice;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.UnexpectedRollbackException;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import com.google.inject.Inject;
+
+/**
+ * A {@link MethodInterceptor} that implements nested transactions.
+ * <p>
+ * Only the outermost transactional method will <code>commit()</code> or
+ * <code>rollback()</code> the contextual transaction. This can be verified
+ * through {@link TransactionStatus#isNewTransaction()}, which returns
+ * <code>true</code> only for the outermost transactional method call.
+ * <p>
+ * This code is tested only for simple {@link Transactional} methods, without
+ * any {@link Propagation} flags.
+ *
+ * @author Lukas Eder
+ */
+class TransactionalMethodInterceptor implements MethodInterceptor {
+
+    @Inject
+    private DataSourceTransactionManager transactionManager;
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        DefaultTransactionDefinition transactionDefinition = new DefaultTransactionDefinition();
+        TransactionStatus transaction = transactionManager.getTransaction(transactionDefinition);
+
+        try {
+            Object result = invocation.proceed();
+
+            try {
+                if (transaction.isNewTransaction())
+                    transactionManager.commit(transaction);
+            }
+            catch (UnexpectedRollbackException ignore) {}
+
+            return result;
+        }
+        catch (Exception e) {
+            if (transaction.isNewTransaction())
+                transactionManager.rollback(transaction);
+
+            throw e;
+        }
+    }
+}

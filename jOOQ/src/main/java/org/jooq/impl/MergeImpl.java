22cf47fe99072ec4203332f7e438e989294111a7
Lukas Eder <lukas.eder@gmail.com>
2014-04-08 15:05:12 +0200

    [#3164] Missing formatting when rendering formatted MERGE statement's SET clause - Removed excess whitespace

--- jOOQ/src/main/java/org/jooq/impl/MergeImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/MergeImpl.java
@@ -1163,7 +1163,7 @@ implements
         // [#999] WHEN MATCHED clause is optional
         if (matchedUpdate != null) {
             context.formatSeparator()
-                   .keyword("when matched then update set").sql(" ")
+                   .keyword("when matched then update set")
                    .formatIndentStart()
                    .formatSeparator()
                    .visit(matchedUpdate)

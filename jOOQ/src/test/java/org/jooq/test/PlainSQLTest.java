d7466b05d6f92cc2f42bd75c8ff57e53f303ea7b
Lukas Eder <lukas.eder@gmail.com>
2014-05-29 15:52:26 +0200

    [#2906] Created test case stub

---
+++ jOOQ/src/test/java/org/jooq/test/PlainSQLTest.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2009-2014, Data Geekery GmbH (http://www.datageekery.com)
+ * All rights reserved.
+ *
+ * This work is dual-licensed
+ * - under the Apache Software License 2.0 (the "ASL")
+ * - under the jOOQ License and Maintenance Agreement (the "jOOQ License")
+ * =============================================================================
+ * You may choose which license applies to you:
+ *
+ * - If you're using this work with Open Source databases, you may choose
+ *   either ASL or jOOQ License.
+ * - If you're using this work with at least one commercial database, you must
+ *   choose jOOQ License
+ *
+ * For more information, please visit http://www.jooq.org/licenses
+ *
+ * Apache Software License 2.0:
+ * -----------------------------------------------------------------------------
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * jOOQ License and Maintenance Agreement:
+ * -----------------------------------------------------------------------------
+ * Data Geekery grants the Customer the non-exclusive, timely limited and
+ * non-transferable license to install and use the Software under the terms of
+ * the jOOQ License and Maintenance Agreement.
+ *
+ * This library is distributed with a LIMITED WARRANTY. See the jOOQ License
+ * and Maintenance Agreement for more details: http://www.jooq.org/licensing
+ */
+package org.jooq.test;
+
+import static org.jooq.impl.DSL.condition;
+
+import org.jooq.QueryPart;
+
+import org.junit.Test;
+
+/**
+ * @author Lukas Eder
+ */
+public class PlainSQLTest extends AbstractTest {
+
+    @Test
+    public void testIndexedParameters() {
+        QueryPart q = condition("a = ? and b = ? and ? = ?", 1, 2, "a", "b");
+
+        assertEquals("(a = ? and b = ? and ? = ?)", create.render(q));
+        assertEquals("(a = 1 and b = 2 and 'a' = 'b')", create.renderInlined(q));
+    }
+
+    @Test
+    public void testNamedParameters() {
+
+        // [#2906] TODO: It's not clear how we want to deal with all sorts of named bind variables. There
+        // is substantial risk of breaking actual SQL syntax, e.g. PostgreSQL's :: operator
+
+//        QueryPart q = condition("a = :1 and b = :2 and :3 = :4", 1, 2, "a", "b");
+//
+//        assertEquals("(a = ? and b = ? and ? = ?)", create.render(q));
+//        assertEquals("(a = :1 and b = :2 and :3 = :4)", create.renderNamedParams(q));
+//        assertEquals("(a = 1 and b = 2 and 'a' = 'b')", create.renderInlined(q));
+    }
+}

5a8a30014cecf1b62e2eeaa65173b0213c9c3460
Lukas Eder <lukas.eder@gmail.com>
2014-06-03 10:00:54 +0200

    [jOOQ/jOOQ#3310] Prepare test database for test case

--- jOOQ-test/src/test/java/org/jooq/test/postgres/generatedclasses/tables/pojos/TArrays.java
+++ jOOQ-test/src/test/java/org/jooq/test/postgres/generatedclasses/tables/pojos/TArrays.java
@@ -9,7 +9,7 @@ package org.jooq.test.postgres.generatedclasses.tables.pojos;
 @java.lang.SuppressWarnings({ "all", "unchecked", "rawtypes" })
 public class TArrays implements org.jooq.test.postgres.generatedclasses.tables.interfaces.ITArrays {
 
-	private static final long serialVersionUID = 1714348615;
+	private static final long serialVersionUID = 1425507789;
 
 	private final java.lang.Integer                                                       id;
 	private final java.lang.String[]                                                      stringArray;
@@ -18,6 +18,9 @@ public class TArrays implements org.jooq.test.postgres.generatedclasses.tables.i
 	private final org.jooq.test.postgres.generatedclasses.udt.records.UStreetTypeRecord[] udtArray;
 	private final org.jooq.test.postgres.generatedclasses.enums.UCountry[]                enumArray;
 	private final java.lang.Integer[]                                                     arrayArray;
+	private final java.lang.String[]                                                      numberList;
+	private final java.lang.String[]                                                      stringList;
+	private final java.lang.String[]                                                      dateList;
 
 	public TArrays(
 		java.lang.Integer                                                       id,
@@ -26,7 +29,10 @@ public class TArrays implements org.jooq.test.postgres.generatedclasses.tables.i
 		java.sql.Date[]                                                         dateArray,
 		org.jooq.test.postgres.generatedclasses.udt.records.UStreetTypeRecord[] udtArray,
 		org.jooq.test.postgres.generatedclasses.enums.UCountry[]                enumArray,
-		java.lang.Integer[]                                                     arrayArray
+		java.lang.Integer[]                                                     arrayArray,
+		java.lang.String[]                                                      numberList,
+		java.lang.String[]                                                      stringList,
+		java.lang.String[]                                                      dateList
 	) {
 		this.id = id;
 		this.stringArray = stringArray;
@@ -35,6 +41,9 @@ public class TArrays implements org.jooq.test.postgres.generatedclasses.tables.i
 		this.udtArray = udtArray;
 		this.enumArray = enumArray;
 		this.arrayArray = arrayArray;
+		this.numberList = numberList;
+		this.stringList = stringList;
+		this.dateList = dateList;
 	}
 
 	@Override
@@ -70,5 +79,20 @@ public class TArrays implements org.jooq.test.postgres.generatedclasses.tables.i
 	@Override
 	public java.lang.Integer[] getArrayArray() {
 		return this.arrayArray;
+	}
+
+	@Override
+	public java.lang.String[] getNumberList() {
+		return this.numberList;
+	}
+
+	@Override
+	public java.lang.String[] getStringList() {
+		return this.stringList;
+	}
+
+	@Override
+	public java.lang.String[] getDateList() {
+		return this.dateList;
 	}
 }

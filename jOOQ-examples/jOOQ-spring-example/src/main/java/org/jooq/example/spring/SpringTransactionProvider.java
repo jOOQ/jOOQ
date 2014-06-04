0e211ff414cf3b2671bf59fbdc63f5356d8dfd88
Lukas Eder <lukas.eder@gmail.com>
2014-06-04 13:18:27 +0200

    [jOOQ/jOOQ#3229] Added examples using binding Spring's TransactionManager to jOOQ's new transaction() API

---
+++ jOOQ-examples/jOOQ-spring-example/src/main/java/org/jooq/example/spring/SpringTransactionProvider.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2009-2013, Data Geekery GmbH (http://www.datageekery.com)
+ * All rights reserved.
+ *
+ * This work is dual-licensed
+ * - under the Apache Software License 2.0 (the "ASL")
+ * - under the jOOQ License and Maintenance Agreement (the "jOOQ License")
+ * =============================================================================
+ * You may choose which license applies to you:
+ *
+ * - If you're using this work with Open Source databases, you may choose
+ *   either ASL or jOOQ License.
+ * - If you're using this work with at least one commercial database, you must
+ *   choose jOOQ License
+ *
+ * For more information, please visit http://www.jooq.org/licenses
+ *
+ * Apache Software License 2.0:
+ * -----------------------------------------------------------------------------
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * jOOQ License and Maintenance Agreement:
+ * -----------------------------------------------------------------------------
+ * Data Geekery grants the Customer the non-exclusive, timely limited and
+ * non-transferable license to install and use the Software under the terms of
+ * the jOOQ License and Maintenance Agreement.
+ *
+ * This library is distributed with a LIMITED WARRANTY. See the jOOQ License
+ * and Maintenance Agreement for more details: http://www.jooq.org/licensing
+ */
+package org.jooq.example.spring;
+
+import static org.springframework.transaction.TransactionDefinition.PROPAGATION_NESTED;
+
+import org.jooq.TransactionContext;
+import org.jooq.TransactionProvider;
+import org.jooq.tools.JooqLogger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+/**
+ * An example <code>TransactionProvider</code> implementing the
+ * {@link TransactionProvider} contract for use with Spring.
+ *
+ * @author Lukas Eder
+ */
+public class SpringTransactionProvider implements TransactionProvider {
+
+    private static final JooqLogger log = JooqLogger.getLogger(SpringTransactionProvider.class);
+
+    @Autowired
+    DataSourceTransactionManager txMgr;
+
+    @Override
+    public void begin(TransactionContext ctx) {
+        log.info("Begin transaction");
+
+        // This TransactionProvider behaves like jOOQ's DefaultTransactionProvider,
+        // which supports nested transactions using Savepoints
+        TransactionStatus tx = txMgr.getTransaction(new DefaultTransactionDefinition(PROPAGATION_NESTED));
+        ctx.transaction(new SpringTransaction(tx));
+    }
+
+    @Override
+    public void commit(TransactionContext ctx) {
+        log.info("commit transaction");
+
+        txMgr.commit(((SpringTransaction) ctx.transaction()).tx);
+    }
+
+    @Override
+    public void rollback(TransactionContext ctx) {
+        log.info("rollback transaction");
+
+        txMgr.rollback(((SpringTransaction) ctx.transaction()).tx);
+    }
+}
--- jOOQ-examples/jOOQ-spring-example/src/test/java/org/jooq/example/TransactionTest.java
+++ jOOQ-examples/jOOQ-spring-example/src/test/java/org/jooq/example/TransactionTest.java
@@ -44,8 +44,11 @@ import static org.jooq.example.db.h2.Tables.BOOK;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.jooq.DSLContext;
 import org.jooq.example.spring.BookService;
+import org.jooq.example.spring.SpringTransactionProvider;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -74,6 +77,7 @@ public class TransactionTest {
 
     @Autowired DSLContext                   dsl;
     @Autowired DataSourceTransactionManager txMgr;
+    @Autowired SpringTransactionProvider    txProvider;
     @Autowired BookService                  books;
 
     @After
@@ -126,5 +130,93 @@ public class TransactionTest {
 
         assertEquals(4, dsl.fetchCount(BOOK));
         assertTrue(rollback);
+    }
+
+    @Test
+    public void testjOOQTransactionsSimple() {
+        boolean rollback = false;
+
+        try {
+            dsl.transaction(c -> {
+
+                // This is a "bug". The same book is created twice, resulting in a
+                // constraint violation exception
+                for (int i = 0; i < 2; i++)
+                    dsl.insertInto(BOOK)
+                       .set(BOOK.ID, 5)
+                       .set(BOOK.AUTHOR_ID, 1)
+                       .set(BOOK.TITLE, "Book 5")
+                       .execute();
+
+                Assert.fail();
+            });
+        }
+
+        // Upon the constraint violation, the transaction must already have been rolled back
+        catch (DataAccessException e) {
+            rollback = true;
+        }
+
+        assertEquals(4, dsl.fetchCount(BOOK));
+        assertTrue(rollback);
+    }
+
+    @Test
+    public void testjOOQTransactionsNested() {
+        AtomicBoolean rollback1 = new AtomicBoolean(false);
+        AtomicBoolean rollback2 = new AtomicBoolean(false);
+
+        try {
+
+            // If using Spring transactions, we don't need the c1 reference
+            dsl.transaction(c1 -> {
+
+                // The first insertion will work
+                dsl.insertInto(BOOK)
+                   .set(BOOK.ID, 5)
+                   .set(BOOK.AUTHOR_ID, 1)
+                   .set(BOOK.TITLE, "Book 5")
+                   .execute();
+
+                assertEquals(5, dsl.fetchCount(BOOK));
+
+                try {
+
+                    // Nest transactions using Spring. This should create a savepoint, right here
+                    // If using Spring transactions, we don't need the c2 reference
+                    dsl.transaction(c2 -> {
+
+                        // The second insertion shouldn't work
+                        for (int i = 0; i < 2; i++)
+                            dsl.insertInto(BOOK)
+                               .set(BOOK.ID, 6)
+                               .set(BOOK.AUTHOR_ID, 1)
+                               .set(BOOK.TITLE, "Book 6")
+                               .execute();
+
+                        Assert.fail();
+                    });
+                }
+
+                catch (DataAccessException e) {
+                    rollback1.set(true);
+                }
+
+                // We should've rolled back to the savepoint
+                assertEquals(5, dsl.fetchCount(BOOK));
+
+                throw new org.jooq.exception.DataAccessException("Rollback");
+            });
+        }
+
+        // Upon the constraint violation, the transaction must already have been rolled back
+        catch (org.jooq.exception.DataAccessException e) {
+            assertEquals("Rollback", e.getMessage());
+            rollback2.set(true);
+        }
+
+        assertEquals(4, dsl.fetchCount(BOOK));
+        assertTrue(rollback2.get());
+        assertTrue(rollback2.get());
     }
 }

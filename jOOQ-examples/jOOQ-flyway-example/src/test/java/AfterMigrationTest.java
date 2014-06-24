1e493688c4101e4fa56654d699aba1850c0e1a34
Lukas Eder <lukas.eder@gmail.com>
2014-06-24 16:52:23 +0200

    [jOOQ/jOOQ#2289] Final cleanup for blog post

--- jOOQ-examples/jOOQ-flyway-example/src/test/java/AfterMigrationTest.java
+++ jOOQ-examples/jOOQ-flyway-example/src/test/java/AfterMigrationTest.java
@@ -2,6 +2,9 @@ import org.jooq.Result;
 import org.jooq.impl.DSL;
 import org.junit.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import static java.util.Arrays.asList;
 import static org.jooq.example.flyway.db.h2.Tables.*;
 import static org.junit.Assert.assertEquals;
@@ -12,22 +15,24 @@ import static org.junit.Assert.assertEquals;
 public class AfterMigrationTest {
 
     @Test
-    public void testQueryingAfterMigration() {
-        Result<?> result =
-        DSL.using("jdbc:h2:~/flyway-test", "sa", "")
-           .select(
-               AUTHOR.FIRST_NAME,
-               AUTHOR.LAST_NAME,
-               BOOK.ID,
-               BOOK.TITLE
-           )
-           .from(AUTHOR)
-           .join(BOOK)
-           .on(AUTHOR.ID.eq(BOOK.AUTHOR_ID))
-           .orderBy(BOOK.ID.asc())
-           .fetch();
+    public void testQueryingAfterMigration() throws Exception {
+        try (Connection c = DriverManager.getConnection("jdbc:h2:~/flyway-test", "sa", "")) {
+            Result<?> result =
+            DSL.using(c)
+                .select(
+                    AUTHOR.FIRST_NAME,
+                    AUTHOR.LAST_NAME,
+                    BOOK.ID,
+                    BOOK.TITLE
+                )
+                .from(AUTHOR)
+                .join(BOOK)
+                .on(AUTHOR.ID.eq(BOOK.AUTHOR_ID))
+                .orderBy(BOOK.ID.asc())
+                .fetch();
 
-        assertEquals(4, result.size());
-        assertEquals(asList(1, 2, 3, 4), result.getValues(BOOK.ID));
+            assertEquals(4, result.size());
+            assertEquals(asList(1, 2, 3, 4), result.getValues(BOOK.ID));
+        }
     }
 }

a6bda3a6c67fe3dbcfcd0c9405870740fc4cc78b
Lukas Eder <lukas.eder@gmail.com>
2014-07-11 11:48:11 +0200

    Reduced visibility of some example class

--- jOOQ-examples/jOOQ-spring-example/src/main/java/org/jooq/example/spring/SpringTransaction.java
+++ jOOQ-examples/jOOQ-spring-example/src/main/java/org/jooq/example/spring/SpringTransaction.java
@@ -44,10 +44,10 @@ import org.jooq.Transaction;
 
 import org.springframework.transaction.TransactionStatus;
 
-public class SpringTransaction implements Transaction {
-    public final TransactionStatus tx;
+class SpringTransaction implements Transaction {
+    final TransactionStatus tx;
 
-    public SpringTransaction(TransactionStatus tx) {
+    SpringTransaction(TransactionStatus tx) {
         this.tx = tx;
     }
 }

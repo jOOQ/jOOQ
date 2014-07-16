c1033c2928c22868a334fd4878bc96b27a81e45b
Lukas Eder <lukas.eder@gmail.com>
2014-07-16 10:48:32 +0200

    [#3398] Avoid generating new SQL statements for every bind variable length in Firebird

--- jOOQ/src/main/java/org/jooq/impl/Val.java
+++ jOOQ/src/main/java/org/jooq/impl/Val.java
@@ -257,12 +257,6 @@ class Val<T> extends AbstractParam<T> {
             toSQL(context, value, getConverter());
         }
 
-        // [#1727] VARCHAR types should be cast to their actual lengths in some
-        // dialects
-        else if ((type == SQLDataType.VARCHAR || type == SQLDataType.CHAR) && asList(FIREBIRD).contains(family)) {
-            toSQLCast(context, dataType, getValueLength(), 0, 0);
-        }
-
         /* [pro] xx
         xx xxxxxxx xxxx xxxx xxxxx xxxxxx xxx xx xxxx xx xxx xxxxxx xx xxxx xxxxxxxx
         xxxx xx xxxxx xx xxxxxxxxxxxxxxxxxxxxxxx xx xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx x
@@ -273,28 +267,6 @@ class Val<T> extends AbstractParam<T> {
         // In all other cases, the bind variable can be cast normally
         else {
             toSQLCast(context, dataType, dataType.length(), dataType.precision(), dataType.scale());
-        }
-    }
-
-    private final int getValueLength() {
-        String string = (String) value;
-        if (string == null) {
-            return 1;
-        }
-
-        else {
-            int length = string.length();
-
-            // If non 7-bit ASCII characters are present, multiply the length by
-            // 4 to be sure that even UTF-32 collations will fit. But don't use
-            // larger numbers than Derby's upper limit 32672
-            for (int i = 0; i < length; i++) {
-                if (string.charAt(i) > 127) {
-                    return Math.min(32672, 4 * length);
-                }
-            }
-
-            return Math.min(32672, length);
         }
     }
 

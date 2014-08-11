69b6cb02d188b3566b2f966a0610071146a7f979
Lukas Eder <lukas.eder@gmail.com>
2014-08-11 14:33:55 +0200

    [jOOQ/jOOQ#3513] Streaming version of formatCSV/formatXML

---
+++ jOOQ/src/main/java/org/jooq/exception/IOException.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2009-2014, Data Geekery GmbH (http://www.datageekery.com)
+ * All rights reserved.
+ *
+ * This work is dual-licensed
+ * - under the Apache Software License 2.0 (the "ASL")
+ * - under the jOOQ License and Maintenance Agreement (the "jOOQ License")
+ * =============================================================================
+ * You may choose which license applies to you:
+ *
+ * - If you're using this work with Open Source databases, you may choose
+ *   either ASL or jOOQ License.
+ * - If you're using this work with at least one commercial database, you must
+ *   choose jOOQ License
+ *
+ * For more information, please visit http://www.jooq.org/licenses
+ *
+ * Apache Software License 2.0:
+ * -----------------------------------------------------------------------------
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * jOOQ License and Maintenance Agreement:
+ * -----------------------------------------------------------------------------
+ * Data Geekery grants the Customer the non-exclusive, timely limited and
+ * non-transferable license to install and use the Software under the terms of
+ * the jOOQ License and Maintenance Agreement.
+ *
+ * This library is distributed with a LIMITED WARRANTY. See the jOOQ License
+ * and Maintenance Agreement for more details: http://www.jooq.org/licensing
+ */
+package org.jooq.exception;
+
+/**
+ * The jOOQ <code>IOException</code> is a wrapper for a
+ * {@link java.io.IOException}.
+ *
+ * @author Lukas Eder
+ */
+public class IOException extends DataAccessException {
+
+    /**
+     * Generated UID
+     */
+    private static final long serialVersionUID = 491834858363345767L;
+
+    /**
+     * Constructor for DataAccessException.
+     *
+     * @param message the detail message
+     * @param cause the root cause (usually from using a underlying data access
+     *            API such as JDBC)
+     */
+    public IOException(String message, java.io.IOException cause) {
+        super(message, cause);
+    }
+
+    @Override
+    public synchronized java.io.IOException getCause() {
+        return (java.io.IOException) super.getCause();
+    }
+
+    @Override
+    public synchronized Throwable initCause(Throwable cause) {
+        if (!(cause instanceof java.io.IOException))
+            throw new IllegalArgumentException("Can only wrap java.io.IOException: " + cause);
+
+        return super.initCause(cause);
+    }
+}

958d1be1539d178e38dba0f02517062024721397
Lukas Eder <lukas.eder@gmail.com>
2014-08-15 11:37:41 +0200

    [jOOQ/jOOQ#3552] Oracle Stored procedures using type synonyms in signatures cannot be used through jOOQ - Regenerated schema

--- jOOQ-test/src/test/java/org/jooq/test/oracle/generatedclasses/test/packages/TestSynonymPackage.java
+++ jOOQ-test/src/test/java/org/jooq/test/oracle/generatedclasses/test/packages/TestSynonymPackage.java
@@ -11,7 +11,7 @@ package org.jooq.test.oracle.generatedclasses.test.packages;
 @java.lang.SuppressWarnings({ "all", "unchecked", "rawtypes" })
 public class TestSynonymPackage extends org.jooq.impl.PackageImpl {
 
-	private static final long serialVersionUID = 803151505;
+	private static final long serialVersionUID = -1003707256;
 
 	/**
 	 * The singleton instance of <code>TEST.TEST_SYNONYM_PACKAGE</code>
@@ -59,7 +59,7 @@ public class TestSynonymPackage extends org.jooq.impl.PackageImpl {
 	/**
 	 * Call <code>TEST.TEST_SYNONYM_PACKAGE.PUBLIC_OBJECT</code>
 	 */
-	public static java.lang.Object publicObject(org.jooq.Configuration configuration) {
+	public static org.jooq.test.oracle.generatedclasses.multi_schema.udt.records.NumberObjectRecord publicObject(org.jooq.Configuration configuration) {
 		org.jooq.test.oracle.generatedclasses.test.packages.test_synonym_package.PublicObject f = new org.jooq.test.oracle.generatedclasses.test.packages.test_synonym_package.PublicObject();
 
 		f.execute(configuration);
@@ -69,7 +69,7 @@ public class TestSynonymPackage extends org.jooq.impl.PackageImpl {
 	/**
 	 * Get <code>TEST.TEST_SYNONYM_PACKAGE.PUBLIC_OBJECT</code> as a field
 	 */
-	public static org.jooq.Field<java.lang.Object> publicObject() {
+	public static org.jooq.Field<org.jooq.test.oracle.generatedclasses.multi_schema.udt.records.NumberObjectRecord> publicObject() {
 		org.jooq.test.oracle.generatedclasses.test.packages.test_synonym_package.PublicObject f = new org.jooq.test.oracle.generatedclasses.test.packages.test_synonym_package.PublicObject();
 
 		return f.asField();
@@ -78,7 +78,7 @@ public class TestSynonymPackage extends org.jooq.impl.PackageImpl {
 	/**
 	 * Call <code>TEST.TEST_SYNONYM_PACKAGE.PUBLIC_TABLE</code>
 	 */
-	public static java.lang.Object publicTable(org.jooq.Configuration configuration) {
+	public static org.jooq.test.oracle.generatedclasses.multi_schema.udt.records.NumberTableRecord publicTable(org.jooq.Configuration configuration) {
 		org.jooq.test.oracle.generatedclasses.test.packages.test_synonym_package.PublicTable f = new org.jooq.test.oracle.generatedclasses.test.packages.test_synonym_package.PublicTable();
 
 		f.execute(configuration);
@@ -88,7 +88,7 @@ public class TestSynonymPackage extends org.jooq.impl.PackageImpl {
 	/**
 	 * Get <code>TEST.TEST_SYNONYM_PACKAGE.PUBLIC_TABLE</code> as a field
 	 */
-	public static org.jooq.Field<java.lang.Object> publicTable() {
+	public static org.jooq.Field<org.jooq.test.oracle.generatedclasses.multi_schema.udt.records.NumberTableRecord> publicTable() {
 		org.jooq.test.oracle.generatedclasses.test.packages.test_synonym_package.PublicTable f = new org.jooq.test.oracle.generatedclasses.test.packages.test_synonym_package.PublicTable();
 
 		return f.asField();
@@ -97,7 +97,7 @@ public class TestSynonymPackage extends org.jooq.impl.PackageImpl {
 	/**
 	 * Call <code>TEST.TEST_SYNONYM_PACKAGE.TEST_OBJECT</code>
 	 */
-	public static java.lang.Object testObject(org.jooq.Configuration configuration) {
+	public static org.jooq.test.oracle.generatedclasses.multi_schema.udt.records.NumberObjectRecord testObject(org.jooq.Configuration configuration) {
 		org.jooq.test.oracle.generatedclasses.test.packages.test_synonym_package.TestObject f = new org.jooq.test.oracle.generatedclasses.test.packages.test_synonym_package.TestObject();
 
 		f.execute(configuration);
@@ -107,7 +107,7 @@ public class TestSynonymPackage extends org.jooq.impl.PackageImpl {
 	/**
 	 * Get <code>TEST.TEST_SYNONYM_PACKAGE.TEST_OBJECT</code> as a field
 	 */
-	public static org.jooq.Field<java.lang.Object> testObject() {
+	public static org.jooq.Field<org.jooq.test.oracle.generatedclasses.multi_schema.udt.records.NumberObjectRecord> testObject() {
 		org.jooq.test.oracle.generatedclasses.test.packages.test_synonym_package.TestObject f = new org.jooq.test.oracle.generatedclasses.test.packages.test_synonym_package.TestObject();
 
 		return f.asField();
@@ -116,7 +116,7 @@ public class TestSynonymPackage extends org.jooq.impl.PackageImpl {
 	/**
 	 * Call <code>TEST.TEST_SYNONYM_PACKAGE.TEST_TABLE</code>
 	 */
-	public static java.lang.Object testTable(org.jooq.Configuration configuration) {
+	public static org.jooq.test.oracle.generatedclasses.multi_schema.udt.records.NumberTableRecord testTable(org.jooq.Configuration configuration) {
 		org.jooq.test.oracle.generatedclasses.test.packages.test_synonym_package.TestTable f = new org.jooq.test.oracle.generatedclasses.test.packages.test_synonym_package.TestTable();
 
 		f.execute(configuration);
@@ -126,8 +126,27 @@ public class TestSynonymPackage extends org.jooq.impl.PackageImpl {
 	/**
 	 * Get <code>TEST.TEST_SYNONYM_PACKAGE.TEST_TABLE</code> as a field
 	 */
-	public static org.jooq.Field<java.lang.Object> testTable() {
+	public static org.jooq.Field<org.jooq.test.oracle.generatedclasses.multi_schema.udt.records.NumberTableRecord> testTable() {
 		org.jooq.test.oracle.generatedclasses.test.packages.test_synonym_package.TestTable f = new org.jooq.test.oracle.generatedclasses.test.packages.test_synonym_package.TestTable();
+
+		return f.asField();
+	}
+
+	/**
+	 * Call <code>TEST.TEST_SYNONYM_PACKAGE.TEST_TRANSITIVE</code>
+	 */
+	public static org.jooq.test.oracle.generatedclasses.multi_schema.udt.records.NumberObjectRecord testTransitive(org.jooq.Configuration configuration) {
+		org.jooq.test.oracle.generatedclasses.test.packages.test_synonym_package.TestTransitive f = new org.jooq.test.oracle.generatedclasses.test.packages.test_synonym_package.TestTransitive();
+
+		f.execute(configuration);
+		return f.getReturnValue();
+	}
+
+	/**
+	 * Get <code>TEST.TEST_SYNONYM_PACKAGE.TEST_TRANSITIVE</code> as a field
+	 */
+	public static org.jooq.Field<org.jooq.test.oracle.generatedclasses.multi_schema.udt.records.NumberObjectRecord> testTransitive() {
+		org.jooq.test.oracle.generatedclasses.test.packages.test_synonym_package.TestTransitive f = new org.jooq.test.oracle.generatedclasses.test.packages.test_synonym_package.TestTransitive();
 
 		return f.asField();
 	}

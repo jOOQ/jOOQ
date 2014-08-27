00f956e39ca4db4ff5e14518e38c540adae784a8
Lukas Eder <lukas.eder@gmail.com>
2014-08-27 13:56:56 +0200

    Fixed compilation error

--- jOOQ/src/main/java/org/jooq/impl/SelectImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/SelectImpl.java
@@ -1909,9 +1909,11 @@ class SelectImpl<R extends Record, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,
         joinConditions = new ConditionProviderImpl();
         joinConditions.addConditions(conditions);
 
-        if (joinPartitionBy != null && joinPartitionBy.length > 0)
-            getQuery().addJoin(joinTable, joinType, new Condition[] { joinConditions }, joinPartitionBy);
-        else
+        /* [pro] xx
+        xx xxxxxxxxxxxxxxxx xx xxxx xx xxxxxxxxxxxxxxxxxxxxxx x xx
+            xxxxxxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxx xxx xxxxxxxxxxx x xxxxxxxxxxxxxx xx xxxxxxxxxxxxxxxxx
+        xxxx
+        xx [/pro] */
             getQuery().addJoin(joinTable, joinType, new Condition[] { joinConditions });
 
         joinTable = null;

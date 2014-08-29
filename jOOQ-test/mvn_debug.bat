cc1cbce6d5322e6de407fac47a12590ad6fec33b
Lukas Eder <lukas.eder@gmail.com>
2014-08-29 15:18:50 +0200

    Maintaining the integration tests in both OSS and commercial versions is too much effort.

--- jOOQ-test/mvn_debug.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-
-IF "%1" == "off" (
-    SET MAVEN_OPTS=
-) ELSE (
-    SET MAVEN_OPTS=-Xdebug -Xnoagent -Djava.compile=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005
-)
-
-SET MAVEN_OPTS

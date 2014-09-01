b8f96ed9ef0e950dc19a6eef999bf30313cba7c2
Lukas Eder <lukas.eder@gmail.com>
2014-09-01 12:25:49 +0200

    [#3611] Add a jOOQ / Nashorn example to the repository

---
+++ jOOQ-examples/jOOQ-nashorn-example/src/test/java/org/jooq/example/NashornTest.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright (c) 2009-2013, Data Geekery GmbH (http://www.datageekery.com)
+ * All rights reserved.
+ *
+ * This work is dual-licensed
+ * - under the Apache Software License 2.0 (the "ASL")
+ * - under the jOOQ License and Maintenance Agreement (the "jOOQ License")
+ * =============================================================================
+ * You may choose which license applies to you:
+ *
+ * - If you're using this work with Open Source databases, you may choose
+ *   either ASL or jOOQ License.
+ * - If you're using this work with at least one commercial database, you must
+ *   choose jOOQ License
+ *
+ * For more information, please visit http://www.jooq.org/licenses
+ *
+ * Apache Software License 2.0:
+ * -----------------------------------------------------------------------------
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * jOOQ License and Maintenance Agreement:
+ * -----------------------------------------------------------------------------
+ * Data Geekery grants the Customer the non-exclusive, timely limited and
+ * non-transferable license to install and use the Software under the terms of
+ * the jOOQ License and Maintenance Agreement.
+ *
+ * This library is distributed with a LIMITED WARRANTY. See the jOOQ License
+ * and Maintenance Agreement for more details: http://www.jooq.org/licensing
+ */
+package org.jooq.example;
+
+import static javax.script.ScriptContext.ENGINE_SCOPE;
+
+import java.io.File;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+import java.util.stream.Stream;
+
+import javax.script.Bindings;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Lukas Eder
+ */
+public class NashornTest {
+
+    private static Connection connection;
+
+    @BeforeClass
+    public static void before() throws Exception {
+        Properties properties = new Properties();
+        properties.load(NashornTest.class.getResourceAsStream("/config.properties"));
+
+        Class.forName(properties.getProperty("db.driver"));
+        connection = DriverManager.getConnection(
+            properties.getProperty("db.url"),
+            properties.getProperty("db.username"),
+            properties.getProperty("db.password")
+        );
+    }
+
+    @Test
+    public void testScripts() throws Exception {
+        Stream.of(
+            new File(getClass().getResource("/org/jooq/example/test").toURI()).listFiles((dir, name) -> name.endsWith(".js"))
+        ).forEach(file -> {
+            ScriptEngineManager manager = new ScriptEngineManager();
+            ScriptEngine engine = manager.getEngineByName("nashorn");
+
+            Bindings bindings = engine.getBindings(ENGINE_SCOPE);
+            bindings.put("connection", connection);
+
+            try {
+                engine.eval(new FileReader(file));
+            }
+            catch (Exception e) {
+                throw new RuntimeException("Error while running " + file, e);
+            }
+        });
+
+    }
+}

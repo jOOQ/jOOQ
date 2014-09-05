29979fc21bf90a0ca62b53b06d5848296a025bb7
Lukas Eder <lukas.eder@gmail.com>
2014-09-05 15:21:52 +0200

    [jOOQ/jOOQ#3624] Added a new test configuration

---
+++ jOOQ-test/src/test/java/org/jooq/test/all/converters/LocalDateTimeConverter.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2009-2014, Data Geekery GmbH (http://www.datageekery.com)
+ * All rights reserved.
+ *
+ * This work is dual-licensed
+ * - under the Apache Software License 2.0 (the "ASL")
+ * - under the jOOQ License and Maintenance Agreement (the "jOOQ License")
+ * =============================================================================
+ * You may choose which license applies to you:
+ *
+ * - If you're using this work with Open Source databases, you may choose
+ *   either ASL or jOOQ License.
+ * - If you're using this work with at least one commercial database, you must
+ *   choose jOOQ License
+ *
+ * For more information, please visit http://www.jooq.org/licenses
+ *
+ * Apache Software License 2.0:
+ * -----------------------------------------------------------------------------
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * jOOQ License and Maintenance Agreement:
+ * -----------------------------------------------------------------------------
+ * Data Geekery grants the Customer the non-exclusive, timely limited and
+ * non-transferable license to install and use the Software under the terms of
+ * the jOOQ License and Maintenance Agreement.
+ *
+ * This library is distributed with a LIMITED WARRANTY. See the jOOQ License
+ * and Maintenance Agreement for more details: http://www.jooq.org/licensing
+ */
+package org.jooq.test.all.converters;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.jooq.Converter;
+
+public class LocalDateTimeConverter implements Converter<Date, LocalDateTime> {
+
+    /**
+     * Generated UID
+     */
+    private static final long serialVersionUID = -5060861060926377086L;
+
+    @Override
+    public LocalDateTime from(Date t) {
+        return t == null ? null : LocalDateTime.ofEpochSecond(t.getTime(), 0, ZoneOffset.UTC);
+    }
+
+    @Override
+    public Date to(LocalDateTime u) {
+        return u == null ? null : new Date(u.toEpochSecond(ZoneOffset.UTC));
+    }
+
+    @Override
+    public Class<Date> fromType() {
+        return Date.class;
+    }
+
+    @Override
+    public Class<LocalDateTime> toType() {
+        return LocalDateTime.class;
+    }
+}

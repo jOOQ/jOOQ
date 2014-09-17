9ff65a95f84148f510c1c29385530f4ff432a95a
Lukas Eder <lukas.eder@gmail.com>
2014-09-17 19:27:24 +0200

    More jOOQ Academy content

---
+++ jOOQ-examples/jOOQ-academy/src/test/java/org/jooq/academy/section4/Example_4_1_ConnectionProvider.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2009-2013, Data Geekery GmbH (http://www.datageekery.com)
+ * All rights reserved.
+ *
+ * This work is dual-licensed
+ * - under the Apache Software License 2.0 (the "ASL")
+ * - under the jOOQ License and Maintenance Agreement (the "jOOQ License")
+ * =============================================================================
+ * You may choose which license applies to you:
+ *
+ * - If you're using this work with Open Source databases, you may choose
+ *   either ASL or jOOQ License.
+ * - If you're using this work with at least one commercial database, you must
+ *   choose jOOQ License
+ *
+ * For more information, please visit http://www.jooq.org/licenses
+ *
+ * Apache Software License 2.0:
+ * -----------------------------------------------------------------------------
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * jOOQ License and Maintenance Agreement:
+ * -----------------------------------------------------------------------------
+ * Data Geekery grants the Customer the non-exclusive, timely limited and
+ * non-transferable license to install and use the Software under the terms of
+ * the jOOQ License and Maintenance Agreement.
+ *
+ * This library is distributed with a LIMITED WARRANTY. See the jOOQ License
+ * and Maintenance Agreement for more details: http://www.jooq.org/licensing
+ */
+package org.jooq.academy.section4;
+
+import static org.jooq.academy.tools.Tools.connection;
+import static org.jooq.example.db.h2.Tables.AUTHOR;
+
+import java.sql.Connection;
+
+import org.jooq.SQLDialect;
+import org.jooq.academy.tools.Tools;
+import org.jooq.impl.DSL;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.junit.Test;
+
+public class Example_4_1_ConnectionProvider {
+
+    @Test
+    public void run() {
+        Connection connection = connection();
+
+        Tools.title("Using jOOQ with a standalone connection");
+        System.out.println(
+            DSL.using(connection)
+               .select()
+               .from(AUTHOR)
+               .fetch()
+        );
+
+        Tools.title("Using jOOQ with a DBCP connection pool");
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName(Tools.driver());
+        ds.setUrl(Tools.url());
+        ds.setUsername(Tools.username());
+        ds.setPassword(Tools.password());
+        System.out.println(
+            DSL.using(ds, SQLDialect.H2)
+               .select()
+               .from(AUTHOR)
+               .fetch()
+        );
+    }
+}

7456d5fa1a0e16201ad22f29ff6a6fdd5c97ea09
Lukas Eder <lukas.eder@gmail.com>
2014-10-14 11:57:41 +0200

    [jOOQ/jOOQ#3507] Bad @Support annotation on Select.intersect() and Select.except()

--- jOOQ/src/main/java/org/jooq/Select.java
+++ jOOQ/src/main/java/org/jooq/Select.java
@@ -40,6 +40,21 @@
  */
 package org.jooq;
 
+import static org.jooq.SQLDialect.ASE;
+import static org.jooq.SQLDialect.CUBRID;
+import static org.jooq.SQLDialect.DB2;
+import static org.jooq.SQLDialect.DERBY;
+import static org.jooq.SQLDialect.H2;
+import static org.jooq.SQLDialect.HSQLDB;
+import static org.jooq.SQLDialect.INGRES;
+import static org.jooq.SQLDialect.MARIADB;
+import static org.jooq.SQLDialect.MYSQL;
+import static org.jooq.SQLDialect.ORACLE;
+import static org.jooq.SQLDialect.POSTGRES;
+import static org.jooq.SQLDialect.SQLITE;
+import static org.jooq.SQLDialect.SQLSERVER;
+import static org.jooq.SQLDialect.SYBASE;
+
 import java.util.List;
 
 import org.jooq.api.annotation.State;
@@ -71,13 +86,13 @@ public interface Select<R extends Record> extends ResultQuery<R>, TableLike<R>, 
     /**
      * Combine with other selects
      */
-    @Support
+    @Support({ ASE, CUBRID, DB2, DERBY, H2, HSQLDB, INGRES, MARIADB, MYSQL, ORACLE, POSTGRES, SQLITE, SQLSERVER, SYBASE })
     Select<R> except(Select<? extends R> select);
 
     /**
      * Combine with other selects
      */
-    @Support
+    @Support({ ASE, CUBRID, DB2, DERBY, H2, HSQLDB, INGRES, MARIADB, MYSQL, ORACLE, POSTGRES, SQLITE, SQLSERVER, SYBASE })
     Select<R> intersect(Select<? extends R> select);
 
     /**

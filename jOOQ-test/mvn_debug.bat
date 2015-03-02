4235877e565340a35f466270fe86c5c669a662e4
lukaseder <lukas.eder@gmail.com>
2015-03-02 13:08:47 +0100

    Fixed MAVEN_OPTS env variable

--- jOOQ-test/mvn_debug.bat
+++ jOOQ-test/mvn_debug.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 
 IF "%1" == "off" (
-    SET MAVEN_OPTS=
+    SET MAVEN_OPTS=-Djava.library.path=C:\sqljdbc_4.0\enu\auth\x64
 ) ELSE (
-    SET MAVEN_OPTS=-Xdebug -Xnoagent -Djava.compile=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005
+    SET MAVEN_OPTS=-Djava.library.path=C:\sqljdbc_4.0\enu\auth\x64 -Xdebug -Xnoagent -Djava.compile=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005
 )
 
 SET MAVEN_OPTS

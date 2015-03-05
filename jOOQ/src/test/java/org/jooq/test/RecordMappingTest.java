e1158079afa287c826bce6a0f9d684769dd7e668
Lukas Eder <lukas.eder@gmail.com>
2015-03-05 17:50:31 +0100

    [jOOQ/jOOQ#4128] @javax.persistence.Column.name() is case-insensitive

--- jOOQ/src/test/java/org/jooq/test/RecordMappingTest.java
+++ jOOQ/src/test/java/org/jooq/test/RecordMappingTest.java
@@ -42,6 +42,7 @@
 package org.jooq.test;
 
 import static org.jooq.impl.DSL.field;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
@@ -50,6 +51,7 @@ import javax.persistence.Column;
 
 import org.jooq.Field;
 import org.jooq.Record1;
+import org.jooq.Record2;
 import org.jooq.Result;
 
 import org.junit.Test;
@@ -104,6 +106,79 @@ public class RecordMappingTest extends AbstractTest {
         @Override
         public String toString() {
             return "Boolean [oneZero=" + oneZero + ", oneZero1=" + oneZero1 + ", oneZero2=" + oneZero2 + "]";
+        }
+    }
+
+    @Test
+    public void testCaseSensitivity() throws Exception {
+
+        // [#4128] Case sensitive @Column annotations have to be dealt with correctly
+        Field<Boolean> field1 = field("a", Boolean.class);
+        Field<Boolean> field2 = field("B", Boolean.class);
+        Record2<Boolean, Boolean> record = create.newRecord(field1, field2);
+        record.setValue(field1, true);
+        record.setValue(field2, true);
+
+        // [#3101] Make sure this works for both "get" and "is" annotated getters
+        CaseSensitiveWithAnnotations pojo = record.into(CaseSensitiveWithAnnotations.class);
+        assertTrue(pojo.a1);
+        assertTrue(pojo.a2);
+        assertFalse(pojo.a3);
+        assertTrue(pojo.a4);
+
+        assertTrue(pojo.b1);
+        assertTrue(pojo.b2);
+        assertTrue(pojo.b3);
+        assertFalse(pojo.b4);
+    }
+
+    public static class CaseSensitiveWithAnnotations {
+
+        // Insensitive
+        @Column(name = "A")
+        boolean a1;
+        @Column(name = "a")
+        boolean a2;
+
+        // Sensitive
+        @Column(name = "\"A\"")
+        boolean a3;
+        @Column(name = "\"a\"")
+        boolean a4;
+
+        boolean b1;
+        boolean b2;
+        boolean b3;
+        boolean b4;
+
+        public void setB1(boolean b1) {
+            this.b1 = b1;
+        }
+        public void setB2(boolean b2) {
+            this.b2 = b2;
+        }
+        public void setB3(boolean b3) {
+            this.b3 = b3;
+        }
+        public void setB4(boolean b4) {
+            this.b4 = b4;
+        }
+
+        @Column(name = "B")
+        public boolean getB1() {
+            return b1;
+        }
+        @Column(name = "b")
+        public boolean getB2() {
+            return b2;
+        }
+        @Column(name = "\"B\"")
+        public boolean getB3() {
+            return b3;
+        }
+        @Column(name = "\"b\"")
+        public boolean getB4() {
+            return b4;
         }
     }
 

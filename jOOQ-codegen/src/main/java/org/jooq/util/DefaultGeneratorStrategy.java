02af0c849acf33ee3703a036e29ee269e1fe96b9
Lukas Eder <lukas.eder@gmail.com>
2015-03-10 17:43:14 +0100

    [#4132] GeneratorStrategy.instanceFields should be true by default

--- jOOQ-codegen/src/main/java/org/jooq/util/DefaultGeneratorStrategy.java
+++ jOOQ-codegen/src/main/java/org/jooq/util/DefaultGeneratorStrategy.java
@@ -56,9 +56,9 @@ import org.jooq.tools.StringUtils;
  */
 public class DefaultGeneratorStrategy extends AbstractGeneratorStrategy {
 
-    private String                           targetDirectory;
-    private String                           targetPackage;
-    private boolean                          instanceFields;
+    private String  targetDirectory;
+    private String  targetPackage;
+    private boolean instanceFields = true;
 
     // -------------------------------------------------------------------------
     // Initialisation

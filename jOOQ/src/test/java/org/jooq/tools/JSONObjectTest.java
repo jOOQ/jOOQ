5d1273f74d820399e9bcef47e9df0d12b50784e1
lukaseder <lukas.eder@gmail.com>
2015-04-21 09:51:17 +0200

    [#4219] Adapted License header to ASL 2.0 only

--- jOOQ/src/test/java/org/jooq/tools/JSONObjectTest.java
+++ jOOQ/src/test/java/org/jooq/tools/JSONObjectTest.java
@@ -1,22 +1,6 @@
-/**
- * Copyright (c) 2009-2015, Data Geekery GmbH (http://www.datageekery.com)
- * All rights reserved.
+/*
+ * Copyright (c) 2015 by Data Geekery GmbH (http://www.datageekery.com)
  *
- * This work is dual-licensed
- * - under the Apache Software License 2.0 (the "ASL")
- * - under the jOOQ License and Maintenance Agreement (the "jOOQ License")
- * =============================================================================
- * You may choose which license applies to you:
- *
- * - If you're using this work with Open Source databases, you may choose
- *   either ASL or jOOQ License.
- * - If you're using this work with at least one commercial database, you must
- *   choose jOOQ License
- *
- * For more information, please visit http://www.jooq.org/licenses
- *
- * Apache Software License 2.0:
- * -----------------------------------------------------------------------------
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -28,24 +12,17 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * jOOQ License and Maintenance Agreement:
- * -----------------------------------------------------------------------------
- * Data Geekery grants the Customer the non-exclusive, timely limited and
- * non-transferable license to install and use the Software under the terms of
- * the jOOQ License and Maintenance Agreement.
- *
- * This library is distributed with a LIMITED WARRANTY. See the jOOQ License
- * and Maintenance Agreement for more details: http://www.jooq.org/licensing
  */
 package org.jooq.tools;
 
-import java.util.HashMap;
-import org.jooq.tools.json.JSONObject;
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+
+import org.jooq.tools.json.JSONObject;
+
+import org.junit.Test;
 
 /**
  * @author Alok Menghrajani

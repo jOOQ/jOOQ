20f1c71e058555edbff8c39a249ead2b4873480e
lukaseder <lukas.eder@gmail.com>
2015-05-06 11:09:46 +0200

    [jOOQ/jOOQ#4272] Add missing <E> E ResultQuery.fetchAny(RecordMapper<? super R, E>)

--- jOOQ/src/main/java/org/jooq/impl/SelectImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/SelectImpl.java
@@ -2513,6 +2513,11 @@ class SelectImpl<R extends Record, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,
     }
 
     @Override
+    public final <E> E fetchAny(RecordMapper<? super R, E> mapper) {
+        return getDelegate().fetchAny(mapper);
+    }
+
+    @Override
     public final Map<String, Object> fetchAnyMap() {
         return getDelegate().fetchAnyMap();
     }

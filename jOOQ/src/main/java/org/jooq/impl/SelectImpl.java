3be9e01da23bfbc9e4ea0a5e72cbef76e3ed4fdd
lukaseder <lukas.eder@gmail.com>
2015-05-13 07:41:35 +0200

    Fixed OSS edition compilation error

--- jOOQ/src/main/java/org/jooq/impl/SelectImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/SelectImpl.java
@@ -2250,16 +2250,6 @@ class SelectImpl<R extends Record, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,
     }
 
     @Override
-    public final SelectImpl straightJoin(String sql, Object... bindings) {
-        return straightJoin(table(sql, bindings));
-    }
-
-    @Override
-    public final SelectImpl straightJoin(String sql, QueryPart... parts) {
-        return straightJoin(table(sql, parts));
-    }
-
-    @Override
     public final SelectImpl partitionBy(Field<?>... fields) {
         joinPartitionBy = fields;
         return this;
@@ -2271,6 +2261,17 @@ class SelectImpl<R extends Record, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,
     }
 
     /* [/pro] */
+
+    @Override
+    public final SelectImpl straightJoin(String sql, Object... bindings) {
+        return straightJoin(table(sql, bindings));
+    }
+
+    @Override
+    public final SelectImpl straightJoin(String sql, QueryPart... parts) {
+        return straightJoin(table(sql, parts));
+    }
+
     @Override
     public final ResultQuery<R> maxRows(int rows) {
         return getDelegate().maxRows(rows);

7f2262c5394aad38e9469b547b6e64676d04c207
JackG <jacques.gonzalez@gmail.com>
2015-07-14 11:49:00 +0200

    Imports cleanup

--- jOOQ-examples/jOOQ-spring-boot-example/src/main/java/org/jooq/example/spring/Application.java
+++ jOOQ-examples/jOOQ-spring-boot-example/src/main/java/org/jooq/example/spring/Application.java
@@ -1,21 +1,7 @@
 package org.jooq.example.spring;
 
-import javax.sql.DataSource;
-
-import org.jooq.ConnectionProvider;
-import org.jooq.DSLContext;
-import org.jooq.ExecuteListenerProvider;
-import org.jooq.SQLDialect;
-import org.jooq.TransactionProvider;
-import org.jooq.impl.DataSourceConnectionProvider;
-import org.jooq.impl.DefaultConfiguration;
-import org.jooq.impl.DefaultDSLContext;
-import org.jooq.impl.DefaultExecuteListenerProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 
 /**
  * @author Thomas Darimont
@@ -23,8 +9,8 @@ import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }

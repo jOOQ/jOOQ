7ee8f7def996d06e2f6ec0ad7d5a3454d0bfb036
lukaseder <lukas.eder@gmail.com>
2015-08-26 11:46:12 +0200

    Added SQL Server test class

---
+++ jOOQ-test/src/main/java/SQLServer.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2009-2015, Data Geekery GmbH (http://www.datageekery.com)
+ * All rights reserved.
+ *
+ * This work is dual-licensed
+ * - under the Apache Software License 2.0 (the "ASL")
+ * - under the jOOQ License and Maintenance Agreement (the "jOOQ License")
+ * =============================================================================
+ * You may choose which license applies to you:
+ *
+ * - If you're using this work with Open Source databases, you may choose
+ *   either ASL or jOOQ License.
+ * - If you're using this work with at least one commercial database, you must
+ *   choose jOOQ License
+ *
+ * For more information, please visit http://www.jooq.org/licenses
+ *
+ * Apache Software License 2.0:
+ * -----------------------------------------------------------------------------
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * jOOQ License and Maintenance Agreement:
+ * -----------------------------------------------------------------------------
+ * Data Geekery grants the Customer the non-exclusive, timely limited and
+ * non-transferable license to install and use the Software under the terms of
+ * the jOOQ License and Maintenance Agreement.
+ *
+ * This library is distributed with a LIMITED WARRANTY. See the jOOQ License
+ * and Maintenance Agreement for more details: http://www.jooq.org/licensing
+ */
+import java.sql.Connection;
+import java.util.Properties;
+
+import org.jooq.impl.DSL;
+
+public class SQLServer {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+            // Run with -Djava.library.path=C:\sqljdbc_4.0\enu\auth\x64
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=test;integratedSecurity=true";
+
+
+            System.out.println("Connect");
+            Connection cn = new com.microsoft.sqlserver.jdbc.SQLServerDriver().connect(url, new Properties());
+
+            System.out.println("Select");
+
+            String sql = "declare @result table(i int); insert into @result values (1); select * from @result;";
+            System.out.println(DSL.using(cn).fetch(sql));
+            System.out.println(DSL.using(cn).fetchMany(sql));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

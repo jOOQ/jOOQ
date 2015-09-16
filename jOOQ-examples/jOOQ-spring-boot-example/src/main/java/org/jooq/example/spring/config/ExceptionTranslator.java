9eb57e8c240a6db182ba10790f6215b182916391
lukaseder <lukas.eder@gmail.com>
2015-09-16 20:47:30 +0200

    [jOOQ/jOOQ#4557] ExceptionTranslator in spring examples shouldn't use dialect.name() for SQLErrorCodeSQLExceptionTranslator

--- jOOQ-examples/jOOQ-spring-boot-example/src/main/java/org/jooq/example/spring/config/ExceptionTranslator.java
+++ jOOQ-examples/jOOQ-spring-boot-example/src/main/java/org/jooq/example/spring/config/ExceptionTranslator.java
@@ -1,36 +1,78 @@
-package org.jooq.example.spring.config;
+/**
+ * Copyright (c) 2011-2013, Data Geekery GmbH, contact@datageekery.com
+ * All rights reserved.
+ *
+ * This software is licensed to you under the Apache License, Version 2.0
+ * (the "License"); You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * . Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * . Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * . Neither the name "jOOQ" nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.jooq.example.spring.exception;
 
 import org.jooq.ExecuteContext;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DefaultExecuteListener;
+
 import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
 import org.springframework.jdbc.support.SQLExceptionTranslator;
 import org.springframework.jdbc.support.SQLStateSQLExceptionTranslator;
 
 /**
- * This class transforms SQLException into a Spring specific DataAccessException. The idea behind this is borrowed from
- * Adam Zell's Gist
+ * This class transforms SQLException into a Spring specific
+ * DataAccessException. The idea behind this is borrowed from Adam Zell's Gist
  *
  * @author Petri Kainulainen
  * @author Adam Zell
  * @author Lukas Eder
  * @see <a
  *      href="http://www.petrikainulainen.net/programming/jooq/using-jooq-with-spring-configuration/">http://www.petrikainulainen.net/programming/jooq/using-jooq-with-spring-configuration/</a>
- * @see <a href="https://gist.github.com/azell/5655888">https://gist.github.com/azell/5655888</a>
+ * @see <a
+ *      href="https://gist.github.com/azell/5655888">https://gist.github.com/azell/5655888</a>
  */
 public class ExceptionTranslator extends DefaultExecuteListener {
 
-	/**
-	 * Generated UID
-	 */
-	private static final long serialVersionUID = -2450323227461061152L;
+    /**
+     * Generated UID
+     */
+    private static final long serialVersionUID = -2450323227461061152L;
 
-	@Override
-	public void exception(ExecuteContext ctx) {
-		SQLDialect dialect = ctx.configuration().dialect();
-		SQLExceptionTranslator translator = (dialect != null) ? new SQLErrorCodeSQLExceptionTranslator(dialect.name())
-				: new SQLStateSQLExceptionTranslator();
+    @Override
+    public void exception(ExecuteContext ctx) {
 
-		ctx.exception(translator.translate("jOOQ", ctx.sql(), ctx.sqlException()));
-	}
+        // [#4391] Translate only SQLExceptions
+        if (ctx.sqlException() != null) {
+            SQLDialect dialect = ctx.dialect();
+            SQLExceptionTranslator translator = (dialect != null)
+                    ? new SQLErrorCodeSQLExceptionTranslator(dialect.thirdParty().springDbName())
+                    : new SQLStateSQLExceptionTranslator();
+
+            ctx.exception(translator.translate("jOOQ", ctx.sql(), ctx.sqlException()));
+        }
+    }
 }

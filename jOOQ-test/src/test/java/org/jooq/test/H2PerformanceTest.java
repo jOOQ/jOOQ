b535ea0f7858f0b044b8a7847f43499e9f7010e8
lukaseder <lukas.eder@gmail.com>
2015-10-15 14:30:40 +0200

    Fixed H2PerformanceTest compilation errors

--- jOOQ-test/src/test/java/org/jooq/test/H2PerformanceTest.java
+++ jOOQ-test/src/test/java/org/jooq/test/H2PerformanceTest.java
@@ -44,6 +44,8 @@ package org.jooq.test;
 import static org.jooq.SQLDialect.H2;
 import static org.jooq.lambda.Unchecked.intConsumer;
 import static org.jooq.lambda.Unchecked.runnable;
+import static org.jooq.test.h2.generatedclasses.public_.Tables.T_PERFORMANCE_JDBC;
+import static org.jooq.test.h2.generatedclasses.public_.Tables.T_PERFORMANCE_JOOQ;
 import static org.junit.Assert.assertTrue;
 
 import java.sql.Connection;

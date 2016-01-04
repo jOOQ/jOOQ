a1312e0e15b30e2c4895c00743a2053cb9163493
lukaseder <lukas.eder@gmail.com>
2016-01-04 16:50:19 +0100

    [#4893] Add Kotlin example to GitHub

---
+++ jOOQ-examples/jOOQ-kotlin-example/src/main/kotlin/org/jooq/example/kotlin/Utils.kt
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2009-2015, Data Geekery GmbH (http://www.datageekery.com)
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Other licenses:
+ * -----------------------------------------------------------------------------
+ * Commercial licenses for this work are available. These replace the above
+ * ASL 2.0 and offer limited warranties, support, maintenance, and commercial
+ * database integrations.
+ *
+ * For more information, please visit: http://www.jooq.org/licenses
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+package org.jooq.example.kotlin
+
+// This is from the stdlib, which doesn't support AutoCloseable yet
+// https://github.com/JetBrains/kotlin/pull/807
+inline fun <T : AutoCloseable, R> T.use(block: (T) -> R): R {
+    var closed = false
+    try {
+        return block(this)
+    } catch (e: Exception) {
+        closed = true
+        try {
+            close()
+        } catch (closeException: Exception) {
+            // eat the closeException as we are already throwing the original cause
+            // and we don't want to mask the real exception
+
+            // TODO on Java 7 we should call
+            // e.addSuppressed(closeException)
+            // to work like try-with-resources
+            // http://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html#suppressed-exceptions
+        }
+        throw e
+    } finally {
+        if (!closed) {
+            close()
+        }
+    }
+}

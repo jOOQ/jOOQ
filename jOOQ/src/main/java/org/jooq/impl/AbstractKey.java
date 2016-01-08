38cf47ef50dacbba3c87c1a620c67f706458d0d1
lukaseder <lukas.eder@gmail.com>
2016-01-08 16:53:46 +0100

    [#4916] Implement Key.equals() and hashCode() for all Key types

--- jOOQ/src/main/java/org/jooq/impl/AbstractKey.java
+++ jOOQ/src/main/java/org/jooq/impl/AbstractKey.java
@@ -99,4 +99,37 @@ abstract class AbstractKey<R extends Record> implements Key<R> {
     public final TableField<R, ?>[] getFieldsArray() {
         return fields;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((table == null) ? 0 : table.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AbstractKey<?> other = (AbstractKey<?>) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        }
+        else if (!name.equals(other.name))
+            return false;
+        if (table == null) {
+            if (other.table != null)
+                return false;
+        }
+        else if (!table.equals(other.table))
+            return false;
+        return true;
+    }
 }

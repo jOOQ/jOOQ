2e3d7d02600c95dafc853aff19894b7106f8588e
lukaseder <lukas.eder@gmail.com>
2016-10-31 14:41:19 +0100

    [#5621] Add FieldEscapeStep as a return type for Field.like(), such that the ESCAPE keyword can be used explicitly

---
+++ jOOQ/src/main/java/org/jooq/LikeEscapeStep.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2009-2016, Data Geekery GmbH (http://www.datageekery.com)
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Other licenses:
+ * -----------------------------------------------------------------------------
+ * Commercial licenses for this work are available. These replace the above
+ * ASL 2.0 and offer limited warranties, support, maintenance, and commercial
+ * database integrations.
+ *
+ * For more information, please visit: http://www.jooq.org/licenses
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+package org.jooq;
+
+// ...
+import static org.jooq.SQLDialect.CUBRID;
+// ...
+import static org.jooq.SQLDialect.DERBY;
+import static org.jooq.SQLDialect.FIREBIRD;
+import static org.jooq.SQLDialect.H2;
+// ...
+import static org.jooq.SQLDialect.HSQLDB;
+// ...
+import static org.jooq.SQLDialect.MARIADB;
+import static org.jooq.SQLDialect.MYSQL;
+// ...
+import static org.jooq.SQLDialect.POSTGRES;
+import static org.jooq.SQLDialect.SQLITE;
+// ...
+// ...
+// ...
+
+/**
+ * A step in the creation of a <code>LIKE</code> predicate to which an
+ * <code>ESCAPE</code> clause can be appended.
+ *
+ * @author Lukas Eder
+ */
+public interface LikeEscapeStep extends Condition {
+
+
+    /**
+     * Add an <code>ESCAPE</code> clause to the <code>LIKE</code> predicate.
+     * <p>
+     * For example:
+     *
+     * <code><pre>
+     * some_column LIKE 'A!%%' ESCAPE '!'
+     * </pre></code>
+     */
+    @Support({ CUBRID, DERBY, FIREBIRD, H2, HSQLDB, MARIADB, MYSQL, POSTGRES, SQLITE })
+    Condition escape(char c);
+}

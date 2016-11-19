80e29d1a623beebd2b587a95beabbf570465bb72
samy deghou <samy.deghou@gmail.com>
2016-11-19 14:06:12 +0100

    adding failing unit test to #5639

--- jOOQ/src/test/java/org/jooq/tools/jdbc/MockFileDatabaseTest.java
+++ jOOQ/src/test/java/org/jooq/tools/jdbc/MockFileDatabaseTest.java
@@ -1,7 +1,33 @@
 package org.jooq.tools.jdbc;
 
+import org.jooq.exception.ErroneousRowSpecificationException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * Created by deghou on 19/11/2016.
  */
-public class MockFileDatabaseTest {
+public class MockFileDatabaseTest{
+
+    @Before
+    public void setUp(){
+
+    }
+
+    @After
+    public void tearDown(){
+
+    }
+
+
+    @Test(expected= ErroneousRowSpecificationException.class)
+    public void test() throws IOException {
+        InputStream fileName = this.getClass().getResourceAsStream("/mock-file-database-file-2.txt");
+        MockFileDatabase mockFileDatabase = new MockFileDatabase(fileName);
+    }
+
 }

432a5648e5af55d2462459ea2ee8e6681edeeb53
lukaseder <lukas.eder@gmail.com>
2016-12-28 15:03:30 +0100

    [#5735] jOOQ-checker is not deployed to Maven Central

--- jOOQ-release/release/template/maven-install.bat
+++ jOOQ-release/release/template/maven-install.bat
@@ -7,6 +7,7 @@ if exist jOOQ-javadoc\jooq-%VERSION%-javadoc.jar (
   set JAVADOC_JOOQ_META_EXTENSIONS=-Djavadoc=jOOQ-javadoc\jooq-meta-extensions-%VERSION%-javadoc.jar
   set JAVADOC_JOOQ_CODEGEN=-Djavadoc=jOOQ-javadoc\jooq-codegen-%VERSION%-javadoc.jar
   set JAVADOC_JOOQ_CODEGEN_MAVEN=-Djavadoc=jOOQ-javadoc\jooq-codegen-maven-%VERSION%-javadoc.jar
+  set JAVADOC_JOOQ_CHECKER=-Djavadoc=jOOQ-javadoc\jooq-checker-%VERSION%-javadoc.jar
   set JAVADOC_JOOQ_SCALA=-Djavadoc=jOOQ-javadoc\jooq-scala-%VERSION%-javadoc.jar
 )
 
@@ -16,6 +17,7 @@ if exist jOOQ-src\jooq-%VERSION%-sources.jar (
   set SOURCES_JOOQ_META_EXTENSIONS=-Dsources=jOOQ-src\jooq-meta-extensions-%VERSION%-sources.jar
   set SOURCES_JOOQ_CODEGEN=-Dsources=jOOQ-src\jooq-codegen-%VERSION%-sources.jar
   set SOURCES_JOOQ_CODEGEN_MAVEN=-Dsources=jOOQ-src\jooq-codegen-maven-%VERSION%-sources.jar
+  set SOURCES_JOOQ_CHECKER=-Dsources=jOOQ-src\jooq-checker-%VERSION%-sources.jar
   set SOURCES_JOOQ_SCALA=-Dsources=jOOQ-src\jooq-scala-%VERSION%-sources.jar
 )
 
@@ -25,6 +27,7 @@ call mvn install:install-file -Dfile=jOOQ-lib\jooq-meta-%VERSION%.jar           
 call mvn install:install-file -Dfile=jOOQ-lib\jooq-meta-extensions-%VERSION%.jar -DgroupId=org.jooq -DartifactId=jooq-meta-extensions -Dversion=%VERSION% -Dpackaging=jar %JAVADOC_JOOQ_META_EXTENSIONS% %SOURCES_JOOQ_META_EXTENSIONS% -DpomFile=jOOQ-pom\jooq-meta-extensions\pom.xml
 call mvn install:install-file -Dfile=jOOQ-lib\jooq-codegen-%VERSION%.jar         -DgroupId=org.jooq -DartifactId=jooq-codegen         -Dversion=%VERSION% -Dpackaging=jar %JAVADOC_JOOQ_CODEGEN%         %SOURCES_JOOQ_CODEGEN%         -DpomFile=jOOQ-pom\jooq-codegen\pom.xml
 call mvn install:install-file -Dfile=jOOQ-lib\jooq-codegen-maven-%VERSION%.jar   -DgroupId=org.jooq -DartifactId=jooq-codegen-maven   -Dversion=%VERSION% -Dpackaging=jar %JAVADOC_JOOQ_CODEGEN_MAVEN%   %SOURCES_JOOQ_CODEGEN_META%    -DpomFile=jOOQ-pom\jooq-codegen-maven\pom.xml
+call mvn install:install-file -Dfile=jOOQ-lib\jooq-checker-%VERSION%.jar         -DgroupId=org.jooq -DartifactId=jooq-checker         -Dversion=%VERSION% -Dpackaging=jar %JAVADOC_JOOQ_CHECKER%         %SOURCES_JOOQ_CHECKER%         -DpomFile=jOOQ-pom\jooq-checker\pom.xml
 call mvn install:install-file -Dfile=jOOQ-lib\jooq-scala-%VERSION%.jar           -DgroupId=org.jooq -DartifactId=jooq-scala           -Dversion=%VERSION% -Dpackaging=jar %JAVADOC_JOOQ_SCALA%           %SOURCES_JOOQ_SCALA%           -DpomFile=jOOQ-pom\jooq-scala\pom.xml
 
 echo
--- jOOQ-release/release/template/maven-install.sh
+++ jOOQ-release/release/template/maven-install.sh
@@ -7,6 +7,7 @@ if [ -f jOOQ-javadoc/jooq-$VERSION-javadoc.jar ]; then
   JAVADOC_JOOQ_META_EXTENSIONS=-Djavadoc=jOOQ-javadoc/jooq-meta-extensions-$VERSION-javadoc.jar
   JAVADOC_JOOQ_CODEGEN=-Djavadoc=jOOQ-javadoc/jooq-codegen-$VERSION-javadoc.jar
   JAVADOC_JOOQ_CODEGEN_MAVEN=-Djavadoc=jOOQ-javadoc/jooq-codegen-maven-$VERSION-javadoc.jar
+  JAVADOC_JOOQ_CHECKER=-Djavadoc=jOOQ-javadoc/jooq-checker-$VERSION-javadoc.jar
   JAVADOC_JOOQ_SCALA=-Djavadoc=jOOQ-javadoc/jooq-scala-$VERSION-javadoc.jar
 fi
 
@@ -16,6 +17,7 @@ if [ -f jOOQ-src/jooq-$VERSION-sources.jar ]; then
   SOURCES_JOOQ_META_EXTENSIONS=-Dsources=jOOQ-src/jooq-meta-extensions-$VERSION-sources.jar
   SOURCES_JOOQ_CODEGEN=-Dsources=jOOQ-src/jooq-codegen-$VERSION-sources.jar
   SOURCES_JOOQ_CODEGEN_MAVEN=-Dsources=jOOQ-src/jooq-codegen-maven-$VERSION-sources.jar
+  SOURCES_JOOQ_CHECKER=-Dsources=jOOQ-src/jooq-checker-$VERSION-sources.jar
   SOURCES_JOOQ_SCALA=-Dsources=jOOQ-src/jooq-scala-$VERSION-sources.jar
 fi
 
@@ -25,6 +27,7 @@ mvn install:install-file -Dfile=jOOQ-lib/jooq-meta-$VERSION.jar            -Dgro
 mvn install:install-file -Dfile=jOOQ-lib/jooq-meta-extensions-$VERSION.jar -DgroupId=org.jooq -DartifactId=jooq-meta-extensions -Dversion=$VERSION -Dpackaging=jar $JAVADOC_JOOQ_META_EXTENSIONS $SOURCES_JOOQ_META_EXTENSIONS -DpomFile=jOOQ-pom/jooq-meta-extensions/pom.xml
 mvn install:install-file -Dfile=jOOQ-lib/jooq-codegen-$VERSION.jar         -DgroupId=org.jooq -DartifactId=jooq-codegen         -Dversion=$VERSION -Dpackaging=jar $JAVADOC_JOOQ_CODEGEN         $SOURCES_JOOQ_CODEGEN         -DpomFile=jOOQ-pom/jooq-codegen/pom.xml
 mvn install:install-file -Dfile=jOOQ-lib/jooq-codegen-maven-$VERSION.jar   -DgroupId=org.jooq -DartifactId=jooq-codegen-maven   -Dversion=$VERSION -Dpackaging=jar $JAVADOC_JOOQ_CODEGEN_MAVEN   $SOURCES_JOOQ_CODEGEN_META    -DpomFile=jOOQ-pom/jooq-codegen-maven/pom.xml
+mvn install:install-file -Dfile=jOOQ-lib/jooq-checker-$VERSION.jar         -DgroupId=org.jooq -DartifactId=jooq-checker         -Dversion=$VERSION -Dpackaging=jar $JAVADOC_JOOQ_CHEKER          $SOURCES_JOOQ_CHECKER         -DpomFile=jOOQ-pom/jooq-checker/pom.xml
 mvn install:install-file -Dfile=jOOQ-lib/jooq-scala-$VERSION.jar           -DgroupId=org.jooq -DartifactId=jooq-scala           -Dversion=$VERSION -Dpackaging=jar $JAVADOC_JOOQ_SCALA           $SOURCES_JOOQ_SCALA           -DpomFile=jOOQ-pom/jooq-scala/pom.xml
 
 echo

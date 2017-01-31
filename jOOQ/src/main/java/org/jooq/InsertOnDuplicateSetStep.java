6d87288883cc5239140065c263dfa1d20ee0063a
lukaseder <lukas.eder@gmail.com>
2017-01-31 16:30:35 +0100

    [#5820] Missing @Support annotations for PostgreSQL on InsertOnDuplicateSetStep.set()

--- jOOQ/src/main/java/org/jooq/InsertOnDuplicateSetStep.java
+++ jOOQ/src/main/java/org/jooq/InsertOnDuplicateSetStep.java
@@ -40,6 +40,7 @@ import static org.jooq.SQLDialect.HSQLDB;
 import static org.jooq.SQLDialect.MARIADB;
 import static org.jooq.SQLDialect.MYSQL;
 // ...
+import static org.jooq.SQLDialect.POSTGRES_9_5;
 // ...
 // ...
 
@@ -68,21 +69,21 @@ public interface InsertOnDuplicateSetStep<R extends Record> {
      * Set values for <code>UPDATE</code> in the <code>INSERT</code> statement's
      * <code>ON DUPLICATE KEY UPDATE</code> clause.
      */
-    @Support({ CUBRID, HSQLDB, MARIADB, MYSQL })
+    @Support({ CUBRID, HSQLDB, MARIADB, MYSQL, POSTGRES_9_5 })
     <T> InsertOnDuplicateSetMoreStep<R> set(Field<T> field, T value);
 
     /**
      * Set values for <code>UPDATE</code> in the <code>INSERT</code> statement's
      * <code>ON DUPLICATE KEY UPDATE</code> clause.
      */
-    @Support({ CUBRID, HSQLDB, MARIADB, MYSQL })
+    @Support({ CUBRID, HSQLDB, MARIADB, MYSQL, POSTGRES_9_5 })
     <T> InsertOnDuplicateSetMoreStep<R> set(Field<T> field, Field<T> value);
 
     /**
      * Set values for <code>UPDATE</code> in the <code>INSERT</code> statement's
      * <code>ON DUPLICATE KEY UPDATE</code> clause.
      */
-    @Support({ CUBRID, HSQLDB, MARIADB, MYSQL })
+    @Support({ CUBRID, HSQLDB, MARIADB, MYSQL, POSTGRES_9_5 })
     <T> InsertOnDuplicateSetMoreStep<R> set(Field<T> field, Select<? extends Record1<T>> value);
 
     /**
@@ -93,7 +94,7 @@ public interface InsertOnDuplicateSetStep<R extends Record> {
      * <code>Field&lt;T&gt;</code>. jOOQ will attempt to convert values to their
      * corresponding field's type.
      */
-    @Support({ CUBRID, HSQLDB, MARIADB, MYSQL })
+    @Support({ CUBRID, HSQLDB, MARIADB, MYSQL, POSTGRES_9_5 })
     InsertOnDuplicateSetMoreStep<R> set(Map<? extends Field<?>, ?> map);
 
     /**
@@ -105,7 +106,7 @@ public interface InsertOnDuplicateSetStep<R extends Record> {
      *
      * @see #set(Map)
      */
-    @Support({ CUBRID, HSQLDB, MARIADB, MYSQL })
+    @Support({ CUBRID, HSQLDB, MARIADB, MYSQL, POSTGRES_9_5 })
     InsertOnDuplicateSetMoreStep<R> set(Record record);
 
 }

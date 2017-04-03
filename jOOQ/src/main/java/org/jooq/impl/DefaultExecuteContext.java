85d25f13270311345ec18adf39d3b5f214a6ed6f
lukaseder <lukas.eder@gmail.com>
2017-04-03 14:36:26 +0100

    Possible leaking JDBC Arrays in DefaultExecuteContext.ARRAYS - Merge [#6038]

--- jOOQ/src/main/java/org/jooq/impl/DefaultExecuteContext.java
+++ jOOQ/src/main/java/org/jooq/impl/DefaultExecuteContext.java
@@ -179,7 +179,7 @@ class DefaultExecuteContext implements ExecuteContext {
                 JDBCUtils.safeFree(array);
             }
 
-            SQLXMLS.remove();
+            ARRAYS.remove();
         }
 
         LOCAL_CONFIGURATION.remove();

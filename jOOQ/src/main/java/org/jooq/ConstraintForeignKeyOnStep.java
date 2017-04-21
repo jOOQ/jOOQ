908b034d30ed491721c6d91f459e30c00009876a
lukaseder <lukas.eder@gmail.com>
2017-04-21 22:13:48 +0200

    [#6130] Cannot combine ON DELETE and ON UPDATE actions on foreign key specifications

--- jOOQ/src/main/java/org/jooq/ConstraintForeignKeyOnStep.java
+++ jOOQ/src/main/java/org/jooq/ConstraintForeignKeyOnStep.java
@@ -66,69 +66,69 @@ public interface ConstraintForeignKeyOnStep extends ConstraintFinalStep {
      * <code>FOREIGN KEY</code> constraint.
      */
     @Support({ CUBRID, DERBY, FIREBIRD, H2, HSQLDB, MARIADB, MYSQL, POSTGRES, SQLITE })
-    ConstraintFinalStep onDeleteNoAction();
+    ConstraintForeignKeyOnStep onDeleteNoAction();
 
     /**
      * Add an <code>ON DELETE RESTRICT</code> clause to the
      * <code>FOREIGN KEY</code> constraint.
      */
     @Support({ CUBRID, DERBY, FIREBIRD, H2, HSQLDB, MARIADB, MYSQL, POSTGRES, SQLITE })
-    ConstraintFinalStep onDeleteRestrict();
+    ConstraintForeignKeyOnStep onDeleteRestrict();
 
     /**
      * Add an <code>ON DELETE CASCADE</code> clause to the
      * <code>FOREIGN KEY</code> constraint.
      */
     @Support
-    ConstraintFinalStep onDeleteCascade();
+    ConstraintForeignKeyOnStep onDeleteCascade();
 
     /**
      * Add an <code>ON DELETE SET NULL</code> clause to the
      * <code>FOREIGN KEY</code> constraint.
      */
     @Support
-    ConstraintFinalStep onDeleteSetNull();
+    ConstraintForeignKeyOnStep onDeleteSetNull();
 
     /**
      * Add an <code>ON DELETE SET DEFAULT</code> clause to the
      * <code>FOREIGN KEY</code> constraint.
      */
     @Support({ CUBRID, FIREBIRD, H2, HSQLDB, MARIADB, MYSQL, POSTGRES, SQLITE })
-    ConstraintFinalStep onDeleteSetDefault();
+    ConstraintForeignKeyOnStep onDeleteSetDefault();
 
     /**
      * Add an <code>ON UPDATE NO ACTION</code> clause to the
      * <code>FOREIGN KEY</code> constraint.
      */
     @Support({ CUBRID, DERBY, FIREBIRD, H2, HSQLDB, MARIADB, MYSQL, POSTGRES, SQLITE })
-    ConstraintFinalStep onUpdateNoAction();
+    ConstraintForeignKeyOnStep onUpdateNoAction();
 
     /**
      * Add an <code>ON UPDATE RESTRICT</code> clause to the
      * <code>FOREIGN KEY</code> constraint.
      */
     @Support({ CUBRID, DERBY, FIREBIRD, H2, HSQLDB, MARIADB, MYSQL, POSTGRES, SQLITE })
-    ConstraintFinalStep onUpdateRestrict();
+    ConstraintForeignKeyOnStep onUpdateRestrict();
 
     /**
      * Add an <code>ON UPDATE CASCADE</code> clause to the
      * <code>FOREIGN KEY</code> constraint.
      */
     @Support({ CUBRID, FIREBIRD, H2, HSQLDB, MARIADB, MYSQL, POSTGRES, SQLITE })
-    ConstraintFinalStep onUpdateCascade();
+    ConstraintForeignKeyOnStep onUpdateCascade();
 
     /**
      * Add an <code>ON UPDATE SET NULL</code> clause to the
      * <code>FOREIGN KEY</code> constraint.
      */
     @Support({ CUBRID, FIREBIRD, H2, HSQLDB, MARIADB, MYSQL, POSTGRES, SQLITE })
-    ConstraintFinalStep onUpdateSetNull();
+    ConstraintForeignKeyOnStep onUpdateSetNull();
 
     /**
      * Add an <code>ON UPDATE SET DEFAULT</code> clause to the
      * <code>FOREIGN KEY</code> constraint.
      */
     @Support({ CUBRID, FIREBIRD, H2, HSQLDB, MARIADB, MYSQL, POSTGRES, SQLITE })
-    ConstraintFinalStep onUpdateSetDefault();
+    ConstraintForeignKeyOnStep onUpdateSetDefault();
 
 }

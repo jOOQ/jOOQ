e84ee20a03b55b274bc7e2feabcfaa3af7bbee67
lukaseder <lukas.eder@gmail.com>
2017-04-24 11:17:00 +0200

    [#6139] Why are the files re-generated by Eclipse?

--- jOOQ/src/main/java/org/jooq/util/xml/jaxb/TableConstraint.java
+++ jOOQ/src/main/java/org/jooq/util/xml/jaxb/TableConstraint.java
@@ -1,9 +1,9 @@
-//
-// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.11 generiert
-// Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a>
-// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren.
-// Generiert: 2017.04.24 um 10:36:23 AM CEST
-//
+
+
+
+
+
+
 
 
 package org.jooq.util.xml.jaxb;

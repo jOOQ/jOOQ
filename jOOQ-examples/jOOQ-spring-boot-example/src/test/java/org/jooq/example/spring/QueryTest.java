3ab0a499aab8e576a062c75d3c2eadff3c4fa7ca
lukaseder <lukas.eder@gmail.com>
2017-05-29 14:21:34 +0200

    [#6287] Overwrite the managed version and remove duplicates

--- jOOQ-examples/jOOQ-spring-boot-example/src/test/java/org/jooq/example/spring/QueryTest.java
+++ jOOQ-examples/jOOQ-spring-boot-example/src/test/java/org/jooq/example/spring/QueryTest.java
@@ -20,13 +20,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @author Lukas Eder
  * @author Thomas Darimont
  */
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 public class QueryTest {
 
--- jOOQ-examples/jOOQ-spring-boot-example/src/test/java/org/jooq/example/spring/TransactionTest.java
+++ jOOQ-examples/jOOQ-spring-boot-example/src/test/java/org/jooq/example/spring/TransactionTest.java
@@ -16,7 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
@@ -27,7 +27,7 @@ import org.springframework.transaction.support.DefaultTransactionDefinition;
  * @see <a
  *      href="http://www.petrikainulainen.net/programming/jooq/using-jooq-with-spring-configuration/">http://www.petrikainulainen.net/programming/jooq/using-jooq-with-spring-configuration/</a>
  */
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 public class TransactionTest {
 

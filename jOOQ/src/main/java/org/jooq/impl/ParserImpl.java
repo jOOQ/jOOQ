726d5261f90ddb37e39e9015781daa5b89da7d49
lukaseder <lukas.eder@gmail.com>
2017-12-21 11:02:50 +0100

    [#6485] [#6931] Just a stylistic change...

--- jOOQ/src/main/java/org/jooq/impl/ParserImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/ParserImpl.java
@@ -1340,11 +1340,13 @@ class ParserImpl implements Parser {
         parseKeywordIf(ctx, "TABLE");
         Table<?> table = parseTableName(ctx);
 
-        RevokeOnStep s1 = null;
-        if (grantOptionFor)
-            s1 = privileges == null ? ctx.dsl.revokeGrantOptionFor(privilege) : ctx.dsl.revokeGrantOptionFor(privileges);
-        else
-            s1 = privileges == null ? ctx.dsl.revoke(privilege) : ctx.dsl.revoke(privileges);
+        RevokeOnStep s1 = grantOptionFor
+            ? privileges == null
+                ? ctx.dsl.revokeGrantOptionFor(privilege)
+                : ctx.dsl.revokeGrantOptionFor(privileges)
+            : privileges == null
+                ? ctx.dsl.revoke(privilege)
+                : ctx.dsl.revoke(privileges);
 
         parseKeyword(ctx, "FROM");
         User user = parseKeywordIf(ctx, "PUBLIC") ? null : parseUser(ctx);

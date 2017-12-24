e1bc3a178a3903ec145232518e782ac33b1af5a4
Timur Shaidullin <timur@shaidullin.net>
2017-12-24 19:38:32 +0300

    [#6659] Added support for PostgreSQL identity column

--- jOOQ/src/main/java/org/jooq/impl/Tools.java
+++ jOOQ/src/main/java/org/jooq/impl/Tools.java
@@ -3866,6 +3866,11 @@ final class Tools {
                 case DERBY:     ctx.sql(' ').visit(K_GENERATED_BY_DEFAULT_AS_IDENTITY); break;
                 case HSQLDB:    ctx.sql(' ').visit(K_GENERATED_BY_DEFAULT_AS_IDENTITY).sql('(').visit(K_START_WITH).sql(" 1)"); break;
                 case SQLITE:    ctx.sql(' ').visit(K_PRIMARY_KEY).sql(' ').visit(K_AUTOINCREMENT); break;
+                case POSTGRES:
+                    switch (ctx.dialect()) {
+                        case POSTGRES_10: ctx.sql(' ').visit(K_GENERATED_BY_DEFAULT_AS_IDENTITY); break;
+                    }
+                    break;
             }
         }
     }
@@ -3907,7 +3912,16 @@ final class Tools {
 
 
 
-                case POSTGRES: ctx.visit(type.getType() == Long.class ? K_SERIAL8 : K_SERIAL); return;
+                case POSTGRES:
+                    switch (ctx.dialect()) {
+                        case POSTGRES:
+                        case POSTGRES_9_3:
+                        case POSTGRES_9_4:
+                        case POSTGRES_9_5:
+                            ctx.visit(type.getType() == Long.class ? K_SERIAL8 : K_SERIAL);
+                    }
+
+                    return;
             }
         }
 

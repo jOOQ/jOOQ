37f116a86e0384aa1d71168ced494dc9c2dcf471
lukaseder <lukas.eder@gmail.com>
2018-01-27 15:42:13 +0100

    [#6485] Support omitting explicit limit in FETCH { FIRST | NEXT }

--- jOOQ/src/main/java/org/jooq/impl/ParserImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/ParserImpl.java
@@ -225,6 +225,7 @@ import static org.jooq.impl.Tools.EMPTY_COMMON_TABLE_EXPRESSION;
 import static org.jooq.impl.Tools.EMPTY_FIELD;
 import static org.jooq.impl.Tools.EMPTY_NAME;
 import static org.jooq.impl.Tools.EMPTY_QUERYPART;
+import static org.jooq.tools.StringUtils.defaultIfNull;
 
 import java.io.ByteArrayOutputStream;
 import java.math.BigDecimal;
@@ -764,7 +765,7 @@ final class ParserImpl implements Parser {
                 if (!parseKeywordIf(ctx, "FIRST") && !parseKeywordIf(ctx, "NEXT"))
                     throw ctx.unexpectedToken();
 
-                result.addLimit(inline((int) (long) parseUnsignedInteger(ctx)));
+                result.addLimit(inline((int) (long) defaultIfNull(parseUnsignedIntegerIf(ctx), 1L)));
 
                 if (!parseKeywordIf(ctx, "ROW") && !parseKeywordIf(ctx, "ROWS"))
                     throw ctx.unexpectedToken();

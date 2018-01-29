98e2b3a52788d573541524fad828983c5dd820b6
lukaseder <lukas.eder@gmail.com>
2018-01-29 12:13:34 +0100

    [#7068] Compilation error in generated DAOs when primary key is a composite type

--- jOOQ-codegen/src/main/java/org/jooq/util/JavaGenerator.java
+++ jOOQ-codegen/src/main/java/org/jooq/util/JavaGenerator.java
@@ -2825,7 +2825,7 @@ public class JavaGenerator extends AbstractGenerator {
         List<ColumnDefinition> keyColumns = key.getKeyColumns();
 
         if (keyColumns.size() == 1) {
-            tType = getJavaType(keyColumns.get(0).getType(resolver()));
+            tType = getJavaType(keyColumns.get(0).getType(resolver()), Mode.POJO);
         }
         else if (keyColumns.size() <= Constants.MAX_ROW_DEGREE) {
             String generics = "";

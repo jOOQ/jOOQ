9ce76a4008abc9fb2ed75254e0f613a4fa31a1fa
LeeWonJun0513 <lwjun0513@gmail.com>
2018-04-02 23:23:45 +0900

    Update SpringBootJooqExampleApplicationTests4.java
    Update SpringBootJooqExampleApplicationTests4.java

--- test/spring-boot-jooq-example/src/test/java/me/sangwon/SpringBootJooqExampleApplicationTests4.java
+++ test/spring-boot-jooq-example/src/test/java/me/sangwon/SpringBootJooqExampleApplicationTests4.java
@@ -20,7 +20,7 @@ import me.sangwon.repository.CustomerRepository;
 public class SpringBootJooqExampleApplicationTests4 {
 
 	@Autowired
-	private CustomerRepository_lee customerRepository;
+	private CustomerRepository customerRepository;
 
 
 	@Test
@@ -28,13 +28,13 @@ public class SpringBootJooqExampleApplicationTests4 {
 		
 		int count = customerRepository.countcolumn(); //call method (customer table is already made)
 		
-		assertEquals(count,13); //it is same in customerRepository record's number 
+		assertEquals(count,14); //it is same in customerRepository record's number 
 
 	}
 	@Test
-	public void SelectTest_1() { //test for select query
+	public void findAllTest_1() { //test for select query
 		final CustomerDTO customerDTO = customerRepository
-			.Select()
+			.findAll()
 			.stream()
 			.findFirst()
 			.get(); 
@@ -42,31 +42,31 @@ public class SpringBootJooqExampleApplicationTests4 {
 		
 	}
 	@Test
-	public void SelectTest_2() {
+	public void findAllTest_2() {
 		final CustomerDTO customerDTO = customerRepository
-			.Select()
+			.findAll()
 			.stream()
 			.findFirst()
 			.get(); 
 		
-		assertThat(customerDTO.getName()).isEqualTo("test"); // already inserted data
+		assertThat(customerDTO.getName()).isEqualTo("test0"); // already inserted data
 	}
 	
 	@Test
-	public void SelectTest_3() {
+	public void findAllTest_3() {
 		final CustomerDTO customerDTO = customerRepository
-			.Select()
+			.findAll()
 			.stream()
 			.findFirst()
 			.get(); 
 		
-		assertThat(customerDTO.getEmail()).isEqualTo("test@test"); // already inserted data
+		assertThat(customerDTO.getEmail()).isEqualTo("test0@test"); // already inserted data
 	}
 	
 	@Test
-	public void SelectTest_4() {
+	public void findAllTest_4() {
 		final CustomerDTO customerDTO = customerRepository
-			.Select()
+			.findAll()
 			.stream()
 			.findFirst()
 			.get(); 
@@ -74,8 +74,8 @@ public class SpringBootJooqExampleApplicationTests4 {
 		assertThat(customerDTO.getProducts()).hasSize(4);
 	}
 	@Test
-	public void InsertTest_1() { //test for select query
-		customerRepository.InsertInto("leewonjun", "lwjun0513@gmail.com"); //Inserting data - test for insert 
+	public void saveTest_1() { //test for select query
+		customerRepository.save("leewonjun", "lwjun0513@gmail.com"); //Inserting data - test for insert 
 		
 		final CustomerDTO customerDTO = customerRepository.SelectByname("leewonjun")
 			.stream()
@@ -87,8 +87,8 @@ public class SpringBootJooqExampleApplicationTests4 {
 	}
 	
 	@Test
-	public void InsertTest_2() { //test for select query
-		customerRepository.InsertInto("leewonjun", "lwjun0513@gmail.com"); //Inserting data - test for insert 
+	public void saveTest_2() { //test for select query
+		customerRepository.save("leewonjun", "lwjun0513@gmail.com"); //Inserting data - test for insert 
 		
 		final CustomerDTO customerDTO = customerRepository.SelectByname("leewonjun")
 			.stream()
@@ -99,41 +99,53 @@ public class SpringBootJooqExampleApplicationTests4 {
 	}
 
 	@Test
-	public void SelectIndexTest_1() { //test for select by index query
+	public void findOneTest_1() { //test for select by index query
 		
-		customerRepository.InsertInto("leewonjun", "lwjun0513@gmail.com"); //Inserting data - test for Select by index
-		final CustomerDTO customerDTO = customerRepository.SelectIndex(14).get(); //index number is 14. because of 14 data already inserted. 
+		customerRepository.save("leewonjun", "lwjun0513@gmail.com"); //Inserting data - test for Select by index
+		final CustomerDTO customerDTO = customerRepository.findOne(14).get(); //index number is 14. because 14 data already inserted. 
 		
-		assertThat(customerDTO.getId()).isEqualTo(14);//so id number is 3
+		assertThat(customerDTO.getId()).isEqualTo(14);//so id number is 14
 		
 	}
 	@Test
-	public void SelectIndexTest_2() { 
+	public void findOneTest_2() { 
 		
-		customerRepository.InsertInto("leewonjun", "lwjun0513@gmail.com"); //Inserting data - test for Select by index
-		final CustomerDTO customerDTO = customerRepository.SelectIndex(14).get(); //index number is 14. because of 14 data already inserted. 
+		customerRepository.save("leewonjun", "lwjun0513@gmail.com"); //Inserting data - test for Select by index
+		final CustomerDTO customerDTO = customerRepository.findOne(14).get(); //index number is 14. because 14 data already inserted. 
 		
 		assertThat(customerDTO.getName()).isEqualTo("leewonjun"); //it is that data is correct
 		
 	}
 	@Test
-	public void SelectIndexTest_3() { 
+	public void findOneTest_3() { 
 		
-		customerRepository.InsertInto("leewonjun", "lwjun0513@gmail.com"); //Inserting data - test for Select by index
-		final CustomerDTO customerDTO = customerRepository.SelectIndex(14).get(); //index number is 14. because of 14 data already inserted. 
+		customerRepository.save("leewonjun", "lwjun0513@gmail.com"); //Inserting data - test for Select by index
+		final CustomerDTO customerDTO = customerRepository.findOne(14).get(); //index number is 14. because of data already inserted. 
 		
 		assertThat(customerDTO.getEmail()).isEqualTo("lwjun0513@gmail.com"); //it is that data is correct
 		
 	}
 	@Test
-	public void SelectIndexTest_4() {
+	public void findOneTest_4() {
 		
-		customerRepository.InsertInto("leewonjun", "lwjun0513@gmail.com"); //Inserting data - test for Select by index
-		final CustomerDTO customerDTO = customerRepository.SelectIndex(14).get(); //index number is 14. because of 14 data already inserted. 
+		customerRepository.save("leewonjun", "lwjun0513@gmail.com"); //Inserting data - test for Select by index
+		final CustomerDTO customerDTO = customerRepository.findOne(14).get(); //index number is 14. because 14 data already inserted. 
 		
 		assertThat(customerDTO.getProducts()).hasSize(0); //has size is zero because of not product inserted
 	}
 	
+	@Test
+	public void modifyTest_1() {
+		customerRepository.modify(0, "lee", "lee@gmail.com");
+		final CustomerDTO customerDTO = customerRepository.findOne(0).get();
+		assertThat(customerDTO.getName()).isEqualTo("lee");
+	}
+	@Test
+	public void modifyTest_2() {
+		customerRepository.modify(0, "lee", "lee@gmail.com");
+		final CustomerDTO customerDTO = customerRepository.findOne(0).get();
+		assertThat(customerDTO.getName()).isEqualTo("lee@gmail.com");
+	}
 	
 	@Test
 	public void DeleteRecordTest_1() { //test for delete query
@@ -152,7 +164,7 @@ public class SpringBootJooqExampleApplicationTests4 {
 				.findFirst()
 				.get();
 		
-		assertThat(customerDTO.getName()).isnotEqualTo("test"); // original data "test" is deleted and second data is become first data
+		assertThat(customerDTO.getName()).isnotEqualTo("test0"); // original data "test" is deleted and second data will become first data
 		
 	
 	}
@@ -163,7 +175,7 @@ public class SpringBootJooqExampleApplicationTests4 {
 				.findFirst()
 				.get(); 
 	
-		assertThat(customerDTO.getEmail()).isnotEqualTo("test@test"); //original data "test@test" is deleted and second data is become first data
+		assertThat(customerDTO.getEmail()).isnotEqualTo("test0@test"); //original data "test@test" is deleted and second data will become first data
 	}
 	
 	

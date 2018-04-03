f6a22f942729743ea1549278a0ba1478e1df92ae
lukaseder <lukas.eder@gmail.com>
2018-04-03 14:52:28 +0200

    Regenerated code in jOOQ-spark-chart-example

--- jOOQ-examples/jOOQ-spark-chart-example/src/main/resources/db/migration/V1__postgres-sakila-schema.sql
+++ jOOQ-examples/jOOQ-spark-chart-example/src/main/resources/db/migration/V1__postgres-sakila-schema.sql
@@ -68,7 +68,7 @@ ALTER TYPE public.mpaa_rating OWNER TO postgres;
 --
 
 CREATE DOMAIN year AS integer
-	CONSTRAINT year_check CHECK (((VALUE >= 1901) AND (VALUE <= 2155)));
+    CONSTRAINT year_check CHECK (((VALUE >= 1901) AND (VALUE <= 2155)));
 
 
 ALTER DOMAIN public.year OWNER TO postgres;

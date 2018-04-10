26053ad80fb34abc9ffe8cb1390138bcc586b97f
lukaseder <lukas.eder@gmail.com>
2018-04-10 13:33:50 +0200

    [#5917] [#7171] Add Settings.parseUnsupportedSyntax
    - [#5917] Add Settings.parseUnsupportedSyntax to define behaviour when a parser error occurs
    - [#7171] Parse unsupported SPATIAL INDEX and FULLTEXT INDEX syntax

---
+++ jOOQ/src/main/java/org/jooq/conf/ParseUnsupportedSyntax.java
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+
+package org.jooq.conf;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for ParseUnsupportedSyntax.
+ *
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * <p>
+ * <pre>
+ * &lt;simpleType name="ParseUnsupportedSyntax"&gt;
+ *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string"&gt;
+ *     &lt;enumeration value="FAIL"/&gt;
+ *     &lt;enumeration value="IGNORE"/&gt;
+ *   &lt;/restriction&gt;
+ * &lt;/simpleType&gt;
+ * </pre>
+ *
+ */
+@XmlType(name = "ParseUnsupportedSyntax")
+@XmlEnum
+public enum ParseUnsupportedSyntax {
+
+    FAIL,
+    IGNORE;
+
+    public String value() {
+        return name();
+    }
+
+    public static ParseUnsupportedSyntax fromValue(String v) {
+        return valueOf(v);
+    }
+
+}

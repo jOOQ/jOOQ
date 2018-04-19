96a5c5134a018421575f6fc78e0504f09949527c
lukaseder <lukas.eder@gmail.com>
2018-04-19 13:18:28 +0200

    [#629] Add support for the Teradata database (WIP)

---
+++ jOOQ/src/main/java/org/jooq/impl/Translate.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Other licenses:
+ * -----------------------------------------------------------------------------
+ * Commercial licenses for this work are available. These replace the above
+ * ASL 2.0 and offer limited warranties, support, maintenance, and commercial
+ * database integrations.
+ *
+ * For more information, please visit: http://www.jooq.org/licenses
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+package org.jooq.impl;
+
+import static org.jooq.impl.DSL.function;
+
+import org.jooq.Configuration;
+import org.jooq.Field;
+import org.jooq.QueryPart;
+
+/**
+ * @author Lukas Eder
+ */
+final class Translate extends AbstractFunction<String> {
+
+    /**
+     * Generated UID
+     */
+    private static final long serialVersionUID = 6776882414592454999L;
+
+    Translate(Field<String> text, Field<String> from, Field<String> to) {
+        super("translate", text.getDataType(), text, from, to);
+    }
+
+    @Override
+    final QueryPart getFunction0(Configuration configuration) {
+        switch (configuration.family()) {
+
+
+
+
+            default:
+                return function("translate", getDataType(), getArguments());
+        }
+    }
+}

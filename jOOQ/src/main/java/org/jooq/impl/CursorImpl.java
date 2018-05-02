39821cb40a6552aeda95695dd55fc397b52fce21
Mark Chesney <mark.chesney@insight.com>
2018-05-02 10:04:45 -0700

    [#7455] Create spliterator correctly

--- jOOQ/src/main/java/org/jooq/impl/CursorImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/CursorImpl.java
@@ -276,9 +276,8 @@ final class CursorImpl<R extends Record> extends AbstractCursor<R> implements Cu
     @Override
     public final Stream<R> stream() throws DataAccessException {
         return StreamSupport.stream(
-            Spliterators.spliterator(
+            Spliterators.spliteratorUnknownSize(
                 iterator(),
-                0,
                 Spliterator.ORDERED | Spliterator.NONNULL
             ),
             false

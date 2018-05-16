ef2caa89b1034cd8086b034c2312576690682cd9
ciaccia <ciaccia@gmail.com>
2018-05-16 11:10:08 +0200

    Update DefaultBinding.java
    Removed PG "special" timestamp with time zone values which are never returned from JDBC driver

--- jOOQ/src/main/java/org/jooq/impl/DefaultBinding.java
+++ jOOQ/src/main/java/org/jooq/impl/DefaultBinding.java
@@ -2392,26 +2392,13 @@ public class DefaultBinding<T, U> implements Binding<T, U> {
         // Probably a bug, let OffsetDateTime or OffsetTime report it
         else {
 
-            // [#7126] Postgresql can return Special Date/Time Inputs such as "epoch", "infinity", "now", ...
-            if ("epoch".equals(formatted)) {
-                return "1970-01-01T00:00Z";
-            } else if ("infinity".equals(formatted)) {
+            // [#7126] Binding Postgres TIMESTAMP WITH TIME ZONE type doesn't support values like INFINITY
+            if ("infinity".equals(formatted)) {
                 // DATE_POSITIVE_INFINITY taken from org.postgresql.PGStatement
                 return "+292278994-08-16T23:00Z";
             } else if ("-infinity".equals(formatted)) {
                 // DATE_NEGATIVE_INFINITY taken from org.postgresql.PGStatement
                 return "-292275055-05-16T23:00Z";
-            } else if ("now".equals(formatted)) {
-                return OffsetDateTime.now().toString();
-            } else if ("today".equals(formatted)) {
-                return OffsetDateTime.ofInstant(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant(), ZoneId.systemDefault()).toString();
-            } else if ("tomorrow".equals(formatted)) {
-                return OffsetDateTime.ofInstant(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).plus(1, ChronoUnit.DAYS).toInstant(), ZoneId.systemDefault()).toString();
-            } else if ("yesterday".equals(formatted)) {
-                return OffsetDateTime.ofInstant(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).minus(1, ChronoUnit.DAYS).toInstant(), ZoneId.systemDefault()).toString();
-            } else if ("allballs".equals(formatted)) {
-                // This only for time, not for date/timestamp
-                // We should return a time for midnight but it is not applicable for OffsetDateTime 
             }
 
             return formatted;

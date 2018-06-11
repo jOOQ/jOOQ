3dc1c751884ba8665f732e1ce3616f2e33866166
lukaseder <lukas.eder@gmail.com>
2018-06-11 09:58:00 +0200

    [#7562] Incomplete Support annotation on some of the H2 style mergeInto overloads

--- jOOQ/src/main/java/org/jooq/impl/DSL.java
+++ jOOQ/src/main/java/org/jooq/impl/DSL.java
@@ -5848,7 +5848,7 @@ public class DSL {
      *
      * @see DSLContext#mergeInto(Table, Field...)
      */
-    @Support({ CUBRID, H2, HSQLDB })
+    @Support({ CUBRID, FIREBIRD_3_0, H2, HSQLDB, MARIADB, MYSQL, POSTGRES_9_5 })
     public static <R extends Record> MergeKeyStepN<R> mergeInto(Table<R> table, Field<?>... fields) {
         return dsl().mergeInto(table, fields);
     }
@@ -5862,7 +5862,7 @@ public class DSL {
      *
      * @see DSLContext#mergeInto(Table, Collection)
      */
-    @Support({ CUBRID, H2, HSQLDB })
+    @Support({ CUBRID, FIREBIRD_3_0, H2, HSQLDB, MARIADB, MYSQL, POSTGRES_9_5 })
     public static <R extends Record> MergeKeyStepN<R> mergeInto(Table<R> table, Collection<? extends Field<?>> fields) {
         return dsl().mergeInto(table, fields);
     }

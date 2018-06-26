4655744e52d1a728ac5b853fd326f60c0f715af0
lukaseder <lukas.eder@gmail.com>
2018-06-26 12:13:07 +0200

    [#7579] org.jooq.meta.xml.XMLDatabase does not generate target classes when project is compiled on JDK 9 or 10

--- jOOQ-meta/src/main/java/org/jooq/meta/xml/XMLDatabase.java
+++ jOOQ-meta/src/main/java/org/jooq/meta/xml/XMLDatabase.java
@@ -241,6 +241,17 @@ public class XMLDatabase extends AbstractDatabase {
                     "<information_schema>",
                     "<information_schema xmlns=\"" + Constants.NS_META + "\">");
 
+                // [#7579] On JDK 9, 10, depending on how JAXB is loaded onto the classpath / module path,
+                //         the xmlns seems to be considered for (un)marshalling, or not. This seems to be
+                //         a bug in JAXB, with no known tracking ID as of yet.
+                //         The following quick fix tests the presence of the xmlns when marshalling, and if absent
+                //         removes it prior to unmarshalling.
+                StringWriter test = new StringWriter();
+                JAXB.marshal(new InformationSchema(), test);
+
+                if (!test.toString().contains("xmlns"))
+                    content = content.replaceAll("xmlns=\".*\"", "");
+
                 info = JAXB.unmarshal(new StringReader(content), InformationSchema.class);
             }
             catch (Exception e) {

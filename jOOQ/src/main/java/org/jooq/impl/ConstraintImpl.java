6f76dd4dd23c9ac0a38b90c44ea3e8978f0e9535
lukaseder <lukas.eder@gmail.com>
2018-10-08 12:50:50 +0200

    [#7918] Bad formatting of constraints in CREATE TABLE statements

--- jOOQ/src/main/java/org/jooq/impl/ConstraintImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/ConstraintImpl.java
@@ -250,7 +250,7 @@ implements
                    .sql(')');
             }
 
-            if (getQualifiedName() == AbstractName.NO_NAME)
+            if (getQualifiedName() != AbstractName.NO_NAME)
                 ctx.formatIndentEnd();
         }
     }

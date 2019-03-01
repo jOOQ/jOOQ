f27ae69702981a10c0af6c6ccce8f1f2a3bf724d
lukaseder <lukas.eder@gmail.com>
2019-03-01 16:21:42 +0100

    Removed unnecessary import

--- jOOQ/src/main/java/org/jooq/impl/AbstractQuery.java
+++ jOOQ/src/main/java/org/jooq/impl/AbstractQuery.java
@@ -47,7 +47,6 @@ import static org.jooq.ExecuteType.DDL;
 // ...
 import static org.jooq.conf.ParamType.INDEXED;
 import static org.jooq.conf.ParamType.INLINED;
-import static org.jooq.conf.ParamType.NAMED;
 import static org.jooq.conf.SettingsTools.executePreparedStatements;
 import static org.jooq.conf.SettingsTools.getParamType;
 import static org.jooq.conf.ThrowExceptions.THROW_NONE;

d6ef24811f182a6ee62dbb4cf27f278e778b1f36
lukaseder <lukas.eder@gmail.com>
2019-03-26 14:40:57 +0100

    [#8400] Fixed PostgreSQL Sakila deletion script

--- jOOQ-examples/Sakila/postgres-sakila-db/postgres-sakila-delete-data.sql
+++ jOOQ-examples/Sakila/postgres-sakila-db/postgres-sakila-delete-data.sql
@@ -1,6 +1,8 @@
 -- Delete data
+ALTER TABLE store DROP CONSTRAINT store_manager_staff_id_fkey;
 ALTER TABLE staff DROP CONSTRAINT staff_address_id_fkey;
 ALTER TABLE staff DROP CONSTRAINT staff_store_id_fkey;
+
 DELETE FROM payment ;
 DELETE FROM rental ;
 DELETE FROM customer ;
@@ -19,3 +21,4 @@ DELETE FROM language ;
 
 ALTER TABLE staff ADD CONSTRAINT staff_address_id_fkey FOREIGN KEY (address_id) REFERENCES address (address_id) ON UPDATE CASCADE;
 ALTER TABLE staff ADD CONSTRAINT staff_store_id_fkey FOREIGN KEY (store_id) REFERENCES store (store_id) ON UPDATE CASCADE;
+ALTER TABLE store ADD CONSTRAINT store_manager_staff_id_fkey FOREIGN KEY (manager_staff_id) REFERENCES staff(staff_id) ON UPDATE CASCADE ON DELETE RESTRICT;

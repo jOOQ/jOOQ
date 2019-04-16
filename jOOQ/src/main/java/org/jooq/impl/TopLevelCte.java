14a0f6ddc866283e34356f85689a9f978c77c9f4
Knut Wannheden <knut.wannheden@gmail.com>
2019-04-16 12:49:37 +0200

    [#3607] [#8522] Support PL/SQL BOOLEAN types in SQL functions

---
+++ jOOQ/src/main/java/org/jooq/impl/TopLevelCte.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Other licenses:
+ * -----------------------------------------------------------------------------
+ * Commercial licenses for this work are available. These replace the above
+ * ASL 2.0 and offer limited warranties, support, maintenance, and commercial
+ * database integrations.
+ *
+ * For more information, please visit: http://www.jooq.org/licenses
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+package org.jooq.impl;
+
+import org.jooq.QueryPart;
+
+/**
+ * [#3607] [#8522] CTEs that need to be added to the top level CTE section.
+ */
+final class TopLevelCte extends QueryPartList<QueryPart> {
+
+    /**
+     * Generated UID
+     */
+    private static final long serialVersionUID = 9211135511038680791L;
+
+    @Override
+    public final boolean declaresCTE() {
+        return true;
+    }
+}

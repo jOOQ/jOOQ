62c42944d379db8e9de64098679e0dbc91e55439
Lukas Eder <lukas.eder@gmail.com>
2019-05-13 12:39:47 +0200

    [#8619] Let Query subtypes extends RowCountQuery, which extends Publisher<Integer>

---
+++ jOOQ/src/main/java/org/jooq/RowCountQuery.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Other licenses:
+ * -----------------------------------------------------------------------------
+ * Commercial licenses for this work are available. These replace the above
+ * ASL 2.0 and offer limited warranties, support, maintenance, and commercial
+ * database integrations.
+ *
+ * For more information, please visit: http://www.jooq.org/licenses
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+package org.jooq;
+
+import java.util.concurrent.Flow;
+
+/**
+ * Any query that is not a {@link ResultQuery}, and thus possibly produces a row
+ * count as a result, instead of a {@link Result}.
+ *
+ * @author Lukas Eder
+ */
+public interface RowCountQuery
+extends
+    Query
+    , org.reactivestreams.Publisher<Integer>
+    , Flow.Publisher<Integer>
+{
+
+}

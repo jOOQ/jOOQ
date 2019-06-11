338c02a89e9b7375579231b24401895b21f3a59f
Knut Wannheden <knut.wannheden@gmail.com>
2019-06-11 14:16:27 +0200

    [jOOQ/jOOQ#8770] Fix @Support annotations of SelectLimitAfterOffsetStep
    Align the @Support annotations of the limit() methods.

--- jOOQ/src/main/java/org/jooq/SelectLimitAfterOffsetStep.java
+++ jOOQ/src/main/java/org/jooq/SelectLimitAfterOffsetStep.java
@@ -128,7 +128,7 @@ public interface SelectLimitAfterOffsetStep<R extends Record> extends SelectForU
      * RDBMS, this may be emulated with a <code>ROW_NUMBER()</code> window
      * function and nested <code>SELECT</code> statements.
      */
-    @Support
+    @Support({ CUBRID, DERBY, FIREBIRD, H2, HSQLDB, MARIADB, MYSQL, POSTGRES, SQLITE })
     SelectLimitPercentAfterOffsetStep<R> limit(int numberOfRows);
 
     /**
@@ -138,7 +138,7 @@ public interface SelectLimitAfterOffsetStep<R extends Record> extends SelectForU
      * RDBMS, this may be emulated with a <code>ROW_NUMBER()</code> window
      * function and nested <code>SELECT</code> statements.
      */
-    @Support
+    @Support({ CUBRID, DERBY, FIREBIRD, H2, HSQLDB, MARIADB, MYSQL, POSTGRES, SQLITE })
     SelectLimitPercentAfterOffsetStep<R> limit(Number numberOfRows);
 
     /**

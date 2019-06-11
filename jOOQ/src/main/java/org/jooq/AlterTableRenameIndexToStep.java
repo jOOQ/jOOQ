b798c038d0cc86447acfd28cc368902a79ed5d15
Knut Wannheden <knut.wannheden@gmail.com>
2019-06-11 15:06:05 +0200

    [jOOQ/jOOQ#2059] Fix AlterTableRenameIndexToStep @Support annotations
    This feature is not supported by MEMSQL.

--- jOOQ/src/main/java/org/jooq/AlterTableRenameIndexToStep.java
+++ jOOQ/src/main/java/org/jooq/AlterTableRenameIndexToStep.java
@@ -43,7 +43,6 @@ package org.jooq;
 import static org.jooq.SQLDialect.DERBY;
 import static org.jooq.SQLDialect.H2;
 import static org.jooq.SQLDialect.HSQLDB;
-// ...
 import static org.jooq.SQLDialect.MYSQL;
 // ...
 import static org.jooq.SQLDialect.POSTGRES;

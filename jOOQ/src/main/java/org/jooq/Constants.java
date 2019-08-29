d27dbf41173b420c1a2d0575ae48b54b36b6b5d1
Knut Wannheden <knut.wannheden@gmail.com>
2019-08-29 16:12:59 +0200

    Preparing master for 3.13 release cycle

--- jOOQ/src/main/java/org/jooq/Constants.java
+++ jOOQ/src/main/java/org/jooq/Constants.java
@@ -47,7 +47,7 @@ public final class Constants {
     /**
      * The latest jOOQ minor version.
      */
-    public static final String MINOR_VERSION  = "3.12";
+    public static final String MINOR_VERSION  = "3.13";
 
     /**
      * The latest jOOQ version.
@@ -55,7 +55,7 @@ public final class Constants {
      * This is the same as {@link #MINOR_VERSION}, but it may include patch
      * version suffixes.
      */
-    public static final String VERSION        = "3.12.0-SNAPSHOT";
+    public static final String VERSION        = "3.13.0-SNAPSHOT";
 
     /**
      * The latest jOOQ full version.
@@ -63,7 +63,7 @@ public final class Constants {
      * This is the same as {@link #VERSION}, but it may include release
      * candidate and other suffixes.
      */
-    public static final String FULL_VERSION   = "3.12.0-SNAPSHOT";
+    public static final String FULL_VERSION   = "3.13.0-SNAPSHOT";
 
     /**
      * The current jooq-runtime XSD file name.

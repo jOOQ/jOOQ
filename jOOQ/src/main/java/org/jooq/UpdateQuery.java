182c18c7379eb8ad7d24cb372b1a134fa4b22007
Lukas Eder <lukas.eder@gmail.com>
2019-10-01 14:47:02 +0200

    [jOOQ/jOQO#9311] Incomplete @Support annotations on UpdateQuery.addValues(Row[N], Select)

--- jOOQ/src/main/java/org/jooq/UpdateQuery.java
+++ jOOQ/src/main/java/org/jooq/UpdateQuery.java
@@ -49,6 +49,7 @@ import static org.jooq.SQLDialect.HSQLDB;
 import static org.jooq.SQLDialect.POSTGRES;
 // ...
 // ...
+// ...
 
 import java.util.Collection;
 
@@ -211,139 +212,139 @@ public interface UpdateQuery<R extends Record> extends StoreQuery<R>, ConditionP
     /**
      * Specify a multi-column set clause for the <code>UPDATE</code> statement.
      */
-    @Support({ H2, HSQLDB })
+    @Support({ H2, HSQLDB, POSTGRES })
     <T1> void addValues(Row1<T1> row, Select<? extends Record1<T1>> select);
 
     /**
      * Specify a multi-column set clause for the <code>UPDATE</code> statement.
      */
-    @Support({ H2, HSQLDB })
+    @Support({ H2, HSQLDB, POSTGRES })
     <T1, T2> void addValues(Row2<T1, T2> row, Select<? extends Record2<T1, T2>> select);
 
     /**
      * Specify a multi-column set clause for the <code>UPDATE</code> statement.
      */
-    @Support({ H2, HSQLDB })
+    @Support({ H2, HSQLDB, POSTGRES })
     <T1, T2, T3> void addValues(Row3<T1, T2, T3> row, Select<? extends Record3<T1, T2, T3>> select);
 
     /**
      * Specify a multi-column set clause for the <code>UPDATE</code> statement.
      */
-    @Support({ H2, HSQLDB })
+    @Support({ H2, HSQLDB, POSTGRES })
     <T1, T2, T3, T4> void addValues(Row4<T1, T2, T3, T4> row, Select<? extends Record4<T1, T2, T3, T4>> select);
 
     /**
      * Specify a multi-column set clause for the <code>UPDATE</code> statement.
      */
-    @Support({ H2, HSQLDB })
+    @Support({ H2, HSQLDB, POSTGRES })
     <T1, T2, T3, T4, T5> void addValues(Row5<T1, T2, T3, T4, T5> row, Select<? extends Record5<T1, T2, T3, T4, T5>> select);
 
     /**
      * Specify a multi-column set clause for the <code>UPDATE</code> statement.
      */
-    @Support({ H2, HSQLDB })
+    @Support({ H2, HSQLDB, POSTGRES })
     <T1, T2, T3, T4, T5, T6> void addValues(Row6<T1, T2, T3, T4, T5, T6> row, Select<? extends Record6<T1, T2, T3, T4, T5, T6>> select);
 
     /**
      * Specify a multi-column set clause for the <code>UPDATE</code> statement.
      */
-    @Support({ H2, HSQLDB })
+    @Support({ H2, HSQLDB, POSTGRES })
     <T1, T2, T3, T4, T5, T6, T7> void addValues(Row7<T1, T2, T3, T4, T5, T6, T7> row, Select<? extends Record7<T1, T2, T3, T4, T5, T6, T7>> select);
 
     /**
      * Specify a multi-column set clause for the <code>UPDATE</code> statement.
      */
-    @Support({ H2, HSQLDB })
+    @Support({ H2, HSQLDB, POSTGRES })
     <T1, T2, T3, T4, T5, T6, T7, T8> void addValues(Row8<T1, T2, T3, T4, T5, T6, T7, T8> row, Select<? extends Record8<T1, T2, T3, T4, T5, T6, T7, T8>> select);
 
     /**
      * Specify a multi-column set clause for the <code>UPDATE</code> statement.
      */
-    @Support({ H2, HSQLDB })
+    @Support({ H2, HSQLDB, POSTGRES })
     <T1, T2, T3, T4, T5, T6, T7, T8, T9> void addValues(Row9<T1, T2, T3, T4, T5, T6, T7, T8, T9> row, Select<? extends Record9<T1, T2, T3, T4, T5, T6, T7, T8, T9>> select);
 
     /**
      * Specify a multi-column set clause for the <code>UPDATE</code> statement.
      */
-    @Support({ H2, HSQLDB })
+    @Support({ H2, HSQLDB, POSTGRES })
     <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> void addValues(Row10<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> row, Select<? extends Record10<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>> select);
 
     /**
      * Specify a multi-column set clause for the <code>UPDATE</code> statement.
      */
-    @Support({ H2, HSQLDB })
+    @Support({ H2, HSQLDB, POSTGRES })
     <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> void addValues(Row11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> row, Select<? extends Record11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>> select);
 
     /**
      * Specify a multi-column set clause for the <code>UPDATE</code> statement.
      */
-    @Support({ H2, HSQLDB })
+    @Support({ H2, HSQLDB, POSTGRES })
     <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12> void addValues(Row12<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12> row, Select<? extends Record12<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>> select);
 
     /**
      * Specify a multi-column set clause for the <code>UPDATE</code> statement.
      */
-    @Support({ H2, HSQLDB })
+    @Support({ H2, HSQLDB, POSTGRES })
     <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13> void addValues(Row13<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13> row, Select<? extends Record13<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>> select);
 
     /**
      * Specify a multi-column set clause for the <code>UPDATE</code> statement.
      */
-    @Support({ H2, HSQLDB })
+    @Support({ H2, HSQLDB, POSTGRES })
     <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> void addValues(Row14<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> row, Select<? extends Record14<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>> select);
 
     /**
      * Specify a multi-column set clause for the <code>UPDATE</code> statement.
      */
-    @Support({ H2, HSQLDB })
+    @Support({ H2, HSQLDB, POSTGRES })
     <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15> void addValues(Row15<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15> row, Select<? extends Record15<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>> select);
 
     /**
      * Specify a multi-column set clause for the <code>UPDATE</code> statement.
      */
-    @Support({ H2, HSQLDB })
+    @Support({ H2, HSQLDB, POSTGRES })
     <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16> void addValues(Row16<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16> row, Select<? extends Record16<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>> select);
 
     /**
      * Specify a multi-column set clause for the <code>UPDATE</code> statement.
      */
-    @Support({ H2, HSQLDB })
+    @Support({ H2, HSQLDB, POSTGRES })
     <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17> void addValues(Row17<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17> row, Select<? extends Record17<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17>> select);
 
     /**
      * Specify a multi-column set clause for the <code>UPDATE</code> statement.
      */
-    @Support({ H2, HSQLDB })
+    @Support({ H2, HSQLDB, POSTGRES })
     <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18> void addValues(Row18<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18> row, Select<? extends Record18<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18>> select);
 
     /**
      * Specify a multi-column set clause for the <code>UPDATE</code> statement.
      */
-    @Support({ H2, HSQLDB })
+    @Support({ H2, HSQLDB, POSTGRES })
     <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19> void addValues(Row19<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19> row, Select<? extends Record19<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19>> select);
 
     /**
      * Specify a multi-column set clause for the <code>UPDATE</code> statement.
      */
-    @Support({ H2, HSQLDB })
+    @Support({ H2, HSQLDB, POSTGRES })
     <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20> void addValues(Row20<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20> row, Select<? extends Record20<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20>> select);
 
     /**
      * Specify a multi-column set clause for the <code>UPDATE</code> statement.
      */
-    @Support({ H2, HSQLDB })
+    @Support({ H2, HSQLDB, POSTGRES })
     <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21> void addValues(Row21<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21> row, Select<? extends Record21<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21>> select);
 
     /**
      * Specify a multi-column set clause for the <code>UPDATE</code> statement.
      */
-    @Support({ H2, HSQLDB })
+    @Support({ H2, HSQLDB, POSTGRES })
     <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> void addValues(Row22<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22> row, Select<? extends Record22<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>> select);
 
     /**
      * Specify a multi-column set clause for the <code>UPDATE</code> statement.
      */
-    @Support({ H2, HSQLDB })
+    @Support({ H2, HSQLDB, POSTGRES })
     void addValues(RowN row, Select<?> select);
 
 

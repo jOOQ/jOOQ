993293ed568ceb0ebf025aed9ea30851f2f875df
Lukas Eder <lukas.eder@gmail.com>
2019-10-17 17:37:01 +0200

    [jOOQ/jOOQ#9401] Downgrade JAXB dependencies to 2.2 for all JDKs

--- jOOQ/src/main/java/org/jooq/SQLDialect.java
+++ jOOQ/src/main/java/org/jooq/SQLDialect.java
@@ -82,7 +82,12 @@ public enum SQLDialect {
 
     /**
      * The CUBRID dialect family.
+     *
+     * @deprecated - [#9403] - 3.13.0 - This dialect is hardly used by anyone
+     *             with jOOQ or without jOOQ and will be removed in the near
+     *             future.
      */
+    @Deprecated
     CUBRID("CUBRID", false, true),
 
     /**

c0df609621ac80b1d6d5ab957431421cab39ff73
Lukas Eder <lukas.eder@gmail.com>
2019-12-10 11:31:16 +0100

    [jOOQ/jOOQ#9157] [jOOQ/jOOR#57] Try again
    When running the Java 8 distribution's code of jOOR on Java 9,
    CACHED_LOOKUP_CONSTRUCTOR is null because we're not allowed to access
    that from JDK 9+. This means we can no longer use it in reflection
    logic, e.g. when implementing in-memory compilation. We have to use the
    Java 9+ logic via meta-reflection.

--- jOOQ/src/main/java/org/jooq/tools/reflect/CompileOptions.java
+++ jOOQ/src/main/java/org/jooq/tools/reflect/CompileOptions.java
@@ -13,10 +13,9 @@
  */
 package org.jooq.tools.reflect;
 
+
+
 import java.util.Arrays;
-
-
-
 import java.util.Collections;
 import java.util.List;
 
@@ -28,25 +27,37 @@ import javax.annotation.processing.Processor;
 public final class CompileOptions {
 
     final List<? extends Processor> processors;
+    final List<String> options;
 
     public CompileOptions() {
         this(
+            Collections.emptyList(),
             Collections.emptyList()
         );
     }
 
-    private CompileOptions(List<? extends Processor> processors) {
+    private CompileOptions(
+        List<? extends Processor> processors,
+        List<String> options
+    ) {
         this.processors = processors;
+        this.options = options;
     }
 
-    @SuppressWarnings("hiding")
-    public CompileOptions processors(Processor... processors) {
-        return processors(Arrays.asList(processors));
+    public final CompileOptions processors(Processor... newProcessors) {
+        return processors(Arrays.asList(newProcessors));
     }
 
-    @SuppressWarnings("hiding")
-    public CompileOptions processors(List<? extends Processor> processors) {
-        return new CompileOptions(processors);
+    public final CompileOptions processors(List<? extends Processor> newProcessors) {
+        return new CompileOptions(newProcessors, options);
+    }
+
+    public final CompileOptions options(String... newOptions) {
+        return options(Arrays.asList(newOptions));
+    }
+
+    public final CompileOptions options(List<String> newOptions) {
+        return new CompileOptions(processors, newOptions);
     }
 }
 

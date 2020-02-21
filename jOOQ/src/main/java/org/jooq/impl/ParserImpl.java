e69c7b8eeca739daab6e9a89cb3c6faee8a04064
Lukas Eder <lukas.eder@gmail.com>
2020-02-21 10:18:06 +0100

    [jOOQ/jOOQ#9856] Parser cannot handle double quotes in string literals

--- jOOQ/src/main/java/org/jooq/impl/ParserImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/ParserImpl.java
@@ -10063,8 +10063,6 @@ final class ParserImpl implements Parser {
 
 
 
-
-
                 case '\'': {
                     if (ctx.character(i + 1) != delimiter) {
                         ctx.position(i + 1);

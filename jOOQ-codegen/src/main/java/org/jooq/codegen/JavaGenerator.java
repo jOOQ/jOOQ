c0490c5f4b898f5b47d294b743a997308704b20a
Lukas Eder <lukas.eder@gmail.com>
2020-03-02 11:18:49 +0100

    [jOOQ/jOOQ#9903] Changing getJavaClassName() for Mode.ENUM in DefaultGeneratorStrategy generates invalid code

--- jOOQ-codegen/src/main/java/org/jooq/codegen/JavaGenerator.java
+++ jOOQ-codegen/src/main/java/org/jooq/codegen/JavaGenerator.java
@@ -6522,7 +6522,7 @@ public class JavaGenerator extends AbstractGenerator {
 
         // Check for ENUM types
         else if (db.getEnum(schema, u) != null) {
-            type = getStrategy().getFullJavaClassName(db.getEnum(schema, u));
+            type = getStrategy().getFullJavaClassName(db.getEnum(schema, u), Mode.ENUM);
         }
 
         // Check for UDTs
@@ -6599,7 +6599,7 @@ public class JavaGenerator extends AbstractGenerator {
                 db, schema, DefaultDataType.getDataType(db.getDialect(), String.class).getTypeName(), l, p, s, n, d, (Name) null
             )));
             sb.append(".asEnumDataType(");
-            sb.append(classOf(getStrategy().getFullJavaClassName(db.getEnum(schema, u))));
+            sb.append(classOf(getStrategy().getFullJavaClassName(db.getEnum(schema, u), Mode.ENUM)));
             sb.append(")");
         }
         else {

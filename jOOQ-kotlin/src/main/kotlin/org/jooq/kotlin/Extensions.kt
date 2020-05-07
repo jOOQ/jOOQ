9d8aa484a409abde03e226558538d644fb36ec6c
Lukas Eder <lukas.eder@gmail.com>
2020-05-07 17:15:45 +0200

    [jOOQ/jOOQ#6256] [jOOQ/jOOQ#10176] Removed extension methods again
    Some kotlin extension methods are no longer necessary, as they have been implemented directly in jOOQ's core library

--- jOOQ-kotlin/src/main/kotlin/org/jooq/kotlin/Extensions.kt
+++ jOOQ-kotlin/src/main/kotlin/org/jooq/kotlin/Extensions.kt
@@ -45,32 +45,12 @@ inline fun <reified T: Any> Select<Record1<T>>.eq(value: Field<T>): Condition {
 }
 
 @Support
-inline fun <reified T: Any> Select<Record1<T>>.eq(value: Select<out Record1<T>>): Condition {
-    return field(this).eq(value);
-}
-
-@Support
-inline fun <reified T: Any> Select<Record1<T>>.eq(value: QuantifiedSelect<out Record1<T>>): Condition {
-    return field(this).eq(value);
-}
-
-@Support
 inline fun <reified T: Any> Select<Record1<T>>.equal(value: T): Condition {
     return field(this).equal(value);
 }
 
 @Support
 inline fun <reified T: Any> Select<Record1<T>>.equal(value: Field<T>): Condition {
-    return field(this).equal(value);
-}
-
-@Support
-inline fun <reified T: Any> Select<Record1<T>>.equal(value: Select<out Record1<T>>): Condition {
-    return field(this).equal(value);
-}
-
-@Support
-inline fun <reified T: Any> Select<Record1<T>>.equal(value: QuantifiedSelect<out Record1<T>>): Condition {
     return field(this).equal(value);
 }
 
@@ -85,32 +65,12 @@ inline fun <reified T: Any> Select<Record1<T>>.ne(value: Field<T>): Condition {
 }
 
 @Support
-inline fun <reified T: Any> Select<Record1<T>>.ne(value: Select<out Record1<T>>): Condition {
-    return field(this).ne(value);
-}
-
-@Support
-inline fun <reified T: Any> Select<Record1<T>>.ne(value: QuantifiedSelect<out Record1<T>>): Condition {
-    return field(this).ne(value);
-}
-
-@Support
 inline fun <reified T: Any> Select<Record1<T>>.notEqual(value: T): Condition {
     return field(this).notEqual(value);
 }
 
 @Support
 inline fun <reified T: Any> Select<Record1<T>>.notEqual(value: Field<T>): Condition {
-    return field(this).notEqual(value);
-}
-
-@Support
-inline fun <reified T: Any> Select<Record1<T>>.notEqual(value: Select<out Record1<T>>): Condition {
-    return field(this).notEqual(value);
-}
-
-@Support
-inline fun <reified T: Any> Select<Record1<T>>.notEqual(value: QuantifiedSelect<out Record1<T>>): Condition {
     return field(this).notEqual(value);
 }
 
@@ -125,32 +85,12 @@ inline fun <reified T: Any> Select<Record1<T>>.gt(value: Field<T>): Condition {
 }
 
 @Support
-inline fun <reified T: Any> Select<Record1<T>>.gt(value: Select<out Record1<T>>): Condition {
-    return field(this).gt(value);
-}
-
-@Support
-inline fun <reified T: Any> Select<Record1<T>>.gt(value: QuantifiedSelect<out Record1<T>>): Condition {
-    return field(this).gt(value);
-}
-
-@Support
 inline fun <reified T: Any> Select<Record1<T>>.greaterThan(value: T): Condition {
     return field(this).greaterThan(value);
 }
 
 @Support
 inline fun <reified T: Any> Select<Record1<T>>.greaterThan(value: Field<T>): Condition {
-    return field(this).greaterThan(value);
-}
-
-@Support
-inline fun <reified T: Any> Select<Record1<T>>.greaterThan(value: Select<out Record1<T>>): Condition {
-    return field(this).greaterThan(value);
-}
-
-@Support
-inline fun <reified T: Any> Select<Record1<T>>.greaterThan(value: QuantifiedSelect<out Record1<T>>): Condition {
     return field(this).greaterThan(value);
 }
 
@@ -165,32 +105,12 @@ inline fun <reified T: Any> Select<Record1<T>>.ge(value: Field<T>): Condition {
 }
 
 @Support
-inline fun <reified T: Any> Select<Record1<T>>.ge(value: Select<out Record1<T>>): Condition {
-    return field(this).ge(value);
-}
-
-@Support
-inline fun <reified T: Any> Select<Record1<T>>.ge(value: QuantifiedSelect<out Record1<T>>): Condition {
-    return field(this).ge(value);
-}
-
-@Support
 inline fun <reified T: Any> Select<Record1<T>>.greaterOrEqual(value: T): Condition {
     return field(this).greaterOrEqual(value);
 }
 
 @Support
 inline fun <reified T: Any> Select<Record1<T>>.greaterOrEqual(value: Field<T>): Condition {
-    return field(this).greaterOrEqual(value);
-}
-
-@Support
-inline fun <reified T: Any> Select<Record1<T>>.greaterOrEqual(value: Select<out Record1<T>>): Condition {
-    return field(this).greaterOrEqual(value);
-}
-
-@Support
-inline fun <reified T: Any> Select<Record1<T>>.greaterOrEqual(value: QuantifiedSelect<out Record1<T>>): Condition {
     return field(this).greaterOrEqual(value);
 }
 
@@ -205,32 +125,12 @@ inline fun <reified T: Any> Select<Record1<T>>.lt(value: Field<T>): Condition {
 }
 
 @Support
-inline fun <reified T: Any> Select<Record1<T>>.lt(value: Select<out Record1<T>>): Condition {
-    return field(this).lt(value);
-}
-
-@Support
-inline fun <reified T: Any> Select<Record1<T>>.lt(value: QuantifiedSelect<out Record1<T>>): Condition {
-    return field(this).lt(value);
-}
-
-@Support
 inline fun <reified T: Any> Select<Record1<T>>.lessThan(value: T): Condition {
     return field(this).lessThan(value);
 }
 
 @Support
 inline fun <reified T: Any> Select<Record1<T>>.lessThan(value: Field<T>): Condition {
-    return field(this).lessThan(value);
-}
-
-@Support
-inline fun <reified T: Any> Select<Record1<T>>.lessThan(value: Select<out Record1<T>>): Condition {
-    return field(this).lessThan(value);
-}
-
-@Support
-inline fun <reified T: Any> Select<Record1<T>>.lessThan(value: QuantifiedSelect<out Record1<T>>): Condition {
     return field(this).lessThan(value);
 }
 
@@ -245,32 +145,12 @@ inline fun <reified T: Any> Select<Record1<T>>.le(value: Field<T>): Condition {
 }
 
 @Support
-inline fun <reified T: Any> Select<Record1<T>>.le(value: Select<out Record1<T>>): Condition {
-    return field(this).le(value);
-}
-
-@Support
-inline fun <reified T: Any> Select<Record1<T>>.le(value: QuantifiedSelect<out Record1<T>>): Condition {
-    return field(this).le(value);
-}
-
-@Support
 inline fun <reified T: Any> Select<Record1<T>>.lessOrEqual(value: T): Condition {
     return field(this).lessOrEqual(value);
 }
 
 @Support
 inline fun <reified T: Any> Select<Record1<T>>.lessOrEqual(value: Field<T>): Condition {
-    return field(this).lessOrEqual(value);
-}
-
-@Support
-inline fun <reified T: Any> Select<Record1<T>>.lessOrEqual(value: Select<out Record1<T>>): Condition {
-    return field(this).lessOrEqual(value);
-}
-
-@Support
-inline fun <reified T: Any> Select<Record1<T>>.lessOrEqual(value: QuantifiedSelect<out Record1<T>>): Condition {
     return field(this).lessOrEqual(value);
 }
 
@@ -305,11 +185,6 @@ inline fun <reified T: Any> Select<Record1<T>>.`in`(vararg values: Field<*>): Co
 }
 
 @Support
-inline fun <reified T: Any> Select<Record1<T>>.`in`(query: Select<out Record1<T>>): Condition {
-    return field(this).`in`(query)
-}
-
-@Support
 inline fun <reified T: Any> Select<Record1<T>>.notIn(values: Collection<*>): Condition {
     return field(this).notIn(values)
 }
@@ -327,10 +202,5 @@ inline fun <reified T: Any> Select<Record1<T>>.notIn(vararg values: T): Conditio
 @Support
 inline fun <reified T: Any> Select<Record1<T>>.notIn(vararg values: Field<*>): Condition {
     return field(this).notIn(values.asList())
-}
-
-@Support
-inline fun <reified T: Any> Select<Record1<T>>.notIn(query: Select<out Record1<T>>): Condition {
-    return field(this).notIn(query)
 }
 

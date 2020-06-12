9034db7e16cc1e7f63343075211dd84104148ba7
Lukas Eder <lukas.eder@gmail.com>
2020-06-12 16:27:23 +0200

    [jOOQ/jOOQ#7172] [jOOQ/jOOQ#10274] Cannot use getQualifiedName() yet here

--- jOOQ/src/main/java/org/jooq/impl/TableFieldImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/TableFieldImpl.java
@@ -51,6 +51,7 @@ import org.jooq.Name;
 import org.jooq.Record;
 import org.jooq.Table;
 import org.jooq.TableField;
+import org.jooq.tools.StringUtils;
 
 /**
  * A common base type for table fields.
@@ -111,7 +112,9 @@ final class TableFieldImpl<R extends Record, T> extends AbstractField<T> impleme
         // rather expensive implementation of AbstractQueryPart.equals()
         if (that instanceof TableField) {
             TableField<?, ?> other = (TableField<?, ?>) that;
-            return getQualifiedName().equals(other.getQualifiedName());
+            return
+                StringUtils.equals(getTable(), other.getTable()) &&
+                StringUtils.equals(getName(), other.getName());
         }
 
         return super.equals(that);

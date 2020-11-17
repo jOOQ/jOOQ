0b221c68f8d8b5a0459db6726ac9518d41810001
Lukas Eder <lukas.eder@gmail.com>
2020-11-17 14:29:11 +0100

    [jOOQ/jOOQ#10697] Improve style of generated ANSI JOIN
    By avoiding nesting joins unnecessarily, we can maintain more intuitive:
    
    - Lexical table ordering when creating trees
    - Flat join syntax rather than nesting too deeply

--- jOOQ/src/main/java/org/jooq/impl/SelectQueryImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/SelectQueryImpl.java
@@ -2653,6 +2653,23 @@ final class SelectQueryImpl<R extends Record> extends AbstractResultQuery<R> imp
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     private final void toSQLOrderBy(
         final Context<?> ctx,
         final Field<?>[] originalFields,

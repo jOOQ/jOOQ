8de5f091650af038837baa78b1572cb1e45f452c
Lukas Eder <lukas.eder@gmail.com>
2020-11-19 15:35:39 +0100

    [jOOQ/jOOQ#10990] Add support for MERGE in Informix

--- jOOQ/src/main/java/org/jooq/MergeNotMatchedSetStep.java
+++ jOOQ/src/main/java/org/jooq/MergeNotMatchedSetStep.java
@@ -37,9 +37,6 @@
  */
 package org.jooq;
 
-import org.jetbrains.annotations.*;
-
-
 import static org.jooq.SQLDialect.CUBRID;
 // ...
 import static org.jooq.SQLDialect.DERBY;
@@ -52,8 +49,11 @@ import static org.jooq.SQLDialect.HSQLDB;
 // ...
 // ...
 // ...
+// ...
 
 import java.util.Map;
+
+import org.jetbrains.annotations.NotNull;
 
 /**
  * This type is used for the {@link Merge}'s DSL API.

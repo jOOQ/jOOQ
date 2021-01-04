53fff73f848f9b084964d2daf738795c9678d48b
Lukas Eder <lukas.eder@gmail.com>
2021-01-04 16:38:48 +0100

    [jOOQ/jOOQ#11179] Compilation errors in generated DAOs when embeddables use <replacesFields/>

--- jOOQ-codegen/src/main/java/org/jooq/codegen/JavaGenerator.java
+++ jOOQ-codegen/src/main/java/org/jooq/codegen/JavaGenerator.java
@@ -4135,6 +4135,14 @@ public class JavaGenerator extends AbstractGenerator {
             out.println("}");
 
         for (ColumnDefinition column : table.getColumns()) {
+
+
+
+
+
+
+
+
             final String colName = column.getOutputName();
             final String colClass = getStrategy().getJavaClassName(column);
             final String colTypeFull = getJavaType(column.getType(resolver(out)), out);

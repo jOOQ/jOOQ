8bc18a566e53664f242d6e98934585c93b7e2772
Lukas Eder <lukas.eder@gmail.com>
2021-01-20 10:11:51 +0100

    [jOOQ/jOOQ#11255] Meta.getTables() does not include PostgreSQL foreign data wrapper tables

--- jOOQ/src/main/java/org/jooq/impl/MetaImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/MetaImpl.java
@@ -335,13 +335,13 @@ final class MetaImpl extends AbstractMeta {
 
                     switch (family()) {
 
-                        // [#3977] PostgreSQL returns other object types, too
+                        // [#3977] [#11255] PostgreSQL returns other object types, too
 
 
 
 
                         case POSTGRES:
-                            types = new String[] { "TABLE", "VIEW", "SYSTEM_TABLE", "SYSTEM_VIEW", "MATERIALIZED VIEW" };
+                            types = new String[] { "FOREIGN TABLE", "MATERIALIZED VIEW", "SYSTEM_TABLE", "SYSTEM_VIEW", "TABLE", "VIEW" };
                             break;
 
                         // [#2323] SQLite JDBC drivers have a bug. They return other

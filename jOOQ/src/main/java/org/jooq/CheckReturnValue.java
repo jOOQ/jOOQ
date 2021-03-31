077685fbd224034e60dc583b378d3079a89ed908
Lukas Eder <lukas.eder@gmail.com>
2021-03-31 09:27:23 +0200

    [jOOQ/jOOQ#11718] Change retention to CLASS and make it @Internal

--- jOOQ/src/main/java/org/jooq/CheckReturnValue.java
+++ jOOQ/src/main/java/org/jooq/CheckReturnValue.java
@@ -7,10 +7,16 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import org.jetbrains.annotations.Contract;
+
 /**
  * Simple version of the JSR 305 annotation that allows for inspecting jOOQ code
  * and detect accidentally omitted calls to {@link Query#execute()} and the
  * likes in IntelliJ.
+ * <p>
+ * This annotation is {@link Internal}. Clients should not rely on its presence.
+ * The annotation may be replaced with a more suitable one by JetBrains, e.g.
+ * the {@link Contract} annotation, when a favourable use-case can be found.
  *
  * @author Lukas Eder
  * @see <a href=
@@ -20,6 +26,7 @@ import java.lang.annotation.Target;
  */
 @Documented
 @Target(METHOD)
-@Retention(SOURCE)
+@Retention(CLASS)
+@Internal
 public @interface CheckReturnValue {
 }

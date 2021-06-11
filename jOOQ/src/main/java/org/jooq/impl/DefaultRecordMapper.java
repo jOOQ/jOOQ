08cfd28898332391a3e66cda20d3a44a9c4708d6
Lukas Eder <lukas.eder@gmail.com>
2021-06-11 11:20:04 +0200

    [jOOQ/jOOQ#11148] Regression calling Record1<JSON>.into(OtherTableRecord)

--- jOOQ/src/main/java/org/jooq/impl/DefaultRecordMapper.java
+++ jOOQ/src/main/java/org/jooq/impl/DefaultRecordMapper.java
@@ -370,14 +370,9 @@ public class DefaultRecordMapper<R extends Record, E> implements RecordMapper<R,
         }
 
 
-        // [#10071] Single-field Record1 types can be mapped if there is a ConverterProvider allowing for this mapping
-        if (fields.length == 1 && Tools.converter(configuration, fields[0].getType(), type) != null) {
-            delegate = new ValueTypeMapper();
-            return;
-        }
-
         // [#1470] Return a proxy if the supplied type is an interface
-        if (Modifier.isAbstract(type.getModifiers())) {
+        // [#10071] [#11148] Primitive types are abstract! They're mapped by a ConverterProvider only later
+        if (Modifier.isAbstract(type.getModifiers()) && !type.isPrimitive()) {
             delegate = new ProxyMapper();
             return;
         }
@@ -385,6 +380,12 @@ public class DefaultRecordMapper<R extends Record, E> implements RecordMapper<R,
         // [#2989] [#2836] Records are mapped
         if (AbstractRecord.class.isAssignableFrom(type)) {
             delegate = (RecordMapper<R, E>) new RecordToRecordMapper();
+            return;
+        }
+
+        // [#10071] Single-field Record1 types can be mapped if there is a ConverterProvider allowing for this mapping
+        if (fields.length == 1 && Tools.converter(configuration, fields[0].getType(), type) != null) {
+            delegate = new ValueTypeMapper();
             return;
         }
 

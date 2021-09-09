61697eb11a97e2ef44609e4a0f51a80dc7584947
Lukas Eder <lukas.eder@gmail.com>
2021-09-09 09:44:17 +0200

    [jOOQ/jOOQ#12415] Add native support for BIT_AND_AGG() and BIT_OR_AGG() in PostgreSQL

--- jOOQ/src/main/java/org/jooq/impl/BitAndAgg.java
+++ jOOQ/src/main/java/org/jooq/impl/BitAndAgg.java
@@ -83,7 +83,7 @@ extends
 
 
 
-    public static final Set<SQLDialect> NO_SUPPORT_NATIVE = SQLDialect.supportedUntil(CUBRID, DERBY, FIREBIRD, HSQLDB, POSTGRES, SQLITE);
+    public static final Set<SQLDialect> NO_SUPPORT_NATIVE = SQLDialect.supportedUntil(CUBRID, DERBY, FIREBIRD, HSQLDB, SQLITE);
 
     @Override
     public final void accept(Context<?> ctx) {

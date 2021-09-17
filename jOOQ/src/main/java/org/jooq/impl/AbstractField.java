7a180187b9647d931e79ded43d2da3ef86b90571
Lukas Eder <lukas.eder@gmail.com>
2021-09-17 22:11:28 +0200

    [jOOQ/jOOQ#12453] Revert

--- jOOQ/src/main/java/org/jooq/impl/AbstractField.java
+++ jOOQ/src/main/java/org/jooq/impl/AbstractField.java
@@ -1421,18 +1421,6 @@ abstract class AbstractField<T> extends AbstractTypedNamed<T> implements Field<T
     @Override
     public final Condition compare(Comparator comparator, Field<T> field) {
         switch (comparator) {
-            case IN:
-                if (field instanceof ScalarSubquery)
-                    return new CompareCondition(this, field, comparator);
-                else
-                    return in(field);
-
-            case NOT_IN:
-                if (field instanceof ScalarSubquery)
-                    return new CompareCondition(this, field, comparator);
-                else
-                    return notIn(field);
-
             case IS_DISTINCT_FROM:
                 return new IsDistinctFrom<>(this, nullSafe(field, getDataType()));
             case IS_NOT_DISTINCT_FROM:

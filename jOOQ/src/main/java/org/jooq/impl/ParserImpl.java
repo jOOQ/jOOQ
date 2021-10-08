f17f1e2a468225f1b64bac6da3f4c61cfe7197e4
Lukas Eder <lukas.eder@gmail.com>
2021-10-08 14:20:18 +0200

    [jOOQ/jOOQ#12495] Ignored COMMENT ON CONSTRAINT syntax should support qualified object identifiers

--- jOOQ/src/main/java/org/jooq/impl/ParserImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/ParserImpl.java
@@ -2573,7 +2573,7 @@ final class DefaultParseContext extends AbstractScope implements ParseContext {
             parseIdentifier();
             parseKeyword("ON");
             parseKeywordIf("DOMAIN");
-            parseIdentifier();
+            parseName();
             parseKeyword("IS");
             parseStringLiteral();
             return IGNORE;

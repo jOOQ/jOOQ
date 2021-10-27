3f0e0cf7a727e99a08fcf27861152e7d42fcda56
Lukas Eder <lukas.eder@gmail.com>
2021-10-27 14:21:36 +0200

    [#12425] [#12561] Fix ROWNUM transformation
    ROWNUM transformation doesn't work when projecting an expression containing ROWNUM. This fix also refactors arithmetic Expressions to their own classes, including internal IAdd, ISub, IMul, IDiv for the unneeded arithmetic transformation (further refactorings scheduled)

---
+++ jOOQ/src/main/java/org/jooq/impl/ISub.java
@@ -0,0 +1,153 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Other licenses:
+ * -----------------------------------------------------------------------------
+ * Commercial licenses for this work are available. These replace the above
+ * ASL 2.0 and offer limited warranties, support, maintenance, and commercial
+ * database integrations.
+ *
+ * For more information, please visit: http://www.jooq.org/licenses
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+package org.jooq.impl;
+
+import static org.jooq.impl.DSL.*;
+import static org.jooq.impl.Internal.*;
+import static org.jooq.impl.Keywords.*;
+import static org.jooq.impl.Names.*;
+import static org.jooq.impl.SQLDataType.*;
+import static org.jooq.impl.Tools.*;
+import static org.jooq.impl.Tools.BooleanDataKey.*;
+import static org.jooq.impl.Tools.DataExtendedKey.*;
+import static org.jooq.impl.Tools.DataKey.*;
+import static org.jooq.SQLDialect.*;
+
+import org.jooq.*;
+import org.jooq.Function1;
+import org.jooq.Record;
+import org.jooq.conf.*;
+import org.jooq.impl.*;
+import org.jooq.impl.QOM.*;
+import org.jooq.tools.*;
+
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
+
+
+/**
+ * The <code>SUB</code> statement.
+ */
+@SuppressWarnings({ "rawtypes", "unchecked", "unused" })
+final class ISub<T>
+extends
+    AbstractTransformable<T>
+implements
+    QOM.Sub<T>
+{
+
+    final Field<T> arg1;
+    final Field<T> arg2;
+
+    ISub(
+        Field<T> arg1,
+        Field<T> arg2
+    ) {
+        super(
+            N_SUB,
+            allNotNull((DataType) dataType(arg1), arg1, arg2)
+        );
+
+        this.arg1 = nullSafeNotNull(arg1, (DataType) OTHER);
+        this.arg2 = nullSafeNotNull(arg2, (DataType) OTHER);
+    }
+
+    // -------------------------------------------------------------------------
+    // XXX: QueryPart API
+    // -------------------------------------------------------------------------
+
+    @Override
+    public final void accept0(Context<?> ctx) {
+        ctx.visit(transform(Sub::new));
+    }
+
+
+
+    @Override
+    public final Field<?> transform(TransformUnneededArithmeticExpressions transform) {
+        return Expression.transform(this, arg1, ExpressionOperator.SUBTRACT, arg2, true, transform);
+    }
+
+
+
+    // -------------------------------------------------------------------------
+    // XXX: Query Object Model
+    // -------------------------------------------------------------------------
+
+    @Override
+    public final Field<T> $arg1() {
+        return arg1;
+    }
+
+    @Override
+    public final Field<T> $arg2() {
+        return arg2;
+    }
+
+    @Override
+    public final QOM.Sub<T> $arg1(Field<T> newValue) {
+        return constructor().apply(newValue, $arg2());
+    }
+
+    @Override
+    public final QOM.Sub<T> $arg2(Field<T> newValue) {
+        return constructor().apply($arg1(), newValue);
+    }
+
+    @Override
+    public final Function2<? super Field<T>, ? super Field<T>, ? extends QOM.Sub<T>> constructor() {
+        return (a1, a2) -> new ISub<>(a1, a2);
+    }
+
+    // -------------------------------------------------------------------------
+    // XXX: The Object API
+    // -------------------------------------------------------------------------
+
+    @Override
+    public boolean equals(Object that) {
+        if (that instanceof ISub) { ISub o = (ISub) that;
+            return
+                StringUtils.equals($arg1(), o.$arg1()) &&
+                StringUtils.equals($arg2(), o.$arg2())
+            ;
+        }
+        else
+            return super.equals(that);
+    }
+}

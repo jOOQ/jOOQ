f0a839df8de05f4e58769676602d852a2ef8efe2
Lukas Eder <lukas.eder@gmail.com>
2021-11-09 14:18:06 +0100

    [jOOQ/jOOQ#9917] Codegeneration fails with case sensitive data types in postgres

--- jOOQ-meta/src/main/java/org/jooq/meta/postgres/PostgresDatabase.java
+++ jOOQ-meta/src/main/java/org/jooq/meta/postgres/PostgresDatabase.java
@@ -70,6 +70,7 @@ import static org.jooq.impl.DSL.row;
 import static org.jooq.impl.DSL.rowNumber;
 import static org.jooq.impl.DSL.select;
 import static org.jooq.impl.DSL.selectFrom;
+import static org.jooq.impl.DSL.sql;
 import static org.jooq.impl.DSL.table;
 import static org.jooq.impl.DSL.val;
 import static org.jooq.impl.DSL.values;
@@ -1177,7 +1178,9 @@ public class PostgresDatabase extends AbstractDatabase implements ResultQueryDat
         Field<?> orderBy =
             exists(PG_ENUM.ENUMSORTORDER)
           ? PG_ENUM.ENUMSORTORDER
-          : field("{0}::{1}", PG_ENUM.ENUMLABEL, name(nspname, typname));
+
+        // [#9511] [#9917] Workaround for regression introduced by avoiding quoting names everywhere
+          : field("{0}::{1}", PG_ENUM.ENUMLABEL, sql(name(nspname, typname) + ""));
 
         if (canCastToEnumType == null) {
 

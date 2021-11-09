f2a7dfd8815697d8212badbc630de073a472efec
Lukas Eder <lukas.eder@gmail.com>
2021-11-09 09:14:24 +0100

    [jOOQ/jOOQ#12611] SQLDataType.null generated when using XMLDatabase with lower case array type

--- jOOQ-meta/src/main/java/org/jooq/meta/AbstractDatabase.java
+++ jOOQ-meta/src/main/java/org/jooq/meta/AbstractDatabase.java
@@ -2796,16 +2796,15 @@ public abstract class AbstractDatabase implements Database {
             case H2:
             case POSTGRES:
             case YUGABYTE:
-                return "ARRAY".equals(dataType);
+                return "ARRAY".equals(dataType.toUpperCase());
 
 
             case HSQLDB:
+            default:
                 // TODO: Is there any more robust way to recognise these?
                 // For instance, there could be a UDT that is called this way
-                return dataType.endsWith(" ARRAY");
+                return dataType.toUpperCase().endsWith(" ARRAY");
         }
-
-        return false;
     }
 
     protected static final String fetchedSize(Collection<?> fetched, Collection<?> included) {

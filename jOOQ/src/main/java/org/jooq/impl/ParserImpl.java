b2465ca45bebb737359db0c05a5c1a9478376bb7
Lukas Eder <lukas.eder@gmail.com>
2021-11-23 14:09:25 +0100

    [jOOQ/jOOQ#12645] Parser should support PostgreSQL date part syntaxes with explicit type casts

--- jOOQ/src/main/java/org/jooq/impl/ParserImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/ParserImpl.java
@@ -10106,6 +10106,11 @@ final class DefaultParseContext extends AbstractScope implements ParseContext {
         else if (string)
             parse('\'');
 
+        // [#12645] In PostgreSQL, function based indexes tend to cast the
+        //          date part to a type explicitly
+        if (parseIf("::"))
+            parseDataType();
+
         return result;
     }
 

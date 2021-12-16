14e5358824d78b9bb61f899a5a84e45e97d93481
Lukas Eder <lukas.eder@gmail.com>
2021-12-16 15:51:12 +0100

    [jOOQ/jOOQ#9077] Add a Spring Boot example, which modifies the Settings for jOOQ

---
+++ jOOQ-examples/jOOQ-spring-boot-example/src/main/java/org/jooq/example/spring/Config.java
@@ -0,0 +1,29 @@
+package org.jooq.example.spring;
+
+import org.jooq.conf.RenderQuotedNames;
+import org.jooq.impl.DefaultConfiguration;
+import org.springframework.boot.autoconfigure.jooq.DefaultConfigurationCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+/**
+ * Optionally, add additional configuration beans
+ *
+ * @author Lukas Eder
+ */
+@Component
+public class Config {
+
+	/**
+	 * Add custom jOOQ configuration.
+	 * <p>
+	 * The {@link DefaultConfigurationCustomizer} type has been added in Spring Boot
+	 * 2.5 to facilitate customising the out of the box provided jOOQ
+	 * {@link DefaultConfiguration}.
+	 */
+	@Bean
+	public DefaultConfigurationCustomizer configurationCustomiser() {
+		return c -> c.settings()
+				.withRenderQuotedNames(RenderQuotedNames.EXPLICIT_DEFAULT_UNQUOTED);
+	}
+}

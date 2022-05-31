5bc526169087f34ccc10baca5e8e4440d4edd7f1
Lukas Eder <lukas.eder@gmail.com>
2022-05-31 14:02:08 +0200

    [jOOQ/jOOQ#13605] Add explicit jackson core dependency, too

--- jOOQ-jackson-extensions/src/main/java/module-info.java
+++ jOOQ-jackson-extensions/src/main/java/module-info.java
@@ -8,6 +8,7 @@ module org.jooq.jackson.extensions {
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.datatype.jsr310;
     requires com.fasterxml.jackson.kotlin;
+    requires com.fasterxml.jackson.core;
 
     // Nullability annotations for better Kotlin interop
     requires static org.jetbrains.annotations;

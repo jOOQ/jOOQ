d3cbf1dabc161a2d1ee3a1f3c53dc26372e4e853
Lukas Eder <lukas.eder@gmail.com>
2022-07-04 11:25:56 +0200

    [jOOQ/jOOQ#13751] GROUP BY <table> does not use table alias

--- jOOQ/src/main/java/org/jooq/impl/GroupFieldList.java
+++ jOOQ/src/main/java/org/jooq/impl/GroupFieldList.java
@@ -133,6 +133,6 @@ final class GroupFieldList extends QueryPartList<GroupField> {
         if (pk == null || NO_SUPPORT_GROUP_FUNCTIONAL_DEP.contains(ctx.dialect()))
             return t.fields();
         else
-            return pk.getFieldsArray();
+            return t.fields(pk.getFieldsArray());
     }
 }

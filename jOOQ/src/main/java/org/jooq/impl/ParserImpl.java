1b43bacca74353803113c48f099ced5c7fcc7128
Lukas Eder <lukas.eder@gmail.com>
2022-08-18 09:40:10 +0200

    [jOOQ/jOOQ#13882] Parse length on unknown data types

--- jOOQ/src/main/java/org/jooq/impl/ParserImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/ParserImpl.java
@@ -12153,7 +12153,7 @@ final class DefaultParseContext extends AbstractScope implements ParseContext {
 
         Name name;
         if (parseUnknownTypes && (name = parseNameIf()) != null)
-            return new DefaultDataType(dsl.dialect(), Object.class, name);
+            return parseDataTypeLength(new DefaultDataType(dsl.dialect(), Object.class, name));
         else
             return null;
     }

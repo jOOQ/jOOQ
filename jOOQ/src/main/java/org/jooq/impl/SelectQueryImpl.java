63321c95d6c3767b911acd30a8ab92f61fbf1455
Lukas Eder <lukas.eder@gmail.com>
2022-12-01 13:12:51 +0100

    [jOOQ/jOOQ#14346] Error: Invalid Top N Value: N is too big for Teradata OFFSET emulation

--- jOOQ/src/main/java/org/jooq/impl/SelectQueryImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/SelectQueryImpl.java
@@ -3123,6 +3123,10 @@ final class SelectQueryImpl<R extends Record> extends AbstractResultQuery<R> imp
 
 
 
+
+
+
+
     private static final Set<SQLDialect> NO_SUPPORT_UNION_PARENTHESES = SQLDialect.supportedBy(SQLITE);
     private static final Set<SQLDialect> NO_SUPPORT_CTE_IN_UNION      = SQLDialect.supportedBy(HSQLDB, MARIADB);
     private static final Set<SQLDialect> UNION_PARENTHESIS            = SQLDialect.supportedBy(DERBY, MARIADB, MYSQL);

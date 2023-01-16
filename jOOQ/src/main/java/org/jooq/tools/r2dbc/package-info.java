ac5e30c75f71d6e33f6117ea13849539ed050141
Lukas Eder <lukas.eder@gmail.com>
2023-01-16 10:48:36 +0100

    [jOOQ/jOOQ#14499] Added package-info.java

---
+++ jOOQ/src/main/java/org/jooq/tools/r2dbc/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * R2DBC tooling.
+ * <p>
+ * This package contains
+ * <ul>
+ * <li>A {@link org.jooq.tools.r2dbc.DefaultConnection} that implements all the
+ * API and delegates to another connection, useful as base class for other
+ * utilities</li>
+ * <li>A {@link org.jooq.tools.r2dbc.LoggingConnection}, which logs all
+ * statements that are executed by it</li>
+ * </ul>
+ */
+package org.jooq.tools.r2dbc;

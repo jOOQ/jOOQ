846977558f5202d94ac2401895a469631f3125a7
Lukas Eder <lukas.eder@gmail.com>
2023-01-16 10:48:28 +0100

    [jOOQ/jOOQ#14499] Added package-info.java

--- jOOQ/src/main/java/org/jooq/tools/jdbc/package-info.java
+++ jOOQ/src/main/java/org/jooq/tools/jdbc/package-info.java
@@ -1,5 +1,19 @@
 /**
- * This package contains compatibility classes to bridge between JDBC 4.0 and
- * JDBC 4.1.
+ * JDBC tooling.
+ * <p>
+ * This package contains
+ * <ul>
+ * <li>Compatibility classes to bridge between JDBC 4.0, 4.1, 4.2, 4.3 (see
+ * {@link org.jooq.tools.jdbc.JDBC41Connection})</li>
+ * <li>The {@link org.jooq.tools.jdbc.BatchedConnection} that allows for
+ * batching statements transparently on a JDBC level</li>
+ * <li>A {@link org.jooq.tools.jdbc.DefaultConnection} that implements all the
+ * API and delegates to another connection, useful as base class for other
+ * utilities</li>
+ * <li>A {@link org.jooq.tools.jdbc.MockConnection}, which mocks the JDBC API
+ * based on fixed data</li>
+ * <li>A {@link org.jooq.tools.jdbc.LoggingConnection}, which logs all
+ * statements that are executed by it</li>
+ * </ul>
  */
 package org.jooq.tools.jdbc;

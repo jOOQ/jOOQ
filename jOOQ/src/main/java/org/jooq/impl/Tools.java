5c38e32d4f935d00d923bac77b9429e29180f999
Lukas Eder <lukas.eder@gmail.com>
2023-02-22 11:33:16 +0100

    [jOOQ/jOOQ#14668] Wrong ON KEY JOIN generated when aliased table appears twice in the JOIN tree

--- jOOQ/src/main/java/org/jooq/impl/Tools.java
+++ jOOQ/src/main/java/org/jooq/impl/Tools.java
@@ -5803,11 +5803,15 @@ final class Tools {
     }
 
     static final <R extends Record> Table<R> unwrap(Table<R> table) {
-        Table<R> r;
+        return unwrap(table, true);
+    }
+
+    static final <R extends Record> Table<R> unwrap(Table<R> table, boolean unalias) {
+        Table<R> r = table;
 
         if (table instanceof AbstractDelegatingTable)
             return unwrap(((AbstractDelegatingTable<R>) table).delegate);
-        else if ((r = unalias(table)) != table)
+        else if (unalias && (r = unalias(table)) != table)
             return unwrap(r);
         else
             return r;
@@ -6701,15 +6705,21 @@ final class Tools {
         return (r, t) -> r || unaliased.equals(f.apply(t));
     }
 
+    static final boolean containsTable(Table<?> in, Table<?> search, boolean unalias) {
+
+        // [#6304] [#7626] [#14668] Improved alias discovery
+        return traverseJoins(in, false, r -> r, search(search, t -> unwrap(t, unalias)));
+    }
+
     static final boolean containsUnaliasedTable(Table<?> in, Table<?> search) {
 
-        // [#6304] [#7626] Improved alias discovery
+        // [#6304] [#7626] [#14668] Improved alias discovery
         return traverseJoins(in, false, r -> r, search(search, Tools::unwrap));
     }
 
     static final boolean containsUnaliasedTable(Iterable<? extends Table<?>> in, Table<?> search) {
 
-        // [#6304] [#7626] Improved alias discovery
+        // [#6304] [#7626] [#14668] Improved alias discovery
         return traverseJoins(in, false, r -> r, search(search, Tools::unwrap));
     }
 

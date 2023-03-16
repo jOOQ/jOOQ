3d19666c3d85ec1811b4b6163772d5368ede254b
Lukas Eder <lukas.eder@gmail.com>
2023-03-16 13:41:03 +0100

    [jOOQ/jOOQ#14800] Fix namespace

--- jOOQ/src/main/java/org/jooq/conf/package-info.java
+++ jOOQ/src/main/java/org/jooq/conf/package-info.java
@@ -1,2 +1,2 @@
-@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://www.jooq.org/xsd/jooq-runtime-3.18.0.xsd", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
+@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://www.jooq.org/xsd/jooq-runtime-3.19.0.xsd", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package org.jooq.conf;

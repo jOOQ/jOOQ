9a03b6f5a3cf4ac60adee1bbf5b55637a5c6d8bc
Lukas Eder <lukas.eder@gmail.com>
2023-03-21 09:01:58 +0100

    [jOOQ/jOOQ#14818] Add a remark on the Converter Javadoc that implementations must be able to handle NULL

--- jOOQ/src/main/java/org/jooq/Converter.java
+++ jOOQ/src/main/java/org/jooq/Converter.java
@@ -43,7 +43,6 @@ import static org.jooq.Converters.nullable;
 import java.io.Serializable;
 import java.util.function.Function;
 
-import org.jooq.exception.DataTypeException;
 import org.jooq.impl.AbstractConverter;
 import org.jooq.impl.SQLDataType;
 
@@ -80,6 +79,10 @@ import org.jetbrains.annotations.NotNull;
  * <li><code>converter.from(null) == null</code></li>
  * <li><code>converter.to(null) == null</code></li>
  * </ul>
+ * <p>
+ * Irrespective of the <code>Converter</code>'s encoding of <code>null</code>
+ * values above, an implementation must be able to handle <code>null</code>
+ * values.
  * <p>
  * <h3>Creating user defined {@link DataType}s</h3>
  * <p>

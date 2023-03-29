838e24d50c34e7989f01eb97ae05de9a1f7fca22
Lukas Eder <lukas.eder@gmail.com>
2023-03-29 11:21:01 +0200

    [jOOQ/jOOQ#14876] Add warnings to manual and Javadoc about ON KEY ambiguity caveats

--- jOOQ/src/main/java/org/jooq/SelectOnStep.java
+++ jOOQ/src/main/java/org/jooq/SelectOnStep.java
@@ -204,7 +204,10 @@ public interface SelectOnStep<R extends Record> {
      *
      * @see TableOnStep#onKey(ForeignKey)
      * @throws DataAccessException If there is no non-ambiguous key definition
-     *             known to jOOQ
+     *             known to jOOQ. <em>Please note that if you evolve your
+     *             schema, a previously non-ambiguous <code>ON KEY</code> clause
+     *             can suddenly become ambiguous on an existing query, so use
+     *             this clause with care.</em>
      */
     @NotNull @CheckReturnValue
     @Support
@@ -218,7 +221,10 @@ public interface SelectOnStep<R extends Record> {
      *
      * @see TableOnStep#onKey(ForeignKey)
      * @throws DataAccessException If there is no non-ambiguous key definition
-     *             known to jOOQ
+     *             known to jOOQ. <em>Please note that if you evolve your
+     *             schema, a previously non-ambiguous <code>ON KEY</code> clause
+     *             can suddenly become ambiguous on an existing query, so use
+     *             this clause with care.</em>
      */
     @NotNull @CheckReturnValue
     @Support

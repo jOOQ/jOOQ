b186f83355fcf1ec4347959ca8a95c072871521b
Lukas Eder <lukas.eder@gmail.com>
2023-05-12 11:46:40 +0200

    [jOOQ/jOOQ#15048] DDLDatabase defaultNameCase=lower doesn't work with ENUM types

--- jOOQ/src/main/java/org/jooq/impl/Tools.java
+++ jOOQ/src/main/java/org/jooq/impl/Tools.java
@@ -5965,6 +5965,9 @@ final class Tools {
 
 
 
+        // [#15048] User defined types may need quoting, etc.
+        else if (type.getType() == Object.class && !(type instanceof BuiltInDataType))
+            ctx.visit(type.getQualifiedName());
         else
             ctx.sql(typeName);
 

dc7d44375a72ca06d9e172dbd2f973de71b4e310
Robin Trietsch <robin.trietsch@gmail.com>
2023-05-23 14:45:28 +0200

    Add argument to provide coroutine context for the transactionCoroutine

--- jOOQ-kotlin-coroutines/src/main/kotlin/org/jooq/kotlin/coroutines/CoroutineExtensions.kt
+++ jOOQ-kotlin-coroutines/src/main/kotlin/org/jooq/kotlin/coroutines/CoroutineExtensions.kt
@@ -4,15 +4,17 @@ import kotlinx.coroutines.reactive.awaitFirstOrNull
 import kotlinx.coroutines.reactor.mono
 import org.jooq.Configuration
 import org.jooq.DSLContext
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
 // ----------------------------------------------------------------------------
 // Extensions to bridge between the reactive-streams and the coroutine world
 // ----------------------------------------------------------------------------
 
-suspend fun <T> DSLContext.transactionCoroutine(transactional: suspend (Configuration) -> T): T {
+suspend fun <T> DSLContext.transactionCoroutine(context: CoroutineContext = EmptyCoroutineContext, transactional: suspend (Configuration) -> T): T {
     @Suppress("UNCHECKED_CAST")
     return transactionPublisher { c ->
-        mono {
+        mono(context) {
             transactional.invoke(c)
         }
     }.awaitFirstOrNull() as T

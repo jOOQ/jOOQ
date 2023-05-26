94eb4e5af9ceb628dad4c2be16b272e2335e03e6
Lukas Eder <lukas.eder@gmail.com>
2023-05-26 15:48:00 +0200

    [jOOQ/jOOQ#14997] Use awaitSingle instead of awaitFirstOrNull in transactionCoroutine to correctly sequence transaction script

--- jOOQ-kotlin-coroutines/src/main/kotlin/org/jooq/kotlin/coroutines/CoroutineExtensions.kt
+++ jOOQ-kotlin-coroutines/src/main/kotlin/org/jooq/kotlin/coroutines/CoroutineExtensions.kt
@@ -1,6 +1,8 @@
 package org.jooq.kotlin.coroutines
 
-import kotlinx.coroutines.reactive.awaitFirstOrNull
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlinx.coroutines.reactive.awaitSingle
 import kotlinx.coroutines.reactor.mono
 import org.jooq.Configuration
 import org.jooq.DSLContext
@@ -9,11 +11,18 @@ import org.jooq.DSLContext
 // Extensions to bridge between the reactive-streams and the coroutine world
 // ----------------------------------------------------------------------------
 
-suspend fun <T> DSLContext.transactionCoroutine(transactional: suspend (Configuration) -> T): T {
-    @Suppress("UNCHECKED_CAST")
+suspend fun <T> DSLContext.transactionCoroutine(transactional: suspend (Configuration) -> T): T =
+    transactionCoroutine(EmptyCoroutineContext, transactional)
+
+
+suspend fun <T> DSLContext.transactionCoroutine(context: CoroutineContext, transactional: suspend (Configuration) -> T): T {
+    // [#14997] Wrap values in an auxiliary class in order to allow nulls, which awaitSingle()
+    //          doesn't allow, otherwise
+    data class Wrap<T>(val t: T)
+
     return transactionPublisher { c ->
-        mono {
-            transactional.invoke(c)
+        mono(context) {
+            Wrap(transactional.invoke(c))
         }
-    }.awaitFirstOrNull() as T
+    }.awaitSingle().t
 }

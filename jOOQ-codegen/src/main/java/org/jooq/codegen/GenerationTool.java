d383805398aead3cb92e1657984f87e9150a5f35
Lukas Eder <lukas.eder@gmail.com>
2023-06-02 12:46:07 +0200

    [jOOQ/jOOQ#15168] NullPointerException in code generator when omitting <target> directory

--- jOOQ-codegen/src/main/java/org/jooq/codegen/GenerationTool.java
+++ jOOQ-codegen/src/main/java/org/jooq/codegen/GenerationTool.java
@@ -1005,7 +1005,7 @@ public class GenerationTool {
     ) {
         String p = System.getProperty(property);
 
-        if (override ? p != null : checkDefault.test(get.apply(configurationObject)))
+        if (p != null && (override || checkDefault.test(get.apply(configurationObject))))
             set.accept(configurationObject, convert.apply(p));
     }
 

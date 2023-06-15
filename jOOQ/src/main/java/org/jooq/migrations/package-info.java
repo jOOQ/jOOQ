f33cb9b04c15c934869fb935c36f5fb7f5103beb
Lukas Eder <lukas.eder@gmail.com>
2023-06-15 08:35:09 +0200

    [jOOQ/jOOQ#9506] Various improvements

---
+++ jOOQ/src/main/java/org/jooq/migrations/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains API for jOOQ the jOOQ migrations feature set, available
+ * via the API entry point {@link org.jooq.DSLContext#migrations()}.
+ */
+package org.jooq.migrations;

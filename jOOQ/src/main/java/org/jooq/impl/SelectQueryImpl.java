f99e82f5f1338b81d25443b90b8bd4b7454e64c8
Lukas Eder <lukas.eder@gmail.com>
2023-07-22 16:08:46 +0200

    [jOOQ/jOOQ#15423] Table list to ANSI JOIN transformation doesn't work if table declarations are fully qualified, but references are not

--- jOOQ/src/main/java/org/jooq/impl/SelectQueryImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/SelectQueryImpl.java
@@ -247,6 +247,8 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -2807,6 +2809,29 @@ final class SelectQueryImpl<R extends Record> extends AbstractResultQuery<R> imp
         else
             return table;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

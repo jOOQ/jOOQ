6255dd68091a5116cf170f2b83b7c3b47a42960e
Lukas Eder <lukas.eder@datageekery.com>
2023-08-23 14:33:01 +0200

    [jOOQ/jOOQ#15497] Work around SQL Server's 1000 row value limitation for INSERT statements

--- jOOQ/src/main/java/org/jooq/impl/FieldMapsForInsert.java
+++ jOOQ/src/main/java/org/jooq/impl/FieldMapsForInsert.java
@@ -52,6 +52,7 @@ import static org.jooq.conf.WriteIfReadonly.IGNORE;
 import static org.jooq.conf.WriteIfReadonly.THROW;
 import static org.jooq.impl.DSL.name;
 import static org.jooq.impl.DSL.select;
+import static org.jooq.impl.FieldMapsForInsert.toSQLInsertSelect;
 import static org.jooq.impl.Keywords.K_DEFAULT_VALUES;
 import static org.jooq.impl.Keywords.K_VALUES;
 import static org.jooq.impl.QueryPartCollectionView.wrap;
@@ -174,6 +175,7 @@ final class FieldMapsForInsert extends AbstractQueryPart implements UNotYetImple
             switch (ctx.family()) {
 
                 // Some dialects don't support multi-record inserts
+
 
 
 
@@ -331,6 +333,10 @@ final class FieldMapsForInsert extends AbstractQueryPart implements UNotYetImple
 
     private final boolean supportsValues(Context<?> ctx) {
         switch (ctx.family()) {
+
+
+
+
 
 
 

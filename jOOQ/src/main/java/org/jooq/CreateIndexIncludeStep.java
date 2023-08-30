36d9a2c17ffd6dcc79a4f5a5f6be41cf5c3a3591
Lukas Eder <lukas.eder@datageekery.com>
2023-08-30 10:21:45 +0200

    [jOOQ/jOOQ#15524] Missing Stringly annotation on String... varargs parameters

--- jOOQ/src/main/java/org/jooq/CreateIndexIncludeStep.java
+++ jOOQ/src/main/java/org/jooq/CreateIndexIncludeStep.java
@@ -78,7 +78,7 @@ public interface CreateIndexIncludeStep extends CreateIndexWhereStep {
      */
     @Support
     @NotNull @CheckReturnValue
-    CreateIndexWhereStep include(String... include);
+    CreateIndexWhereStep include(@Stringly.Name String... include);
 
     /**
      * Add the <code>INCLUDE</code> clause to the <code>CREATE INDEX</code> statement.

2b7675766b1e17a8122ee1ebadb8447726cda638
Lukas Eder <lukas.eder@datageekery.com>
2023-09-28 14:40:36 +0200

    [jOOQ/jOOQ#15619] On JDK 21, generated code should apply "this-escape" warning suppression

--- jOOQ/src/main/java/org/jooq/impl/Internal.java
+++ jOOQ/src/main/java/org/jooq/impl/Internal.java
@@ -86,6 +86,8 @@ import org.jooq.UDTRecord;
 import org.jooq.UniqueKey;
 import org.jooq.exception.DataAccessException;
 import org.jooq.impl.QOM.CreateTable;
+import org.jooq.tools.reflect.Reflect;
+import org.jooq.tools.reflect.ReflectException;
 // ...
 // ...
 
@@ -657,5 +659,37 @@ public final class Internal {
         // [#6175] TODO: Speed this up with a faster way to calculate a hash code
         else
             return 0x7FFFFFF & object.hashCode();
+    }
+
+    private static final Lazy<Integer> JAVA_VERSION = Lazy.of(() -> {
+        try {
+
+            return Reflect.onClass(Runtime.class)
+
+                // Since Java 9
+                .call("version")
+
+                // Since Java 10
+                .call("feature")
+                .get();
+        }
+        catch (ReflectException e) {
+            return 8;
+        }
+    });
+
+    /**
+     * Get the Java version (relevant to jOOQ) as an int.
+     * <p>
+     * Supported versions are:
+     * <ul>
+     * <li>8</li>
+     * <li>11</li>
+     * <li>17</li>
+     * <li>21</li>
+     * </ul>
+     */
+    public static final int javaVersion() {
+        return JAVA_VERSION.get();
     }
 }

4eae4cf38ca04c421317deea74f0e3257d9b95f3
Lukas Eder <lukas.eder@gmail.com>
2023-09-29 16:46:56 +0200

    [jOOQ/jOOQ#15564] MutablePOJOMapper should be able to map to instance, even if there isn't a default constructor
    This fixes:
    
    - [jOOQ/jOOQ#15559] KotlinGenerator - isKotlinNotNullPojoAttributes
    setting prevents generated DAO to set serial ID to pojo

--- jOOQ/src/main/java/org/jooq/impl/Tools.java
+++ jOOQ/src/main/java/org/jooq/impl/Tools.java
@@ -4101,7 +4101,7 @@ final class Tools {
             }
 
             return result;
-        }, REFLECTION_CACHE_GET_ANNOTATED_MEMBERS, () -> Cache.key(type, name));
+        }, REFLECTION_CACHE_GET_ANNOTATED_MEMBERS, () -> Cache.key(type, name, makeAccessible));
     }
 
     private static final boolean namesMatch(String name, String annotation) {
@@ -4136,7 +4136,7 @@ final class Tools {
                     result.add(accessible(member, makeAccessible));
 
             return result;
-        }, REFLECTION_CACHE_GET_MATCHING_MEMBERS, () -> Cache.key(type, name));
+        }, REFLECTION_CACHE_GET_MATCHING_MEMBERS, () -> Cache.key(type, name, makeAccessible));
     }
 
     /**
@@ -4187,7 +4187,7 @@ final class Tools {
             }
 
             return SourceMethod.methods(set);
-        }, REFLECTION_CACHE_GET_ANNOTATED_SETTERS, () -> Cache.key(type, name));
+        }, REFLECTION_CACHE_GET_ANNOTATED_SETTERS, () -> Cache.key(type, name, makeAccessible));
     }
 
     /**
@@ -4238,7 +4238,7 @@ final class Tools {
             }
 
             return null;
-        }, REFLECTION_CACHE_GET_ANNOTATED_GETTER, () -> Cache.key(type, name));
+        }, REFLECTION_CACHE_GET_ANNOTATED_GETTER, () -> Cache.key(type, name, makeAccessible));
     }
 
     /**
@@ -4275,7 +4275,7 @@ final class Tools {
             }
 
             return SourceMethod.methods(set);
-        }, REFLECTION_CACHE_GET_MATCHING_SETTERS, () -> Cache.key(type, name));
+        }, REFLECTION_CACHE_GET_MATCHING_SETTERS, () -> Cache.key(type, name, makeAccessible));
     }
 
 
@@ -4310,7 +4310,7 @@ final class Tools {
                         return accessible(method, makeAccessible);
 
             return null;
-        }, REFLECTION_CACHE_GET_MATCHING_GETTER, () -> Cache.key(type, name));
+        }, REFLECTION_CACHE_GET_MATCHING_GETTER, () -> Cache.key(type, name, makeAccessible));
     }
 
     /**

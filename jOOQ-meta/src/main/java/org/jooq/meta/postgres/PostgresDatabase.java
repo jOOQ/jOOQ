22c008df0c578d1b3612d886343bc7d965ea4a30
Lukas Eder <lukas.eder@gmail.com>
2023-10-03 20:21:47 +0200

    [jOOQ/jOOQ#15677] Backport fix

--- jOOQ-meta/src/main/java/org/jooq/meta/postgres/PostgresDatabase.java
+++ jOOQ-meta/src/main/java/org/jooq/meta/postgres/PostgresDatabase.java
@@ -111,6 +111,7 @@ import java.math.BigInteger;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;

be528f2e8e6465d1406f9b36b068675f2fbe83ec
Lukas Eder <lukas.eder@gmail.com>
2023-10-26 17:16:59 +0200

    [jOOQ/jOOQ#15767] MiniJAXB::marshal(XMLAppendable, OutputStream) does not flush decorating writer

--- jOOQ/src/main/java/org/jooq/util/jaxb/tools/MiniJAXB.java
+++ jOOQ/src/main/java/org/jooq/util/jaxb/tools/MiniJAXB.java
@@ -144,6 +144,7 @@ public final class MiniJAXB {
                 builder.append(object);
 
             builder.appendTo(out);
+            out.flush();
         }
         catch (Exception e) {
             throw new ConfigurationException("Cannot print object", e);

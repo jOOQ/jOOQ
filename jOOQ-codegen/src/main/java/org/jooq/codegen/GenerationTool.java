b76718516ffaca43923b0e40e398353105018345
Lukas Eder <lukas.eder@gmail.com>
2023-11-20 16:16:01 +0100

    [jOOQ/jOOQ#15843] Deprecation log warning about <dateAsTimestamp/> should link to relevant manual section

--- jOOQ-codegen/src/main/java/org/jooq/codegen/GenerationTool.java
+++ jOOQ-codegen/src/main/java/org/jooq/codegen/GenerationTool.java
@@ -679,7 +679,7 @@ public class GenerationTool {
             if (d.getEnumTypes().size() > 0)
                 log.warn("DEPRECATED", "The configuration property /configuration/generator/database/enumTypes is experimental and deprecated and will be removed in the future.");
             if (Boolean.TRUE.equals(d.isDateAsTimestamp()))
-                log.warn("DEPRECATED", "The configuration property /configuration/generator/database/dateAsTimestamp is deprecated as it is superseded by custom bindings and converters. It will thus be removed in the future.");
+                log.warn("DEPRECATED", "The configuration property /configuration/generator/database/dateAsTimestamp is deprecated as it is superseded by custom bindings and converters. It will thus be removed in the future. More information here: https://www.jooq.org/doc/latest/manual/reference/reference-data-types/data-types-oracle-date/");
             if (Boolean.TRUE.equals(d.isIgnoreProcedureReturnValues()))
                 log.warn("DEPRECATED", "The <ignoreProcedureReturnValues/> flag is deprecated and used for backwards-compatibility only. It will be removed in the future.");
 

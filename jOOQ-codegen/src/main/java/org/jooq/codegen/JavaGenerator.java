7a8b2b83917fabe800b5173ff6b4b0f536d843b0
Lukas Eder <lukas.eder@gmail.com>
2023-11-20 15:34:49 +0100

    [jOOQ/jOOQ#15848] Compilation error in KotlinGenerator generated code for tables containing a FROM column, when <interfaces> is activated

--- jOOQ-codegen/src/main/java/org/jooq/codegen/JavaGenerator.java
+++ jOOQ-codegen/src/main/java/org/jooq/codegen/JavaGenerator.java
@@ -8793,7 +8793,7 @@ public class JavaGenerator extends AbstractGenerator {
             if (scala)
                 out.println("%s(from.%s)", setter, getter);
             else if (kotlin)
-                out.println("%s = from.%s", member, member);
+                out.println("this.%s = from.%s", member, member);
             else
                 out.println("%s(from.%s());", setter, getter);
         }

fabf806953576612f75d58b3e44af2075be85c5a
Lukas Eder <lukas.eder@gmail.com>
2023-11-28 14:01:18 +0100

    [jOOQ/jOOQ#15883] Compilation error in generated code when using <embeddableDomains/> and the domain is part of a composite key

--- jOOQ-codegen/src/main/java/org/jooq/codegen/JavaGenerator.java
+++ jOOQ-codegen/src/main/java/org/jooq/codegen/JavaGenerator.java
@@ -40,6 +40,7 @@ package org.jooq.codegen;
 
 import static java.util.Arrays.asList;
 import static java.util.Collections.nCopies;
+import static java.util.Collections.singletonList;
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
@@ -1319,13 +1320,27 @@ public class JavaGenerator extends AbstractGenerator {
     }
 
     private void printCreateNonEmbeddableUniqueKey(JavaWriter out, UniqueKeyDefinition uniqueKey) {
+        List<Definition> keyColumns = new ArrayList<>(uniqueKey.getKeyColumns());
+
+
+
+
+
+
+
+
+
+
+
+
+
         if (scala)
             out.print("%s.createUniqueKey(%s, %s.name(\"%s\"), Array([[%s]]).asInstanceOf[Array[%s[%s, _] ] ], %s)",
                 Internal.class,
                 out.ref(getStrategy().getFullJavaIdentifier(uniqueKey.getTable()), 2),
                 DSL.class,
                 escapeString(uniqueKey.getOutputName()),
-                out.ref(getStrategy().getFullJavaIdentifiers(uniqueKey.getKeyColumns()), colRefSegments(null)),
+                out.ref(getStrategy().getFullJavaIdentifiers(keyColumns), colRefSegments(null)),
                 TableField.class,
                 out.ref(getStrategy().getJavaClassName(uniqueKey.getTable(), Mode.RECORD)),
                 uniqueKey.enforced());
@@ -1335,7 +1350,7 @@ public class JavaGenerator extends AbstractGenerator {
                 out.ref(getStrategy().getFullJavaIdentifier(uniqueKey.getTable()), 2),
                 DSL.class,
                 escapeString(uniqueKey.getOutputName()),
-                out.ref(getStrategy().getFullJavaIdentifiers(uniqueKey.getKeyColumns()), colRefSegments(null)),
+                out.ref(getStrategy().getFullJavaIdentifiers(keyColumns), colRefSegments(null)),
                 uniqueKey.enforced());
         else
             out.print("%s.createUniqueKey(%s, %s.name(\"%s\"), new %s[] { [[%s]] }, %s)",
@@ -1344,7 +1359,7 @@ public class JavaGenerator extends AbstractGenerator {
                 DSL.class,
                 escapeString(uniqueKey.getOutputName()),
                 TableField.class,
-                out.ref(getStrategy().getFullJavaIdentifiers(uniqueKey.getKeyColumns()), colRefSegments(null)),
+                out.ref(getStrategy().getFullJavaIdentifiers(keyColumns), colRefSegments(null)),
                 uniqueKey.enforced());
     }
 

322d07ebd02d932ac4eb2dcb998359bb0d4e51d7
Lukas Eder <lukas.eder@datageekery.com>
2024-02-14 10:33:53 +0100

    [jOOQ/jOOQ#16287] Support DuckDB NULL pseudo type as OTHER

--- jOOQ/src/main/java/org/jooq/impl/SQLDataTypes.java
+++ jOOQ/src/main/java/org/jooq/impl/SQLDataTypes.java
@@ -116,6 +116,7 @@ final class SQLDataTypes {
         public static final DataType<byte[]>       BLOB                     = new BuiltInDataType<>(FAMILY, SQLDataType.BLOB, "blob");
         public static final DataType<byte[]>       BINARYLARGEOBJECT        = new BuiltInDataType<>(FAMILY, SQLDataType.BLOB, "binary large object", "blob");
         public static final DataType<Object>       OTHER                    = new BuiltInDataType<>(FAMILY, SQLDataType.OTHER, "other");
+        public static final DataType<Object>       NULL                     = new BuiltInDataType<>(FAMILY, SQLDataType.OTHER, "null");
         public static final DataType<YearToSecond> INTERVAL                 = new BuiltInDataType<>(FAMILY, SQLDataType.INTERVAL, "interval");
         public static final DataType<YearToMonth>  INTERVALYEARTOMONTH      = new BuiltInDataType<>(FAMILY, SQLDataType.INTERVALYEARTOMONTH, "interval year to month");
         public static final DataType<DayToSecond>  INTERVALDAYTOSECOND      = new BuiltInDataType<>(FAMILY, SQLDataType.INTERVALDAYTOSECOND, "interval day to second", "interval day(9) to second");

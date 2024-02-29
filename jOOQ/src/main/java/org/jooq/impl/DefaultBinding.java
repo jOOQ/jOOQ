831ed76d4380a254b64ffeb461bb68080e121c7a
Lukas Eder <lukas.eder@gmail.com>
2024-02-29 13:07:22 +0100

    [jOOQ/jOOQ#16334] Cast UUID bind values also in HSQLDB

--- jOOQ/src/main/java/org/jooq/impl/DefaultBinding.java
+++ jOOQ/src/main/java/org/jooq/impl/DefaultBinding.java
@@ -818,6 +818,7 @@ public class DefaultBinding<T, U> implements Binding<T, U> {
                 switch (ctx.family()) {
 
 
+                    case HSQLDB:
                     case POSTGRES:
                     case YUGABYTEDB:
                         return true;

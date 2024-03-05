ff764592de6da370f3c3e0f438169fc002b75f9d
Lukas Eder <lukas.eder@gmail.com>
2024-03-05 10:14:09 +0100

    [jOOQ/jOOQ#15934] Code generator and MetaImpl return CREATE DEFAULT statement instead of just the default expression

--- jOOQ/src/main/java/org/jooq/impl/ParserImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/ParserImpl.java
@@ -8575,6 +8575,19 @@ final class DefaultParseContext extends AbstractScope implements ParseContext {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
         switch (characterUpper()) {
 
             // [#8821] Known prefixes so far:

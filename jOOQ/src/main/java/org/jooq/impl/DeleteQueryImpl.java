7b266c82163638e379e0d3beaeda7ae711e319b4
Lukas Eder <lukas.eder@gmail.com>
2024-04-22 09:40:55 +0200

    [jOOQ/jOOQ#16569] DELETE .. ORDER BY .. LIMIT on partitioned tables without unique constraint uses non-unique ctid for self joins

--- jOOQ/src/main/java/org/jooq/impl/DeleteQueryImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/DeleteQueryImpl.java
@@ -78,9 +78,11 @@ import static org.jooq.SQLDialect.YUGABYTEDB;
 // ...
 import static org.jooq.conf.SettingsTools.getExecuteDeleteWithoutWhere;
 import static org.jooq.impl.ConditionProviderImpl.extractCondition;
+import static org.jooq.impl.DSL.field;
 import static org.jooq.impl.DSL.noCondition;
 import static org.jooq.impl.DSL.row;
 import static org.jooq.impl.DSL.select;
+import static org.jooq.impl.DSL.systemName;
 import static org.jooq.impl.DSL.trueCondition;
 import static org.jooq.impl.InlineDerivedTable.hasInlineDerivedTables;
 import static org.jooq.impl.InlineDerivedTable.transformInlineDerivedTables;
@@ -281,6 +283,18 @@ implements
         ctx.scopeEnd();
     }
 
+    static final Field<?>[] keyFields(Context<?> ctx, Table<?> table) {
+
+        // [#16569] [#16571] The PostgreSQL ctid is not unique in a logically partitioned table
+        return table.getKeys().isEmpty()
+            ? ctx.family() == POSTGRES
+                ? new Field[] { field(systemName("tableoid")), table.rowid() }
+                : new Field[] { table.rowid() }
+            : (table.getPrimaryKey() != null
+                ? table.getPrimaryKey()
+                : table.getKeys().get(0)).getFieldsArray();
+    }
+
     @SuppressWarnings({ "unchecked", "rawtypes" })
     @Override
     final void accept1(Context<?> ctx) {
@@ -366,12 +380,7 @@ implements
         Condition where = DSL.and(getWhere(), moreWhere);
 
         if (limit != null && NO_SUPPORT_LIMIT.contains(ctx.dialect()) || !orderBy.isEmpty() && NO_SUPPORT_ORDER_BY_LIMIT.contains(ctx.dialect())) {
-            Field<?>[] keyFields =
-                  table().getKeys().isEmpty()
-                ? new Field[] { table().rowid() }
-                : (table().getPrimaryKey() != null
-                    ? table().getPrimaryKey()
-                    : table().getKeys().get(0)).getFieldsArray();
+            Field<?>[] keyFields = keyFields(ctx, table());
 
             ctx.start(DELETE_WHERE)
                .formatSeparator()

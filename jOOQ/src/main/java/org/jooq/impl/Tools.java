1ff66d6cf9c9445950635e65251f8cf2721da00b
Lukas Eder <lukas.eder@gmail.com>
2024-06-25 08:50:33 +0200

    [jOOQ/jOOQ#16853] MySQL error "BLOB, TEXT, GEOMETRY or JSON column can't have a default value" when running DDL from generated code

--- jOOQ/src/main/java/org/jooq/impl/Tools.java
+++ jOOQ/src/main/java/org/jooq/impl/Tools.java
@@ -1186,6 +1186,7 @@ final class Tools {
     static final Set<SQLDialect>         NO_SUPPORT_TIMESTAMP_PRECISION     = SQLDialect.supportedBy(DERBY);
     static final Set<SQLDialect>         DEFAULT_TIMESTAMP_NOT_NULL         = SQLDialect.supportedBy(MARIADB);
     static final Set<SQLDialect>         REQUIRES_PARENTHESISED_DEFAULT     = SQLDialect.supportedBy(SQLITE);
+    static final Set<SQLDialect>         REQUIRES_PARENTHESISED_DEFAULT_FOR_LOBS = SQLDialect.supportedBy(MYSQL);
 
 
 
@@ -6047,6 +6048,11 @@ final class Tools {
             //          expressions, not if actual parentheses are rendered.
             if (REQUIRES_PARENTHESISED_DEFAULT.contains(ctx.dialect()))
                 ctx.sql('(').visit(v).sql(')');
+
+            // [#16853] MySQL LOB types can't have defaults. Except if we parenthesise them, then they can o_O
+            else if (REQUIRES_PARENTHESISED_DEFAULT_FOR_LOBS.contains(ctx.dialect()) && (type.isLob() || type.isJSON() || type.isSpatial()))
+                ctx.sql('(').visit(v).sql(')');
+
             else
                 ctx.visit(v);
         }

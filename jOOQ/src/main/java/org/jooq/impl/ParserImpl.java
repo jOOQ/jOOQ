46f84c2c704a71eadaddaef8d46b32522d8bb764
Lukas Eder <lukas.eder@datageekery.com>
2024-08-05 16:39:54 +0200

    [jOOQ/jOOQ#16979] Exception when doing Meta.ddl() export with tables that have USE or GO or similar field or table aliases

--- jOOQ/src/main/java/org/jooq/impl/ParserImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/ParserImpl.java
@@ -6723,7 +6723,10 @@ final class DefaultParseContext extends AbstractScope implements ParseContext {
 
     @Override
     public final Table<?> parseTable() {
-        return parseTable(() -> peekKeyword(KEYWORDS_IN_SELECT_FROM));
+        return parseTable(() -> peekKeyword(delimiterRequired
+            ? KEYWORDS_IN_FROM
+            : KEYWORDS_IN_SELECT_FROM
+        ));
     }
 
     private final Table<?> parseTable(BooleanSupplier forbiddenKeywords) {
@@ -7414,7 +7417,7 @@ final class DefaultParseContext extends AbstractScope implements ParseContext {
 
                     if (parseKeywordIf("AS"))
                         alias = parseIdentifier(true, false);
-                    else if (!peekKeyword(KEYWORDS_IN_SELECT) && !peekKeyword(KEYWORDS_IN_STATEMENTS))
+                    else if (!peekKeyword(KEYWORDS_IN_SELECT) && (delimiterRequired || !peekKeyword(KEYWORDS_IN_STATEMENTS)))
                         alias = parseIdentifierIf(true, false);
                 }
 
@@ -14087,6 +14090,7 @@ final class DefaultParseContext extends AbstractScope implements ParseContext {
     private int                         bindIndex       = 0;
     private final Map<String, Param<?>> bindParams      = new LinkedHashMap<>();
     private String                      delimiter       = ";";
+    private boolean                     delimiterRequired = false;
     private LanguageContext             languageContext = LanguageContext.QUERY;
     private EnumSet<FunctionKeyword>    forbidden       = EnumSet.noneOf(FunctionKeyword.class);
     private ParseScope                  scope           = new ParseScope();
@@ -14123,6 +14127,7 @@ final class DefaultParseContext extends AbstractScope implements ParseContext {
         // [#8722] This is an undocumented flag that allows for collecting parameters from the parser
         //         Do not rely on this flag. It will change incompatibly in the future.
         this.bindParamListener = (Consumer<Param<?>>) dsl.configuration().data("org.jooq.parser.param-collector");
+        this.delimiterRequired = TRUE.equals(dsl.configuration().data("org.jooq.parser.delimiter-required"));
 
 
 

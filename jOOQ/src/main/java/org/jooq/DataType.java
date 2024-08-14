567e180d919cc0b0c5d1fa2e01fb380c8a504aee
Lukas Eder <lukas.eder@datageekery.com>
2024-08-14 11:41:14 +0200

    [jOOQ/jOOQ#17063] Update DataType.isXYZ() Javadoc with new types

--- jOOQ/src/main/java/org/jooq/DataType.java
+++ jOOQ/src/main/java/org/jooq/DataType.java
@@ -1318,6 +1318,7 @@ public interface DataType<T> extends Named {
      * <li> {@link SQLDataType#OFFSETTIME}</li>
      * <li> {@link SQLDataType#OFFSETDATETIME}</li>
      * <li> {@link SQLDataType#INSTANT}</li>
+     * <li> {@link SQLDataType#YEAR}</li>
      * </ul>
      *
      * @see #isDate()
@@ -1370,6 +1371,8 @@ public interface DataType<T> extends Named {
      * <li> {@link SQLDataType#LOCALDATETIME}</li>
      * <li> {@link SQLDataType#OFFSETTIME}</li>
      * <li> {@link SQLDataType#OFFSETDATETIME}</li>
+     * <li> {@link SQLDataType#INSTANT}</li>
+     * <li> {@link SQLDataType#YEAR}</li>
      * <li> {@link YearToSecond}</li>
      * <li> {@link YearToMonth}</li>
      * <li> {@link DayToSecond}</li>

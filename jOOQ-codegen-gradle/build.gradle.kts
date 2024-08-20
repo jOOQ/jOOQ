97f2b8be00d37c09dabec3fd2c79abb317f30631
Lukas Eder <lukas.eder@gmail.com>
2024-08-20 15:32:41 +0200

    [jOOQ/jOOQ#15936] Implicit path correlation produces correlation predicate in ON clause rather than WHERE clause in some cases

--- jOOQ-codegen-gradle/build.gradle.kts
+++ jOOQ-codegen-gradle/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {

ad3cf68109560b4f7c6f14eae3e971b2497d8e66
Lukas Eder <lukas.eder@datageekery.com>
2024-09-12 10:42:27 +0200

    [jOOQ/jOOQ#17236] Improve error message in GeneratorWriter when brackets mismatch, including the offending file

--- jOOQ-codegen/src/main/java/org/jooq/codegen/GeneratorWriter.java
+++ jOOQ-codegen/src/main/java/org/jooq/codegen/GeneratorWriter.java
@@ -177,7 +177,15 @@ public abstract class GeneratorWriter<W extends GeneratorWriter<W>> {
             blockComment = false;
 
         if (indentTabsAllLines < 0 && !Boolean.getBoolean("mute-indentation-error"))
-            new IllegalStateException("A formatting error has been produced by https://github.com/jOOQ/jOOQ/issues/10196").printStackTrace(System.err);
+            new IllegalStateException("""
+                A formatting error has been produced
+
+                This is most likely due to a mismatch of opening and closing brackets, e.g. [], {}, ().
+                The problematic file is {file}
+
+                See also https://github.com/jOOQ/jOOQ/issues/10196
+                """.replace("{file}", "" + file)
+            ).printStackTrace(System.err);
 
         int indentTabsThisLine0 = indentTabsThisLine;
         StringBuilder indent = new StringBuilder();
@@ -258,7 +266,13 @@ public abstract class GeneratorWriter<W extends GeneratorWriter<W>> {
                 translated = new ArrayList<>();
             }
 
-            appendWrapped(String.format(string, translated.toArray()), indent.toString());
+            try {
+                appendWrapped(String.format(string, translated.toArray()), indent.toString());
+                appendWrapped(String.format(string, translated.toArray()), indent.toString());
+            }
+            catch (Exception e) {
+                throw new RuntimeException("Error when formatting " + string + " with args " + Arrays.asList(args) + " in file " + file, e);
+            }
         }
         else
             appendWrapped(string, indent.toString());
@@ -481,7 +495,16 @@ public abstract class GeneratorWriter<W extends GeneratorWriter<W>> {
 
     protected String beforeClose(String string) {
         if (indentTabsAllLines > 0 && !Boolean.getBoolean("mute-indentation-error"))
-            new IllegalStateException("A formatting error has been produced by https://github.com/jOOQ/jOOQ/issues/10196").printStackTrace(System.err);
+            new IllegalStateException(
+                """
+                A formatting error has been produced
+
+                This is most likely due to a mismatch of opening and closing brackets, e.g. [], {}, ().
+                The problematic file is {file}
+
+                See also https://github.com/jOOQ/jOOQ/issues/10196
+                """.replace("{file}", "" + file)
+            ).printStackTrace(System.err);
 
         return string;
     }

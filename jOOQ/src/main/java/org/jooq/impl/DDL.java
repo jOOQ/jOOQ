84847393ad8c4848a6459b0402fd447556d7ecdd
Lukas Eder <lukas.eder@datageekery.com>
2024-09-25 10:30:39 +0200

    [jOOQ/jOOQ#17359] DDL::applyAsPlainSQL should trim plain SQL string to check if it starts with CREATE

--- jOOQ/src/main/java/org/jooq/impl/DDL.java
+++ jOOQ/src/main/java/org/jooq/impl/DDL.java
@@ -209,7 +209,7 @@ final class DDL {
         return asList(s0);
     }
 
-    static final Pattern P_CREATE_VIEW = Pattern.compile("^(?i:\\bcreate\\b.*?\\bview\\b.*?\\bas\\b\\s+)(.*)$");
+    static final Pattern P_CREATE_VIEW = Pattern.compile("^(?i:\\s*\\bcreate\\b.*?\\bview\\b.*?\\bas\\b\\s+)(.*)$");
 
     @SuppressWarnings({ "rawtypes", "unchecked" })
     private final Query applyAs(CreateViewAsStep q, TableOptions options) {
@@ -251,7 +251,7 @@ final class DDL {
 
         // [#15238] If the command prefix is supplied, use a heursitic where the view
         //          body is expected to start after the first "AS" keyword
-        if (options.source().toLowerCase().startsWith("create"))
+        if (options.source().trim().toLowerCase().startsWith("create"))
             return q.as(P_CREATE_VIEW.matcher(options.source()).replaceFirst("$1"));
         else
             return q.as(options.source());

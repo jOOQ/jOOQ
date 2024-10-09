115286a1d94234686241ec38d49fdbe0dfda4843
Lukas Eder <lukas.eder@gmail.com>
2024-10-09 10:31:42 +0200

    [jOOQ/jOOQ#17418] Render an E0 adjustment for numeric values of scale 0

--- jOOQ/src/main/java/org/jooq/impl/DefaultBinding.java
+++ jOOQ/src/main/java/org/jooq/impl/DefaultBinding.java
@@ -1809,6 +1809,9 @@ public class DefaultBinding<T, U> implements Binding<T, U> {
 
 
 
+
+
+
                 default:
                     ctx.render().sql(value.toString());
                     break;

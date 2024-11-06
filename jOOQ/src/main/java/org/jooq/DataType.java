303e886441b031b35d34e9cc9c371b6b640985b3
Lukas Eder <lukas.eder@datageekery.com>
2024-11-06 11:42:30 +0100

    [jOOQ/jOOQ#17549] Add DataType.isRowId() and use it instead of comparing Field.getType() == RowId.class internally

--- jOOQ/src/main/java/org/jooq/DataType.java
+++ jOOQ/src/main/java/org/jooq/DataType.java
@@ -1609,6 +1609,11 @@ public interface DataType<T> extends Named {
     boolean isUUID();
 
     /**
+     * Whether this data type is a {@link RowId} type.
+     */
+    boolean isRowId();
+
+    /**
      * Whether this data type is an OTHER type.
      * <p>
      * The {@link SQLDataType#OTHER} type maps any unknown data types to a jOOQ

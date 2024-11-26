b26b21b000fb05a30195d4cbec9ee93a113eda37
Lukas Eder <lukas.eder@gmail.com>
2024-11-26 09:03:12 +0100

    [jOOQ/jOOQ#17666] Inaccurate Javadoc on ExecuteContext setters

--- jOOQ/src/main/java/org/jooq/ExecuteContext.java
+++ jOOQ/src/main/java/org/jooq/ExecuteContext.java
@@ -51,6 +51,7 @@ import org.jooq.exception.DataAccessException;
 import org.jooq.impl.CustomQueryPart;
 import org.jooq.impl.DSL;
 
+import org.jetbrains.annotations.ApiStatus.Internal;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -371,8 +372,11 @@ public interface ExecuteContext extends Scope {
     Record record();
 
     /**
-     * Calling this has no effect. It is used by jOOQ internally.
+     * Set the last record that was fetched from the result set.
+     * <p>
+     * Users shouldn't call this method, it is used by jOOQ internally.
      */
+    @Internal
     void record(Record record);
 
     /**
@@ -390,8 +394,11 @@ public interface ExecuteContext extends Scope {
     int rows();
 
     /**
-     * Calling this has no effect. It is used by jOOQ internally.
+     * Set the number of rows that were affected by the last statement.
+     * <p>
+     * Users shouldn't call this method, it is used by jOOQ internally.
      */
+    @Internal
     void rows(int rows);
 
     /**
@@ -430,8 +437,11 @@ public interface ExecuteContext extends Scope {
     Result<?> result();
 
     /**
-     * Calling this has no effect. It is used by jOOQ internally.
+     * Set the last result that was fetched from the result set.
+     * <p>
+     * Users shouldn't call this method, it is used by jOOQ internally.
      */
+    @Internal
     void result(Result<?> result);
 
     /**

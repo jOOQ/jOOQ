833f44bb8f364fc5c56852406d3a7948906c5d83
Lukas Eder <lukas.eder@gmail.com>
2024-12-23 10:12:05 +0100

    [jOOQ/jOOQ#17787] Regression when enabling <jpaAnnotation> for NOT NULL DEFAULT columns, which should continue to be marked as @Column(nullable = true)

--- jOOQ-codegen/src/main/java/org/jooq/codegen/JavaGenerator.java
+++ jOOQ-codegen/src/main/java/org/jooq/codegen/JavaGenerator.java
@@ -9218,7 +9218,7 @@ public class JavaGenerator extends AbstractGenerator {
             }
 
             String nullable = "";
-            if (effectivelyNotNull(out, column))
+            if (!column.getType(resolver(out)).isNullable())
                 nullable = ", nullable = false";
 
             String length = "";

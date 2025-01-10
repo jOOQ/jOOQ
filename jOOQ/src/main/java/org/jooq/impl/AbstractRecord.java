d69db14a0f1a6d5b3ae2a73c368e897ae28b6b10
Lukas Eder <lukas.eder@gmail.com>
2025-01-10 10:35:18 +0100

    I deleted my account on this terrible hate platform

--- jOOQ/src/main/java/org/jooq/impl/AbstractRecord.java
+++ jOOQ/src/main/java/org/jooq/impl/AbstractRecord.java
@@ -809,8 +809,7 @@ implements
     }
 
     // [#10191] Java and Kotlin can produce overloads for this method despite
-    // generic type erasure, but Scala cannot, see
-    // https://twitter.com/lukaseder/status/1262652304773259264
+    // generic type erasure, but Scala cannot
     @Override
     public /* final */ <E> E into(E object) {
         if (object == null)

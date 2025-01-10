e1cf9739308868cfd30f0ff4a819f556a489bc43
Lukas Eder <lukas.eder@gmail.com>
2025-01-10 10:19:10 +0100

    [jOOQ/jOOQ#17855] ORA-12704: character set mismatch placing NULL and non-NULL NVARCHAR values in UNION ALL subqueries

--- jOOQ/src/main/java/org/jooq/impl/DefaultBinding.java
+++ jOOQ/src/main/java/org/jooq/impl/DefaultBinding.java
@@ -4557,6 +4557,12 @@ public class DefaultBinding<T, U> implements Binding<T, U> {
     }
 
     static final class DefaultNStringBinding<U> extends InternalBinding<String, U> {
+
+
+
+
+
+
         private final DefaultStringBinding<U> fallback;
 
         DefaultNStringBinding(DataType<String> dataType, Converter<String, U> converter) {

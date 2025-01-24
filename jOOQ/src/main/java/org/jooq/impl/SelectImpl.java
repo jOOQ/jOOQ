a245c90c57834d9a1216c38740de49d9dbf06d11
Lukas Eder <lukas.eder@gmail.com>
2025-01-24 09:29:03 +0100

    [jOOQ/jOOQ#17922] Add new AbstractDelegatingResultQuery internal type to avoid excessive default method bytecode generation at JVM startup time

--- jOOQ/src/main/java/org/jooq/impl/SelectImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/SelectImpl.java
@@ -143,9 +143,8 @@ import org.jetbrains.annotations.Nullable;
  */
 @SuppressWarnings({ "unchecked", "rawtypes" })
 final class SelectImpl<R extends Record, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22>
-extends AbstractDelegatingQuery<R, SelectQueryImpl<R>>
+extends AbstractDelegatingResultQuery<R, SelectQueryImpl<R>>
 implements
-    ResultQueryTrait<R>,
 
     // Cascading interface implementations for Select behaviour
     SelectSelectStep<R>,

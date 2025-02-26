cf98510a6af9f7bf6cb552c21e7af3604e1ec0af
Lukas Eder <lukas.eder@datageekery.com>
2025-02-26 10:54:23 +0100

    [jOOQ/jOOQ#18047] Include fields and values lengths in "The number of values must match the number of fields" error message

--- jOOQ/src/main/java/org/jooq/impl/InsertImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/InsertImpl.java
@@ -815,7 +815,7 @@ final class InsertImpl<R extends Record, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10
 
         // [#4629] Plain SQL INSERT INTO t VALUES (a, b, c) statements don't know the insert columns
         else if (!Tools.isEmpty(fields) && fields.length != values.length)
-            throw new IllegalArgumentException("The number of values must match the number of fields");
+            throw new IllegalArgumentException("The number of values (" + values.length + ") must match the number of fields (" + fields.length + ")");
 
         getDelegate().newRecord();
         if (Tools.isEmpty(fields))
@@ -969,7 +969,7 @@ final class InsertImpl<R extends Record, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10
 
         // [#4629] Plain SQL INSERT INTO t VALUES (a, b, c) statements don't know the insert columns
         else if (!Tools.isEmpty(fields) && fields.length != values.length)
-            throw new IllegalArgumentException("The number of values must match the number of fields");
+            throw new IllegalArgumentException("The number of values (" + values.length + ") must match the number of fields (" + fields.length + ")");
 
         getDelegate().newRecord();
 

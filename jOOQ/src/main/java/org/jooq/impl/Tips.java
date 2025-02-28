f01812d8faefe51d7ed77733df36e52414af5c17
Lukas Eder <lukas.eder@gmail.com>
2025-02-28 14:52:25 +0100

    [jOOQ/jOOQ#15742] Add more tips

--- jOOQ/src/main/java/org/jooq/impl/Tips.java
+++ jOOQ/src/main/java/org/jooq/impl/Tips.java
@@ -110,6 +110,8 @@ final class Tips {
         "When it comes to using Converter or Binding, only the first UNION subquery is relevant. Learn more about this here: https://blog.jooq.org/how-to-use-jooqs-converters-with-union-operations/",
         "Want to upgrade your jOOQ experience? Check out these commercial only features of the jOOQ Professional and Enterprise Editions: https://www.jooq.org/commercial-only-features/",
         "Should you use jOOQ's DAOs? They're convenient, but don't overuse them. SQL is often more powerful: https://blog.jooq.org/to-dao-or-not-to-dao/",
+        "Different jOOQ editions support multiple JDK versions. Check out the complete list here: https://www.jooq.org/download/support-matrix-jdk",
+        "Different jOOQ editions support multiple RDBMS versions. Check out the complete list here: https://www.jooq.org/download/support-matrix",
         ""
     };
 

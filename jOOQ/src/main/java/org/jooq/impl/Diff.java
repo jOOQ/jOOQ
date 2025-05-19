44dd99e3cd7772374db57d709d6319d0436cb27e
Lukas Eder <lukas.eder@datageekery.com>
2025-05-19 17:33:05 +0200

    [jOOQ/jOOQ#18481] Diff generates CREATE VIEW statements that depend on not-yet existing tables

--- jOOQ/src/main/java/org/jooq/impl/Diff.java
+++ jOOQ/src/main/java/org/jooq/impl/Diff.java
@@ -935,6 +935,7 @@ final class Diff {
     }
 
     static final int sortIndex(Query q) {
+        final int VIEW = 5;
         final int FKEY = 4;
         final int CONS = 3;
         final int NULL = 2;
@@ -973,6 +974,10 @@ final class Diff {
             return -CONS;
         else if (q instanceof QOM.CreateIndex)
             return CONS;
+        else if (q instanceof QOM.DropView)
+            return -VIEW;
+        else if (q instanceof QOM.CreateView)
+            return VIEW;
         else
             return 0;
     }

b0d45e75e24e48c1bd2cb4c38e614f18621353f3
Lukas Eder <lukas.eder@datageekery.com>
2025-06-25 13:05:48 +0200

    [jOOQ/jOOQ#18666] Parser cannot parse Oracle style IN, OUT modifier in procedure declarations

--- jOOQ/src/main/java/org/jooq/impl/ParserImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/ParserImpl.java
@@ -6154,6 +6154,8 @@ final class DefaultParseContext extends AbstractParseContext implements ParseCon
 
 
 
+
+
     private final DDLQuery parseCreateDomain() {
         boolean ifNotExists = parseKeywordIf("IF NOT EXISTS");
         Domain<?> domainName = parseDomainName();

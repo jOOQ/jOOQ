f57b75d00bed9c21fd0eec2c36938c815c6b31e2
Lukas Eder <lukas.eder@datageekery.com>
2025-07-04 08:28:00 +0200

    [jOOQ/jOOQ#18722] Cannot place Oracle UDT in MULTISET with JSON emulation

--- jOOQ/src/main/java/org/jooq/impl/JSONEntryImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/JSONEntryImpl.java
@@ -63,6 +63,7 @@ import static org.jooq.impl.Names.N_JSON_ARRAY;
 import static org.jooq.impl.Names.N_JSON_EXTRACT;
 import static org.jooq.impl.Names.N_JSON_MERGE;
 import static org.jooq.impl.Names.N_JSON_MERGE_PRESERVE;
+import static org.jooq.impl.Names.N_JSON_OBJECT;
 import static org.jooq.impl.Names.N_JSON_QUERY;
 import static org.jooq.impl.Names.N_PARSE_JSON;
 import static org.jooq.impl.Names.N_RAWTOHEX;
@@ -242,6 +243,11 @@ final class JSONEntryImpl<T> extends AbstractQueryPart implements JSONEntry<T>, 
                     return function(N_JSON, SQLDataType.JSON, field);
 
                 break;
+
+
+
+
+
 
 
 

cae54c717c493fb8fdf6ade64d981f9b9d27ff77
Lukas Eder <lukas.eder@datageekery.com>
2025-07-16 17:43:34 +0200

    [jOOQ/jOOQ#18787] Fix backport

--- jOOQ-meta/src/main/java/org/jooq/meta/xml/XMLDatabase.java
+++ jOOQ-meta/src/main/java/org/jooq/meta/xml/XMLDatabase.java
@@ -58,6 +58,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;

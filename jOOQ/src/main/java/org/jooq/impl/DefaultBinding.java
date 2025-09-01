a7040f3a4948b0cf1de622e4d20791d6c5cf7db7
Lukas Eder <lukas.eder@datageekery.com>
2025-09-01 12:45:53 +0200

    [jOOQ/jOOQ#18974] Bad rendering of unsupported OffsetTime type in Oracle

--- jOOQ/src/main/java/org/jooq/impl/DefaultBinding.java
+++ jOOQ/src/main/java/org/jooq/impl/DefaultBinding.java
@@ -3809,6 +3809,7 @@ public class DefaultBinding<T, U> implements Binding<T, U> {
 
 
 
+
                 // Some dialects implement SQL standard time literals
                 default:
                     ctx.render().visit(K_TIME_WITH_TIME_ZONE).sql(" '").sql(escape(format(value), ctx.render())).sql('\'');

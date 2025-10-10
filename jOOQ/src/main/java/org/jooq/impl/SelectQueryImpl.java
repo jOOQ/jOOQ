ccd041b0116ba9295e7be3a24f6d32b23b76e43d
Lukas Eder <lukas.eder@datageekery.com>
2025-10-10 11:04:51 +0200

    [jOOQ/jOOQ#19188] Select::$replace doesn't maintain SELECT .. INTO clause

--- jOOQ/src/main/java/org/jooq/impl/SelectQueryImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/SelectQueryImpl.java
@@ -576,6 +576,10 @@ final class SelectQueryImpl<R extends Record> extends AbstractResultQuery<R> imp
 
     private final SelectQueryImpl<R> copyBetween(CopyClause start, CopyClause end, boolean scalarSelect, SelectQueryImpl<R> result) {
         if (CopyClause.START.between(start, end)) {
+            result.intoTable = intoTable;
+            if (intoVariables != null)
+                result.intoVariables = new QueryPartList<>(intoVariables);
+
             result.from.addAll(from);
             result.condition.setWhere(condition.getWhere());
 
@@ -5288,6 +5292,10 @@ final class SelectQueryImpl<R extends Record> extends AbstractResultQuery<R> imp
         else
             return copy(s -> s.getLimit().setOffset(newOffset));
     }
+
+
+
+
 
 
 

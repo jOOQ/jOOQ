a3748cee17e315a767a3f5cba024f2351b4708e1
Lukas Eder <lukas.eder@datageekery.com>
2025-10-31 09:34:24 +0100

    [jOOQ/jOOQ#19304] BigQuery nested ROW emulation produces invalid column names

--- jOOQ/src/main/java/org/jooq/impl/Tools.java
+++ jOOQ/src/main/java/org/jooq/impl/Tools.java
@@ -1886,8 +1886,17 @@ final class Tools {
         return map(names, n -> n instanceof Name name ? name : DSL.name(String.valueOf(n)));
     }
 
+
+
+
+
+
     static final String sanitiseName(Configuration configuration, String name) {
         switch (configuration.family()) {
+
+
+
+
 
 
 
@@ -7325,6 +7334,7 @@ final class Tools {
                 case POSTGRES:
                 case YUGABYTEDB:
                     return NestedCollectionEmulation.JSONB;
+
 
 
                 case H2:

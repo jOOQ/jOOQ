4e46e252ca431048a1f0be26eef915ab6863d87c
Lukas Eder <lukas.eder@datageekery.com>
2025-10-31 14:23:31 +0100

    [jOOQ/jOOQ#19312] BigQuery ALTER TABLE .. DROP FOREIGN KEY renders wrong SQL

--- jOOQ/src/main/java/org/jooq/impl/AlterTableImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/AlterTableImpl.java
@@ -1713,7 +1713,7 @@ implements
                      .sql(' ')
                      .visit(dropConstraint);
                 }
-                else if (dropConstraintType == PRIMARY_KEY && NO_SUPPORT_DROP_CONSTRAINT.contains(c.dialect()) || AbstractConstraint.NO_SUPPORT_NAMED_PK.contains(c.dialect())) {
+                else if (dropConstraintType == PRIMARY_KEY && (NO_SUPPORT_DROP_CONSTRAINT.contains(c.dialect()) || AbstractConstraint.NO_SUPPORT_NAMED_PK.contains(c.dialect()))) {
                     c.visit(K_DROP).sql(' ').visit(K_PRIMARY_KEY);
                 }
                 else {

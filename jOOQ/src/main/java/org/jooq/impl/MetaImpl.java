a4c27dff11155addae4e0c531495790d78758263
Lukas Eder <lukas.eder@datageekery.com>
2025-11-19 10:41:06 +0100

    [jOOQ/jOOQ#19394] Error when generating code for SQLite table containing both UNIQUE and FOREIGN KEY, when referenced table has no UNIQUE key

--- jOOQ/src/main/java/org/jooq/impl/MetaImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/MetaImpl.java
@@ -139,6 +139,7 @@ import org.jooq.TableOptions.TableType;
 // ...
 // ...
 import org.jooq.UniqueKey;
+import org.jooq.conf.InterpreterWithMetaLookups;
 import org.jooq.conf.ParseUnknownFunctions;
 import org.jooq.conf.Settings;
 import org.jooq.exception.DataAccessException;
@@ -777,7 +778,9 @@ final class MetaImpl extends AbstractMeta {
             try {
                 m = dsl()
                     .configuration()
-                    .deriveSettings(s -> s.withInterpreterDelayForeignKeyDeclarations(true))
+                    .deriveSettings(s -> s
+                        .withInterpreterDelayForeignKeyDeclarations(true)
+                        .withInterpreterWithMetaLookups(InterpreterWithMetaLookups.IGNORE_ON_FAILURE))
                     .dsl()
                     .meta(sb.toString());
             }

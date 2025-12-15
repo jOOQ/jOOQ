d8af259044f27f0eec63e2f1350fb6599156fa47
Lukas Eder <lukas.eder@datageekery.com>
2025-12-15 13:01:59 +0100

    [jOOQ/jOOQ#19512] DuckDBDatabase produces both identity flags and DEFAULT expression

--- jOOQ-meta/src/main/java/org/jooq/meta/DefaultDataTypeDefinition.java
+++ jOOQ-meta/src/main/java/org/jooq/meta/DefaultDataTypeDefinition.java
@@ -162,8 +162,10 @@ public class DefaultDataTypeDefinition implements DataTypeDefinition {
         this.nullable = nullable == null ? true : nullable.booleanValue();
         this.hidden = hidden;
         this.readonly = readonly;
-        this.generatedAlwaysAs = generatedAlwaysAs;
-        this.defaultValue = defaultValue;
+
+        // [#19512] Identity and generators / defaults are mutually exclusive, even if the identity is implemented using a default.
+        this.generatedAlwaysAs = identity ? null : generatedAlwaysAs;
+        this.defaultValue = identity ? null : defaultValue;
         this.identity = identity;
 
         if (generator != null)

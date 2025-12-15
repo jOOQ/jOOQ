bd3ce78699c4a634147009a5833bd9c88d67c84b
Lukas Eder <lukas.eder@datageekery.com>
2025-12-15 13:47:58 +0100

    [jOOQ/jOOQ#19515] Bad decoding of spatial data from DuckDB database

--- jOOQ/src/main/java/org/jooq/impl/DefaultBinding.java
+++ jOOQ/src/main/java/org/jooq/impl/DefaultBinding.java
@@ -5995,6 +5995,10 @@ public class DefaultBinding<T, U> implements Binding<T, U> {
 
 
 
+
+
+
+
     static final class DefaultJSONBinding<U> extends InternalBinding<org.jooq.JSON, U> {
 
         DefaultJSONBinding(DataType<JSON> dataType, Converter<JSON, U> converter) {

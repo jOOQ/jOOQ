5ec8999214eb41b8173b38c7e0f4c46254629fed
Lukas Eder <lukas.eder@datageekery.com>
2025-12-16 10:20:52 +0100

    [jOOQ/jOOQ#19531] [jOOQ/jOOQ#18825] Add dialect version support for DuckDB

--- jOOQ/src/main/java/org/jooq/MergeMatchedDeleteStep.java
+++ jOOQ/src/main/java/org/jooq/MergeMatchedDeleteStep.java
@@ -42,6 +42,7 @@ package org.jooq;
 import static org.jooq.SQLDialect.DERBY;
 import static org.jooq.SQLDialect.DUCKDB;
 // ...
+// ...
 import static org.jooq.SQLDialect.FIREBIRD;
 import static org.jooq.SQLDialect.H2;
 import static org.jooq.SQLDialect.HSQLDB;

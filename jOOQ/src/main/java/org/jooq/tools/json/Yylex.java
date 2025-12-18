1d6b2517665f6e93bb7302c3e50413878325bb17
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-12-18 16:39:35 -0500

    spelling: occurred
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- jOOQ/src/main/java/org/jooq/tools/json/Yylex.java
+++ jOOQ/src/main/java/org/jooq/tools/json/Yylex.java
@@ -449,7 +449,7 @@ class Yylex {
 
 
     /**
-     * Reports an error that occured while scanning.
+     * Reports an error that occurred while scanning.
      *
      * In a wellformed scanner (no or only correct usage of
      * yypushback(int) and a match-all fallback rule) this method

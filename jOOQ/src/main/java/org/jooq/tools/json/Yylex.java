c2802800e2a4671a0cd33a75201d49ff8328a1e9
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-12-18 23:35:05 -0500

    spelling: unknown
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- jOOQ/src/main/java/org/jooq/tools/json/Yylex.java
+++ jOOQ/src/main/java/org/jooq/tools/json/Yylex.java
@@ -183,7 +183,7 @@ class Yylex {
 
     /* error messages for the codes above */
     private static final String ZZ_ERROR_MSG[] = {
-            "Unkown internal scanner error",
+            "Unknown internal scanner error",
             "Error: could not match input",
             "Error: pushback value was too large"
     };

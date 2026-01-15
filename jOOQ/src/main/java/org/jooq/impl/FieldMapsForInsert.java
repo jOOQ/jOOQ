980ca0647a2b2cc54be9b6d7335693514c428d2e
Lukas Eder <lukas.eder@datageekery.com>
2026-01-15 16:53:33 +0100

    [jOOQ/jOOQ#16942] Support emulating EXCLUDED in MySQL, when used in expressions

--- jOOQ/src/main/java/org/jooq/impl/FieldMapsForInsert.java
+++ jOOQ/src/main/java/org/jooq/impl/FieldMapsForInsert.java
@@ -57,6 +57,7 @@ import static org.jooq.impl.FieldMapsForInsert.toSQLInsertSelect;
 import static org.jooq.impl.DSL.selectFrom;
 import static org.jooq.impl.Keywords.K_DEFAULT_VALUES;
 import static org.jooq.impl.Keywords.K_VALUES;
+import static org.jooq.impl.Names.N_EXCLUDED;
 import static org.jooq.impl.Names.N_T;
 import static org.jooq.impl.QueryPartCollectionView.wrap;
 import static org.jooq.impl.Tools.EMPTY_FIELD;
@@ -92,6 +93,7 @@ import org.jooq.DataType;
 import org.jooq.Field;
 import org.jooq.FieldOrRow;
 import org.jooq.GeneratorStatementType;
+import org.jooq.Name;
 import org.jooq.Param;
 // ...
 import org.jooq.Record;
@@ -231,6 +233,15 @@ final class FieldMapsForInsert extends AbstractQueryPart implements UNotYetImple
         }
     }
 
+    private final Name alias(Context<?> ctx) {
+        if (ctx.topLevel() instanceof QOM.Insert<?> i) {
+            if (i.$onDuplicateKeyUpdate())
+                return N_EXCLUDED;
+        }
+
+        return N_T;
+    }
+
     private final void toSQLValues(Context<?> ctx) {
         ctx.formatSeparator()
            .start(INSERT_VALUES)
@@ -263,6 +274,18 @@ final class FieldMapsForInsert extends AbstractQueryPart implements UNotYetImple
                 return select;
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

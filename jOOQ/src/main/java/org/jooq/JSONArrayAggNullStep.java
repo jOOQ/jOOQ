d4b454c369c094da474481a1de7a3b9a61c27942
Lukas Eder <lukas.eder@datageekery.com>
2026-02-11 12:51:40 +0100

    [jOOQ/jOOQ#19645] Add missing MariaDB, MySQL support on JSON_ARRAYAGG clause ABSENT ON NULL

--- jOOQ/src/main/java/org/jooq/JSONArrayAggNullStep.java
+++ jOOQ/src/main/java/org/jooq/JSONArrayAggNullStep.java
@@ -42,7 +42,10 @@ import org.jetbrains.annotations.*;
 
 // ...
 // ...
+// ...
 import static org.jooq.SQLDialect.H2;
+import static org.jooq.SQLDialect.MARIADB;
+import static org.jooq.SQLDialect.MYSQL;
 // ...
 import static org.jooq.SQLDialect.POSTGRES;
 // ...
@@ -71,6 +74,6 @@ public interface JSONArrayAggNullStep<T> extends JSONArrayAggReturningStep<T> {
      * Exclude <code>NULL</code> values in output JSON.
      */
     @NotNull
-    @Support({ H2, POSTGRES, SQLITE, YUGABYTEDB })
+    @Support({ H2, MARIADB, MYSQL, POSTGRES, SQLITE, YUGABYTEDB })
     JSONArrayAggReturningStep<T> absentOnNull();
 }

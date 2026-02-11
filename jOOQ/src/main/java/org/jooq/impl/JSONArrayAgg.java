437a316edf04796768760e13928d886a62de6936
Lukas Eder <lukas.eder@datageekery.com>
2026-02-11 13:18:33 +0100

    [jOOQ/jOOQ#19643] JSON_ARRAYAGG emulation using GROUP_CONCAT doesn't properly quote string-style data types, like UUID, DATE, etc.

--- jOOQ/src/main/java/org/jooq/impl/JSONArrayAgg.java
+++ jOOQ/src/main/java/org/jooq/impl/JSONArrayAgg.java
@@ -65,6 +65,7 @@ import static org.jooq.impl.SQLDataType.JSON;
 import static org.jooq.impl.SQLDataType.JSONB;
 import static org.jooq.impl.SQLDataType.VARCHAR;
 import static org.jooq.impl.Tools.BooleanDataKey.DATA_FORCE_CASE_ELSE_NULL;
+import static org.jooq.impl.Tools.castIfNeeded;
 
 import java.util.Collection;
 import java.util.Set;
@@ -195,12 +196,22 @@ implements
     private final Field<?> groupConcatEmulation(Context<?> ctx) {
         Field<?> arg1 = arguments.get(0);
 
-        if (arg1.getDataType().isString()) {
+        // [#19643] Treat all non-JSON data types as string types
+        if (!arg1.getDataType().isBoolean()
+            && !arg1.getDataType().isNumeric()
+            && !arg1.getDataType().isJSON()
+        ) {
             switch (ctx.family()) {
                 case MARIADB:
                 case MYSQL:
-                    arg1 = function(N_JSON_QUOTE, getDataType(), arg1);
+                    arg1 = function(N_JSON_QUOTE, getDataType(), castIfNeeded(arg1, String.class));
                     break;
+
+
+
+
+
+
 
 
 

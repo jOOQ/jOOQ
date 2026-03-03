7e1813110e2538a4f4d145b93c91c35bad9c1432
Lukas Eder <lukas.eder@datageekery.com>
2026-03-03 11:01:58 +0100

    [jOOQ/jOOQ#19695] SQLDataType.LOCALDATE produces Nullable(Nullable(date)) in DDL or casts, instead of just Nullable(date)

--- jOOQ/src/main/java/org/jooq/impl/AbstractDataType.java
+++ jOOQ/src/main/java/org/jooq/impl/AbstractDataType.java
@@ -665,7 +665,7 @@ implements
 
         switch (configuration.family()) {
             case CLICKHOUSE:
-                if (nullable() && !isJSON() && !isArray() && !isSpatial())
+                if (nullable() && !isJSON() && !isArray() && !isSpatial() && !n.startsWith("Nullable("))
                     return "Nullable(" + n + ")";
                 else
                     return n;

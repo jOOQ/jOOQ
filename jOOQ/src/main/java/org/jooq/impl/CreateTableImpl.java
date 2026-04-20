1a596cc811aa2c6b98d3c4246d1949dd6c310215
Lukas Eder <lukas.eder@datageekery.com>
2026-04-20 10:30:51 +0200

    [jOOQ/jOOQ#19855] Avoid rendering GLOBAL keyword in PostgreSQL's createGlobalTemporaryTable() support

--- jOOQ/src/main/java/org/jooq/impl/CreateTableImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/CreateTableImpl.java
@@ -881,11 +881,13 @@ implements
 
 
 
+
             case CLICKHOUSE:
 
             case DUCKDB:
             case MARIADB:
             case MYSQL:
+            case POSTGRES:
 
 
             case YUGABYTEDB:
@@ -893,9 +895,7 @@ implements
                 break;
 
 
-
             case FIREBIRD:
-            case POSTGRES:
 
                 ctx.visit(K_GLOBAL).sql(' ').visit(K_TEMPORARY).sql(' ');
                 break;

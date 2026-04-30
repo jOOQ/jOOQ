7641e13039e4ceeb778971a9ff728ab34ee42248
Lukas Eder <lukas.eder@datageekery.com>
2026-04-30 09:05:15 +0200

    [jOOQ/jOOQ#19908] Nested rows within MULTISET projections do not apply JSON or XML cast necessary for serialising certain data types

--- jOOQ/src/main/java/org/jooq/impl/AbstractRowAsField.java
+++ jOOQ/src/main/java/org/jooq/impl/AbstractRowAsField.java
@@ -46,6 +46,8 @@ import static org.jooq.impl.DSL.jsonbObject;
 import static org.jooq.impl.DSL.quotedName;
 import static org.jooq.impl.DSL.select;
 import static org.jooq.impl.DSL.xmlelement;
+import static org.jooq.impl.Multiset.castForJSON;
+import static org.jooq.impl.Multiset.castForXML;
 import static org.jooq.impl.Multiset.returningClob;
 import static org.jooq.impl.Multiset.wrapXmlelement;
 import static org.jooq.impl.Names.N_RECORD;
@@ -187,7 +189,7 @@ implements
 
 
                         default:
-                            ctx.visit(alias(ctx, alias, returningClob(ctx, jsonArray(row.fields()).nullOnNull())));
+                            ctx.visit(alias(ctx, alias, returningClob(ctx, jsonArray(map(row.fields(), f -> castForJSON(ctx, f))).nullOnNull())));
                             break;
                     }
 
@@ -212,7 +214,7 @@ implements
 
 
                         default:
-                            ctx.visit(alias(ctx, alias, returningClob(ctx, jsonbArray(row.fields()).nullOnNull())));
+                            ctx.visit(alias(ctx, alias, returningClob(ctx, jsonbArray(map(row.fields(), f -> castForJSON(ctx, f))).nullOnNull())));
                             break;
                     }
 
@@ -231,7 +233,7 @@ implements
 
                         default:
                             ctx.visit(alias(ctx, alias, xmlelement(N_RECORD,
-                                map(row.fields(), (f, i) -> wrapXmlelement(ctx, f, fieldNameString(i)))
+                                map(row.fields(), (f, i) -> wrapXmlelement(ctx, castForXML(ctx, f), fieldNameString(i)))
                             )));
 
                             break;

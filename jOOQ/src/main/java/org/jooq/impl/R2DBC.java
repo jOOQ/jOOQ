54185b0691c9f41370220e0471b8070701d7b337
Lukas Eder <lukas.eder@datageekery.com>
2026-05-19 18:33:36 +0200

    [jOOQ/jOOQ#20001] [jOOQ/jOOQ#20002] BLOB array support in R2DBC
    This includes:
    - [jOOQ/jOOQ#20001] Error when reading a BLOB.array() typed NULL value
    from PostgreSQL using R2DBC
    - [jOOQ/jOOQ#20002] Cannot encode null parameter of type [[B when
    binding bytea[] in PostgreSQL using R2DBC

--- jOOQ/src/main/java/org/jooq/impl/R2DBC.java
+++ jOOQ/src/main/java/org/jooq/impl/R2DBC.java
@@ -1121,6 +1121,14 @@ final class R2DBC {
 
 
 
+                    case POSTGRES:
+                        if (type == byte[][].class)
+                            s.bindNull(parameterIndex - 1, String.class);
+                        else
+                            s.bindNull(parameterIndex - 1, type);
+
+                        break;
+
                     default:
                         s.bindNull(parameterIndex - 1, type);
                         break;
@@ -1467,6 +1475,9 @@ final class R2DBC {
         @Override
         public final Array getArray(int columnIndex) throws SQLException {
             Object o = nullable(columnIndex, Object.class);
+
+            if (o == null)
+                return null;
 
             switch (c.family()) {
                 case POSTGRES: {

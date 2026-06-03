f39f27724539c09206482353056e1169cea5e2ce
Lukas Eder <lukas.eder@datageekery.com>
2026-06-03 16:00:29 +0200

    [jOOQ/jOOQ#19700] Get this to work for DuckDB as well

--- jOOQ/src/main/java/org/jooq/impl/InsertQueryImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/InsertQueryImpl.java
@@ -188,7 +188,7 @@ implements
     static final Set<SQLDialect> NO_SUPPORT_SUBQUERY_IN_MERGE_USING            = SQLDialect.supportedBy(DERBY);
     static final Set<SQLDialect> REQUIRE_NEW_MYSQL_EXCLUDED_EMULATION          = SQLDialect.supportedBy(MYSQL);
     static final Set<SQLDialect> NO_SUPPORT_INSERT_ALIASED_TABLE               = SQLDialect.supportedBy(DERBY, DUCKDB, FIREBIRD, H2, MARIADB, MYSQL, TRINO);
-    static final Set<SQLDialect> SUPPORT_NATIVE_EXCLUDED                       = SQLDialect.supportedBy(POSTGRES, SQLITE, YUGABYTEDB);
+    static final Set<SQLDialect> SUPPORT_NATIVE_EXCLUDED                       = SQLDialect.supportedBy(DUCKDB, POSTGRES, SQLITE, YUGABYTEDB);
     static final Set<SQLDialect> NO_SUPPORT_ON_CONSTRAINT_ON_CONFLICT          = SQLDialect.supportedUntil(DUCKDB);
 
     final FieldMapsForInsert     insertMaps;

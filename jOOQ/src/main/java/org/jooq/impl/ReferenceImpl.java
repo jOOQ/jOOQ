b4f06c7c7daee92fae96351f30460f4965a46554
Lukas Eder <lukas.eder@datageekery.com>
2026-08-24 13:26:49 +0200

    [jOOQ/jOOQ#20147] Treat to-many joins with UNIQUE constraint on FOREIGN KEY as to-one joins

--- jOOQ/src/main/java/org/jooq/impl/ReferenceImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/ReferenceImpl.java
@@ -42,10 +42,13 @@ import static org.jooq.impl.Tools.filterOne;
 import static org.jooq.impl.Tools.first;
 import static org.jooq.impl.Tools.list;
 import static org.jooq.impl.Tools.map;
+import static org.jooq.impl.Tools.unaliasedFields;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.jooq.ConstraintEnforcementStep;
 import org.jooq.DSLContext;
@@ -78,6 +81,7 @@ implements
     private final TableField<PARENT, ?>[] ukFields;
     private final ForeignKeyRule          deleteRule;
     private final ForeignKeyRule          updateRule;
+    private transient Boolean             unique;
 
     ReferenceImpl(
         Table<CHILD> table,
@@ -261,5 +265,21 @@ implements
         return DSL.constraint(getName())
                   .foreignKey(getFieldsArray())
                   .references(uk.getTable(), getKeyFieldsArray());
+    }
+
+    @Override
+    public final boolean unique() {
+        if (unique == null) {
+            Set<Field<?>> fkf = new HashSet<>(Tools.map(getFields(), Tools::unqualified));
+            List<TableField<CHILD, ?>> ukf;
+
+            for (UniqueKey<CHILD> u : table.getKeys())
+                if (fkf.size() == (ukf = u.getFields()).size() && fkf.equals(new HashSet<>(Tools.map(ukf, Tools::unqualified))))
+                    return unique = true;
+
+            return unique = false;
+        }
+
+        return unique;
     }
 }

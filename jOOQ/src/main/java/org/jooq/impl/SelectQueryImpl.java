ecc0f2dd20b0428b3063288246fbc02449af5a3f
Lukas Eder <lukas.eder@datageekery.com>
2026-09-10 17:59:00 +0200

    [jOOQ/jOOQ#20220] Wrong no-op logic in $limitWithTies() and $offset() mutators of SelectQueryImpl

--- jOOQ/src/main/java/org/jooq/impl/SelectQueryImpl.java
+++ jOOQ/src/main/java/org/jooq/impl/SelectQueryImpl.java
@@ -5704,7 +5704,7 @@ final class SelectQueryImpl<R extends Record> extends AbstractResultQuery<R> imp
 
     @Override
     public final Select<R> $limitWithTies(boolean newLimitWithTies) {
-        if ($limitPercent() == newLimitWithTies)
+        if ($limitWithTies() == newLimitWithTies)
             return this;
         else
             return copy(s -> s.getLimit().setWithTies(newLimitWithTies));
@@ -5717,7 +5717,7 @@ final class SelectQueryImpl<R extends Record> extends AbstractResultQuery<R> imp
 
     @Override
     public final Select<R> $offset(Field<? extends Number> newOffset) {
-        if ($limit() == newOffset)
+        if ($offset() == newOffset)
             return this;
 
         // [#5695] TODO: Support all types of Field!

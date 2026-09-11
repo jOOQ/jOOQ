f1a8c169d087dd83ed5961c6d2ba66d6baa2b6e0
Lukas Eder <lukas.eder@datageekery.com>
2026-09-11 13:58:29 +0200

    [jOOQ/jOOQ#20237] Code generator doesn't escape UDT name or schema name

--- jOOQ-codegen/src/main/java/org/jooq/codegen/JavaGenerator.java
+++ jOOQ-codegen/src/main/java/org/jooq/codegen/JavaGenerator.java
@@ -3432,7 +3432,7 @@ public class JavaGenerator extends AbstractGenerator {
         else {
             out.javadoc(NO_FURTHER_INSTANCES_ALLOWED);
             out.println("private %s() {", className);
-            out.println("super(%s.name(\"%s\"), null, %s, %s);", DSL.class, udt.getOutputName(), packageId, synthetic);
+            out.println("super(%s.name(\"%s\"), null, %s, %s);", DSL.class, escapeString(udt.getOutputName()), packageId, synthetic);
             out.println("}");
         }
 
@@ -3448,7 +3448,7 @@ public class JavaGenerator extends AbstractGenerator {
             else {
                 out.overrideInherit();
                 out.println("%s%s getSchema() {", visibilityPublic(), Schema.class);
-                out.println("return %s != null ? %s : new %s(%s.name(\"%s\"));", schemaId, schemaId, SchemaImpl.class, DSL.class, schema.getOutputName());
+                out.println("return %s != null ? %s : new %s(%s.name(\"%s\"));", schemaId, schemaId, SchemaImpl.class, DSL.class, escapeString(schema.getOutputName()));
                 out.println("}");
             }
         }

458401ef07feb56ea5701f9c0754c8ee6dc6fb8f
Lukas Eder <lukas.eder@gmail.com>
2026-09-25 15:55:43 +0200

    [jOOQ/jOOQ#17832] nullability() should return this if unchanged

--- jOOQ/src/main/java/org/jooq/impl/BuiltInDataType.java
+++ jOOQ/src/main/java/org/jooq/impl/BuiltInDataType.java
@@ -180,6 +180,9 @@ public class BuiltInDataType<T> extends DefaultDataType<T> {
 
     @Override
     public final DataType<T> nullability(Nullability n) {
+        if (n == nullability())
+            return this;
+
         if (n == NULL) {
             DataType<T> result = cachedNull;
 
